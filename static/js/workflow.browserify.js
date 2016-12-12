eeb33ba1a1d9bbd83475d2df4ccbc175d11332c5
jstray <jonathanstray@gmail.com>
2016-12-12 17:31:25 -0500

    Detect which item clicked

--- static/js/workflow.browserify.js
+++ static/js/workflow.browserify.js
@@ -26,35 +26,49 @@ class ButtonMenu extends React.Component {
     this.state = { open: false, items: []};
 
     // annoying bind to make 'this' accessible in handlers
-    this.click= this.click.bind(this);
-    this.blur= this.blur.bind(this);
+    this.buttonClick = this.buttonClick.bind(this);
+    this.itemMouseDown = this.itemMouseDown.bind(this);
+    this.itemClick = this.itemClick.bind(this);
+    this.blur = this.blur.bind(this);
   }
 
   componentDidMount() {
-     var _this = this;
+    var _this = this;
     fetch('/api/modules/')
       .then(response => response.json())
       .then(json => {
         _this.setState({open: false, items: json}) })
   }
 
-  click() {
+  buttonClick() {
+    // Toggle menu state
     var newOpen = !this.state.open;
     this.setState( { open: newOpen});
   }
 
-  // close the menu when user clicks anywhere else
-  blur() {
+  itemMouseDown(evt) {
+    evt.preventDefault()    // so the menu button doesn't lose focus and trigger blur, preventing item clicked
+  }
+
+  itemClick(evt) {
+    console.log('itemClick');
+    var itemID = evt.target.getAttribute('data-id');
+    console.log("Clicked item " + itemID)
     this.setState( { open: false});
+  }
+
+  // close the menu when user clicks anywhere but on a menu item
+  blur() {
+    this.setState({ open: false});
   }
 
   render() {
     return (
         <div className="toolMenuOuter" onBlur={this.blur}>
-            <button className="toolMenuButton" onClick={this.click}>+</button>
+            <button className="toolMenuButton" onClick={this.buttonClick}>+</button>
             <ul className="toolMenuItemHolder" style={{display: this.state.open ? 'block' : 'none'}}>
               {this.state.items.map(
-                  item => {return <li className="toolMenuItem" key={item.id}>{item.name}</li>;})
+                  item => {return <li className="toolMenuItem" key={item.id} data-id={item.id} onMouseDown={this.itemMouseDown} onClick={this.itemClick}> {item.name} </li>;})
               }
             </ul>
         </div>
