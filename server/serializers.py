f818f7ded8e090c55386bd475cd909d1937c7bf6
alexgonca <alexandre.reportagem@gmail.com>
2016-12-13 15:00:12 -0500

    fix related_names in serializers

--- server/serializers.py
+++ server/serializers.py
@@ -4,27 +4,28 @@ from server.models import Workflow, WfModule, ParameterVal, ParameterSpec, Modul
 class ParameterValSerializer(serializers.ModelSerializer):
     class Meta:
         model = ParameterVal
-        fields = ('wf_module', 'parameter_specs', 'number', 'string', 'text')
+        fields = ('id', 'parameter_spec', 'number', 'string', 'text')
 
 class ParameterSpecSerializer(serializers.ModelSerializer):
     class Meta:
         model = ParameterSpec
-        fields = ('name', 'type', 'module', 'default_number', 'default_string', 'default_text')
+        fields = ('id', 'name', 'type', 'default_number', 'default_string', 'default_text')
 
 class ModuleSerializer(serializers.ModelSerializer):
     parameter_specs = ParameterSpecSerializer(many=True, read_only=True)
     class Meta:
         model = Module
-        fields = ('name', 'parameter_specs')
+        fields = ('id', 'name', 'parameter_specs')
 
 class WfModuleSerializer(serializers.ModelSerializer):
-    parameters = ParameterValSerializer(many=True, read_only=True)
+    parameter_vals = ParameterValSerializer(many=True, read_only=True)
+    module = ModuleSerializer(many=False, read_only=True)
     class Meta:
         model = WfModule
-        fields = ('id', 'order', 'module', 'parameters')
+        fields = ('id', 'order', 'module', 'parameter_vals')
 
 class WorkflowSerializer(serializers.ModelSerializer):
-    modules = WfModuleSerializer(many=True, read_only=True)
+    wf_modules = WfModuleSerializer(many=True, read_only=True)
     class Meta:
         model = Workflow
-        fields = ('id', 'name', 'creation_date', 'modules')
+        fields = ('id', 'name', 'creation_date', 'wf_modules')
