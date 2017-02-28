68a32a8af0c3eef1c3c9f3a1dd8871e0ec5b78dd
jstray <jonathanstray@gmail.com>
2017-02-27 20:41:06 -0500

    Add workflow button

--- assets/js/workflows.js
+++ assets/js/workflows.js
@@ -1,41 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { csrfToken } from './utils'
 
 require('../css/style.css');
 
-var TestApp = React.createClass({
+class Workflows extends React.Component {
+  constructor(props) {
+    super(props);
+    this.click = this.click.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.state = { workflows: [], newWorkflowName: '' }
+  }
 
-  getInitialState: function() {
-    return {
-      workflows: []
-    }
-  },
+  handleTextChange(event) {
+    this.setState({workflows: this.state.workflows, newWorkflowName: event.target.value});
+  }
 
-  componentDidMount: function() {
+  // Make a new workflow when button clicked
+  click(e) {
+    fetch('/api/workflows', {
+      method: 'post',
+      credentials: 'include',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrfToken
+      },
+      body: JSON.stringify({name: this.state.newWorkflowName})
+    })
+    .then(response => response.json())
+    .then(json => {
+      var newWorkflows = this.state.workflows.slice();
+      newWorkflows.push(json);
+      console.log(newWorkflows)
+      this.setState({workflows: newWorkflows, newWorkflowName: ''})
+    })
+  }
+
+  componentDidMount() {
     var _this = this;
 
-    fetch('/api/workflows', { credentials: 'include' })
+    fetch('/api/workflows', {credentials: 'include'})
       .then(response => response.json())
       .then(json => {
-        _this.setState({workflows: json})
+        _this.setState({workflows: json, newWorkflowName: this.state.newWorkflowName})
       })
-  },
+  }
 
-  render: function() {
+  render() {
     return (
       <div className="page">
         <h1>Workflows!</h1>
         <ul>
-          {this.state.workflows.map(function(listValue){
+          {this.state.workflows.map(function (listValue) {
             return <li key={listValue.id}><a href={"/workflows/" + listValue.id}>{listValue.name}</a></li>;
           })}
         </ul>
+        <input type="text" className='newWorkflowName' value={this.state.value} onChange={this.handleTextChange}/>
+        <button className='newWorkflowButton' onClick={this.click}>New</button>
       </div>
     );
   }
-});
+}
 
 ReactDOM.render(
-  React.createElement(TestApp, null),
+  React.createElement(Workflows, null),
   document.getElementById('root')
 );
