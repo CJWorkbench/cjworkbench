0707d5347df06659bd2c78de3cd0a391cf0ef37c
jstray <jonathanstray@gmail.com>
2017-05-15 19:13:14 -0400

    Need the tests too.

---
+++ assets/js/MenuParam.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow, render, mount } from 'enzyme';
+
+import MenuParam from './MenuParam';
+
+it('renders correctly', () => {
+  var menuStr = 'Apple|Kittens|Banana';
+
+  const wrapper = shallow( <MenuParam items={menuStr} name="SuperMenu" selectedIdx={1} onChange={ () => {} } /> );
+  expect(wrapper).toMatchSnapshot();
+});
+
