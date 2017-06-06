abb74fd5162693d3ded28a3acde6275545a2af45
jstray <jonathanstray@gmail.com>
2017-06-05 19:01:48 -0700

    Replace twarc with tweepy, use app auth

--- server/modules/twitter.py
+++ server/modules/twitter.py
@@ -1,4 +1,4 @@
-from twarc import Twarc
+import tweepy
 import pandas as pd
 from datetime import date, timedelta
 import requests
@@ -27,16 +27,16 @@ class Twitter(ModuleImpl):
 
     # Get from Twitter, return as dataframe
     @staticmethod
-    def get_new_tweets(querytype, query, old_tweets):
+    def get_new_tweets(wfm, querytype, query, old_tweets):
+
+        # Authenticate with "app authentication" mode (high rate limit, read only)
         consumer_key = os.environ['CJW_TWITTER_CONSUMER_KEY']
         consumer_secret = os.environ['CJW_TWITTER_CONSUMER_SECRET']
-        access_token = os.environ['CJW_TWITTER_ACCESS_TOKEN']
-        access_token_secret = os.environ['CJW_TWITTER_ACCESS_TOKEN_SECRET']
-
-        tw = Twarc(consumer_key, consumer_secret, access_token, access_token_secret)
+        auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
+        api = tweepy.API(auth)
 
         if querytype == 'User':
-            tweetsgen = tw.timeline(screen_name=query)
+            tweetsgen = api.user_timeline(query, count=200)
         else:
             # Only get last 24 hours of tweets, because we have to bound this somehow
             today = date.today()
@@ -45,21 +45,21 @@ class Twitter(ModuleImpl):
                 yesterday.year, yesterday.month, yesterday.day,
                 today.year, today.month, today.day)
 
-            tweetsgen = tw.search(query + date_clause)
+            tweetsgen = api.search(query + date_clause)
 
         # Columns to retrieve and store from Twitter
         # Also, we use this to figure ou the index the id field when merging old and new tweets
         cols = ['id', 'created_at', 'text', 'in_reply_to_screen_name', 'in_reply_to_status_id', 'retweeted',
                 'retweet_count', 'favorited', 'favorite_count', 'source']
 
-        tweets = [ [t[x] for x in cols] for t in tweetsgen]
+        tweets = [[getattr(t, x) for x in cols] for t in tweetsgen]
         table = pd.DataFrame(tweets, columns=cols)
         return table
 
 
     # Combine this set of tweets with previous set of tweets
     def merge_tweets(wf_module, new_table):
-        old_table = Twitter.get_stored_tweets()
+        old_table = Twitter.get_stored_tweets(wf_module)
         if old_table != None:
             new_table = pd.concat([new_table,old_table]).drop_duplicates().sort_values('id',ascending=False).reset_index(drop=True)
         return new_table
@@ -85,24 +85,28 @@ class Twitter(ModuleImpl):
 
             if wfm.get_param_checkbox('accumulate'):
                 old_tweets = Twitter.get_stored_tweets(wfm)
-                tweets = Twitter.get_new_tweets(querytype, query, old_tweets)
+                tweets = Twitter.get_new_tweets(wfm, querytype, query, old_tweets)
                 tweets = Twitter.merge_tweets(wfm, tweets)
             else:
-                tweets = Twitter.get_new_tweets(querytype, query, None)
+                tweets = Twitter.get_new_tweets(wfm, querytype, query, None)
 
         except requests.exceptions.HTTPError as e:
             if querytype=='User' and e.response.status_code==401:
                 wfm.set_error('User %s\'s tweets are protected' % query)
+                wfm.store_text('csv', '')
                 return
             elif querytype=='User'and response.status_code==404:
                 wfm.set_error('User %s does not exist' % query)
+                wfm.store_text('csv', '')
                 return
             else:
                 wfm.set_error('HTTP rrror %s fetching tweets' % str(res.status_code))
+                wfm.store_text('csv', '')
                 return
 
         except Exception as e:
             wfm.set_error('Error fetching tweets: ' + str(e))
+            wfm.store_text('csv', '')
             return
 
 
