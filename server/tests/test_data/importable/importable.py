4186046cec4192c9efbc5044d238e455172192dd
anothercookiecrumbles <priyanjana@mac.com>
2017-06-28 01:59:13 -0400

    Provides ability for modules to load dynamically on the server-side, provided that these modules are already checked out on the file system.

---
+++ server/tests/test_data/importable/importable.py
@@ -0,0 +1,14 @@
+class Importable:
+  @staticmethod
+  def event():
+    print("event")
+
+  @staticmethod
+  def render(wf_module, table):
+    return table
+
+  def __init__(self, name):
+    self.name = name
+
+  def __init__(self, name):
+    self.name = name
