a2afeb7685499d9a3c7c1bf2a766e9ca5890961f
Matthew Gerring <gerring.matthew@gmail.com>
2017-07-12 13:47:39 -0700

    Simplest possible registration flow

--- cjworkbench/urls.py
+++ cjworkbench/urls.py
@@ -18,10 +18,17 @@ from django.contrib import admin
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = [
     url(r'^login/?$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/?$', auth_views.logout, {'template_name': 'logout.html', 'next_page' : '/login/' }, name='logout' ),
+    url(r'^register/?$', CreateView.as_view(
+        template_name='register.html',
+        form_class=UserCreationForm,
+        success_url='/'
+    ) ),
     url(r'^admin/?', admin.site.urls),
     url(r'^', include('server.urls'))
 ]
