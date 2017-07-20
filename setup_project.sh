87c83163d02acc0c7dbccdaab8010528c28c4599
anothercookiecrumbles <priyanjana@mac.com>
2017-07-19 20:16:59 -0400

    Adds colour and also adds a mode where users can just enter three args. However, this mode will only work if all three args are present. Else, it'll use the defaults. The earlier getopts and defaults still works as expected.

--- setup_project.sh
+++ setup_project.sh
@@ -1,7 +1,14 @@
 if [[ $1 == '-h' ]]; then
-  echo "Usage: $0 [-u username] [-p password] [-e email]"
+  tput setaf 6; echo "Usage: $0 [-u username] [-p password] [-e email]"
   exit 0;
 fi
+
+if [ "$#" -eq 3 ]; then
+  u=$1
+  p=$2
+  e=$3
+fi
+
 
 # check user input, set defaults
 while getopts ":u:p:e:" o; do
@@ -22,23 +29,25 @@ user="${u:-`whoami`}"
 password="${p:-"password"}"
 email="${e:-"boom@boom.com"}"
 
+tput setaf 6; echo "Using credentials username: ${user}, password: ${password}, email: ${email}"; tput setaf 7;
+
 # validate python install
 case "$(python --version 2>&1)" in
   *" 3."*)
-    echo "Checking Python... installed"
+    tput setaf 35; echo "Checking Python... installed"; tput setaf 7;
     ;;
   *)
-    echo "Checking Python... python 3 not installed, exiting."
+    tput setaf 1; echo "Checking Python... python 3 not installed, exiting."; tput setaf 7;
     exit 1
     ;;
 esac
 # check npm 
 case "$(npm --version 2>&1)" in
   "4."*)
-    echo "Checking npm... installed."
+    tput setaf 35; echo "Checking npm... installed."; tput setaf 7;
     ;;
   *)
-    echo "Checking npm... npm 4.* not installed, exiting."
+    tput setaf 1; echo "Checking npm... npm 4.* not installed, exiting."; tput setaf 7;
     exit 1
     ;;
 esac
@@ -47,29 +56,30 @@ esac
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
 pip install -r requirements-dev.txt
-echo "Installed Python dependencies..."
+tput setaf 35; echo "Installed Python dependencies..."; tput setaf 7;
 
 # install npm modules 
 echo "Installing node modules..."
 npm install
-echo "Installed node modules..."
+tput setaf 35; echo "Installed node modules..."; tput setaf 7;
 
 # project-specific malarky 
 echo "Updating submodules..."
 git submodule update --init --recursive
 git submodule update --remote
-echo "Updated submodules" 
+tput setaf 35; echo "Updated submodules..."; tput setaf 7;
 
 # database malarky
 echo "Setting up the database..."
 python manage.py migrate
-echo "Set up database..."
+tput setaf 35; echo "Finished setting up database..."; tput setaf 7;
 
 #You can thank Django for this... if I try to simply pipe input in, it says:
 #"Superuser creation skipped due to not running in a TTY." So, here I go.
 echo "Creating superuser..."
 echo "from django.contrib.auth.models import User;
 User.objects.create_superuser('${user}', '${email}', '${password}')" | python manage.py shell
-echo "Created superuser with username '${user}', email '${email}' and password '${password}'."
 
-echo "DONE."
+tput setaf 6; echo "Created superuser with username '${user}', email '${email}' and password '${password}'."
+
+tput setaf 34; echo "DONE."
