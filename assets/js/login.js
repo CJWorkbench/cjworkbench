01e2f1ef84f1ed0a65493c1e43e4bd86ead5512c
Matthew Gerring <gerring.matthew@gmail.com>
2017-08-02 20:11:01 -0700

    classname -> class

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,3 @@
 // There really isn't any login on the login page. This file really just sucks in our css
 require('bootstrap/dist/css/bootstrap.css');
 require('../css/style.css'); // put second to override background color etc
-
