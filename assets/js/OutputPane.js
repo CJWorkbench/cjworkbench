b00ea505a0c67bb2e48084dcf6365932f4f9f203
jstray <jonathanstray@gmail.com>
2017-08-31 16:19:30 -0700

    Merge duplicated JSX code for output pane

--- assets/js/OutputPane.js
+++ assets/js/OutputPane.js
@@ -43,44 +43,38 @@ export default class OutputPane extends React.Component {
 
   render() {
     // Don't show anything if we don't have a selected WfModule to show
-    if (this.props.id && this.state.tableData && this.state.tableData.totalrows>0) {
-
-      return (
-        <div className="outputpane-box">
-          <div className="outputpane-header d-flex flex-row justify-content-start">
-            <div className='d-flex flex-column align-items-center justify-content-center mr-5'>
-              <div className='content-4 t-m-gray mb-2'>Rows</div>
-              <div className='content-2 t-d-gray'>{this.state.tableData.totalrows}</div>
-            </div>
-            <div className='d-flex flex-column align-items-center justify-content-center'>
-              <div className='content-4 t-m-gray mb-2'>Columns</div>
-              <div className='content-2 t-d-gray'>{this.state.tableData.columns.length}</div>
-            </div>
-          </div>
-          <div className="outputpane-data">
-            <TableView tableData={this.state.tableData} />
-          </div>
-        </div>
-      );
-    } else if (this.props.id) {
-      // When we want an output, but no TableData is present
-      return (
-        <div className="outputpane-box">
-          <div className="outputpane-header d-flex flex-row justify-content-start">
-            <div className='d-flex flex-column align-items-center justify-content-center mr-5'>
-              <div className='content-4 t-m-gray mb-2'>Rows</div>
-              <div className='content-2 t-d-gray'>0</div>
-            </div>
-            <div className='d-flex flex-column align-items-center justify-content-center'>
-              <div className='content-4 t-m-gray mb-2'>Columns</div>
-              <div className='content-2 t-d-gray'>0</div>
-            </div>
-          </div>
-        </div>
-      );
-    } else {
-      return null;
+    if (!this.props.id) {
+      return null
     }
+
+    // Make a table component if we have the data
+    var tableView = null;
+    var nrows = 0;
+    var ncols = 0;
+    if (this.state.tableData && this.state.tableData.totalrows>0) {
+      tableView =
+        <div className="outputpane-data">
+          <TableView tableData={this.state.tableData} />
+        </div>
+      nrows = this.state.tableData.totalrows;
+      ncols = this.state.tableData.columns.length;
+    }
+
+    return (
+      <div className="outputpane-box">
+        <div className="outputpane-header d-flex flex-row justify-content-start">
+          <div className='d-flex flex-column align-items-center justify-content-center mr-5'>
+            <div className='content-4 t-m-gray mb-2'>Rows</div>
+            <div className='content-2 t-d-gray'>{nrows}</div>
+          </div>
+          <div className='d-flex flex-column align-items-center justify-content-center'>
+            <div className='content-4 t-m-gray mb-2'>Columns</div>
+            <div className='content-2 t-d-gray'>{ncols}</div>
+          </div>
+        </div>
+        {tableView}
+      </div>
+    );
   }
 }
 
