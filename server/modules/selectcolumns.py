98bada275872e01a9c6b146175bfe177ca4c550f
alexgonca <alexandre.reportagem@gmail.com>
2017-09-14 12:19:29 -0400

    Add keep or drop option to select columns

--- server/modules/selectcolumns.py
+++ server/modules/selectcolumns.py
@@ -4,9 +4,11 @@ from .moduleimpl import ModuleImpl
 
 class SelectColumns(ModuleImpl):
     def render(wf_module, table):
+        drop_or_keep = wf_module.get_param_menu_idx("drop_or_keep")
         cols = wf_module.get_param_string('colnames').split(',')
         cols = [c.strip() for c in cols]
 
+        # if no column has been select, keep the columns
         if cols == [] or cols == ['']:
             return table
 
@@ -16,5 +18,8 @@ class SelectColumns(ModuleImpl):
                 return None
 
         wf_module.set_ready(notify=False)
-        newtab = table[cols]
+        if drop_or_keep == 1:
+            newtab = table[cols]
+        else:
+            newtab = table.drop(cols, axis=1)
         return newtab
