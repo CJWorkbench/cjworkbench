9f992573618bfe53b3b5d1a1954d560fe31945ab
Pierre Conti <pierre@tablesdata.com>
2017-10-20 08:53:09 -0700

    Upload file styling

--- assets/js/DropZone.js
+++ assets/js/DropZone.js
@@ -19,7 +19,7 @@ export default class DropZone extends Component{
       <div>
         <Dropzone
             onDrop={this.onDrop}
-            wfModuleId={this.props.wfModuleId} className="dropzone d-flex justify-content-center align-items-center">
+            wfModuleId={this.props.wfModuleId} className="dropzone parameter-margin d-flex justify-content-center align-items-center">
           <div className='icon-add-blue'></div>
           <div className="title-3 ml-4">Drop file here, or click to select</div>
         </Dropzone>
