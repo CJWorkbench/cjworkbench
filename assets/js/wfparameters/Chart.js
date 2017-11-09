df600585041f956e9f2b69606942034eb08c8e2f
Matthew Gerring <gerring.matthew@gmail.com>
2017-11-08 17:26:42 -0800

    Don't load chartbuilder css for now

--- assets/js/wfparameters/Chart.js
+++ assets/js/wfparameters/Chart.js
@@ -11,7 +11,7 @@ var chartConfig = require("chartbuilder/src/js/charts/chart-type-configs");
 var saveSvgAsPng = require("save-svg-as-png");
 
 require("chartbuilder/dist/css/core.css");
-require("chartbuilder-ui/dist/styles.css");
+//require("chartbuilder-ui/dist/styles.css");
 
 // adapter, eventually obsolete with CSV format /input call, or direct edit of ChartBuilder data model
 function JSONtoCSV(d) {
