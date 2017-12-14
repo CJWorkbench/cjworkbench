c336a154c2c9f17f0ebaff90c9df596568ae0e1b
jstray <jonathanstray@gmail.com>
2017-12-14 18:07:53 -0500

    Catch format errors for CSV, Excel. Fixes https://www.pivotaltracker.com/story/show/153681095

--- server/modules/uploadfile.py
+++ server/modules/uploadfile.py
@@ -1,5 +1,7 @@
 from .moduleimpl import ModuleImpl
 from server.utils import sanitize_dataframe
+from pandas.errors import ParserError
+from xlrd import XLRDError
 from .utils import *
 
 class UploadFile(ModuleImpl):
@@ -7,25 +9,34 @@ class UploadFile(ModuleImpl):
     # Input table ignored.
     @staticmethod
     def render(wf_module, table):
-        wf_module.set_busy(notify=True)
         file = wf_module.retrieve_fetched_file()
-        if file != None:
-
-            if file.name.endswith('.xls') or file.name.endswith('.xlsx') or file.name.endswith('.XLS') or file.name.endswith('.XLSX'):
-                table_aux = pd.read_excel(file)
-                sanitize_dataframe(table_aux)
-                wf_module.set_ready(notify=True)
-                return table_aux
-
-            elif file.name.endswith('.csv') or file.name.endswith('.CSV'):
-                table_aux = pd.read_csv(file)
-                sanitize_dataframe(table_aux)
-                wf_module.set_ready(notify=True)
-                return table_aux
-
-            else:
-                wf_module.set_error('Unknown file type.')
-                return None
-        else:
+        if file is None:
             wf_module.set_ready(notify=True)
+            return
+
+        if file.name.endswith('.xls') or file.name.endswith('.xlsx') or file.name.endswith('.XLS') or file.name.endswith('.XLSX'):
+
+            try:
+                table_aux = pd.read_excel(file)
+            except XLRDError as e:
+                wf_module.set_error(str(e))
+                return None
+
+            sanitize_dataframe(table_aux)
+            wf_module.set_ready(notify=True)
+            return table_aux
+
+        elif file.name.endswith('.csv') or file.name.endswith('.CSV'):
+            try:
+                table_aux = pd.read_csv(file)
+            except ParserError as e:
+                wf_module.set_error(str(e))
+                return None
+
+            sanitize_dataframe(table_aux)
+            wf_module.set_ready(notify=True)
+            return table_aux
+
+        else:
+            wf_module.set_error('Unknown file type.')
             return None
