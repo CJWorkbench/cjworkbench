ae190ddb55ec14b1e2115aaaf63aefc4911dabbf
Pierre Conti <pierre@tablesdata.com>
2017-12-22 21:56:47 -0800

    error message text in CSV

--- server/modules/pastecsv.py
+++ server/modules/pastecsv.py
@@ -19,7 +19,7 @@ class PasteCSV(ModuleImpl):
             header_row = None
 
         if (len(tablestr)==0):
-            wf_module.set_error('Please enter a CSV')
+            wf_module.set_error('Paste data here')
             return None
         try:
             table = pd.read_csv(io.StringIO(tablestr), header=header_row,
