ab2ede4bb30e2d95ef904bd6f092a3c2f33216c8
Matthew Gerring <gerring.matthew@gmail.com>
2018-01-17 15:23:05 -0800

    Moving toward component-based SASS

--- assets/js/OutputPane.js
+++ assets/js/OutputPane.js
@@ -4,6 +4,8 @@ import React from 'react'
 import TableView from './TableView'
 import PropTypes from 'prop-types'
 
+require('../css/components/OutputPane.scss');
+
 export default class OutputPane extends React.Component {
 
   constructor(props) {
@@ -154,5 +156,3 @@ OutputPane.propTypes = {
   revision: PropTypes.number,
   api:      PropTypes.object.isRequired
 };
-
-
