69da8bbeb74567a154474d848e02b78a1e05396c
Matthew Gerring <gerring.matthew@gmail.com>
2018-01-26 15:15:56 -0800

    Test picking the correct module and setting notifications

--- assets/js/AddNotificationButtonClosed.test.js
+++ assets/js/AddNotificationButtonClosed.test.js
@@ -3,7 +3,7 @@
  * -Renders icon
  * -Mouse events on icon will show/hide full card
  * -Icon and card can be dragged
- * 
+ * -Click on button will find data-loading module and update notifications
  */
 
 import React from 'react'
@@ -11,18 +11,54 @@ import AddNotificationButtonClosed  from './AddNotificationButtonClosed'
 import { mount } from 'enzyme'
 import HTML5Backend from 'react-dnd-html5-backend'
 import { DragDropContextProvider } from 'react-dnd'
+var reducer = require("./workflow-reducer");
 
-
-describe('AddNotificationButtonOpen ', () => {
+describe('AddNotificationButtonClosed ', () => {
 
   var wrapper;  
-  var setOpenCategory = () => {};
+  var mockSetOpenCategory;
 
-  beforeEach(() => wrapper = mount(
-    <DragDropContextProvider backend={HTML5Backend}>
-      <AddNotificationButtonClosed setOpenCategory={setOpenCategory}/>
-    </DragDropContextProvider>
-  ));
+  beforeEach(() => {
+      var mockGetState = jest.fn();
+      mockGetState.mockReturnValue({
+         workflow: {
+             wf_modules: [
+                 {
+                     id: 1,
+                     notifications: false,
+                     module_version: {
+                         module: {
+                             loads_data: true
+                         }
+                     }
+                 },
+                 {
+                     id: 2,
+                     notifications: false,
+                     module_version: {
+                         module: {
+                             loads_data: false
+                         }
+                     }
+                 }
+             ]
+         }
+      });
+
+      reducer.store = {
+          getState: mockGetState,
+          dispatch: jest.fn()
+      };
+      reducer.updateWfModuleAction = jest.fn();
+
+      mockSetOpenCategory = jest.fn();
+
+      wrapper = mount(
+          <DragDropContextProvider backend={HTML5Backend}>
+              <AddNotificationButtonClosed setOpenCategory={mockSetOpenCategory}/>
+          </DragDropContextProvider>
+      );
+  });
   afterEach(() => wrapper.unmount());    
 
   it('Renders', () => { 
@@ -38,6 +74,7 @@ describe('AddNotificationButtonOpen ', () => {
     expect(popout.props().style).toEqual({display:'none'});
     // simulate mouse enter
     icon.simulate('mouseEnter');
+    expect(mockSetOpenCategory.mock.calls.length).toBe(1);
     // look for card display
     expect(popout.props().style).toEqual({display:'block'});
     // simulate mouse leave
@@ -46,10 +83,17 @@ describe('AddNotificationButtonOpen ', () => {
     expect(popout.props().style).toEqual({display:'none'});
   });
 
+  it('Should fire click events on click', () => {
+      let icon = wrapper.find('.notification-button-closed');
+      icon.simulate('click');
+      expect(reducer.store.getState.mock.calls.length).toBe(1);
+      expect(reducer.store.dispatch.mock.calls.length).toBe(1);
+      expect(reducer.updateWfModuleAction.mock.calls[0][0]).toBe(1); //Should have found wfmodule id 1
+      expect(reducer.updateWfModuleAction.mock.calls[0][1].notifications).toBe(true);
+  });
+
   it('Card is draggable', () => { 
     // search for property on the component that indicates drag-ability
     expect( Object.keys(wrapper.find('AddNotificationButtonClosed').props()).includes('connectDragSource') ).toBe(true);
   });
-
-
 });
--- assets/js/AddNotificationButtonOpen.test.js
+++ assets/js/AddNotificationButtonOpen.test.js
@@ -10,17 +10,53 @@ import AddNotificationButtonOpen  from './AddNotificationButtonOpen'
 import { mount } from 'enzyme'
 import HTML5Backend from 'react-dnd-html5-backend'
 import { DragDropContextProvider } from 'react-dnd'
+var reducer = require('./workflow-reducer.js');
 
 
 describe('AddNotificationButtonOpen ', () => {
 
-  var wrapper;  
+  var wrapper;
 
-  beforeEach(() => wrapper = mount(
-    <DragDropContextProvider backend={HTML5Backend}>
-      <AddNotificationButtonOpen/>
-    </DragDropContextProvider>
-  ));
+  beforeEach(() => {
+    var mockGetState = jest.fn();
+    mockGetState.mockReturnValue({
+       workflow: {
+           wf_modules: [
+               {
+                   id: 1,
+                   notifications: false,
+                   module_version: {
+                       module: {
+                           loads_data: true
+                       }
+                   }
+               },
+               {
+                   id: 2,
+                   notifications: false,
+                   module_version: {
+                       module: {
+                           loads_data: false
+                       }
+                   }
+               }
+           ]
+       }
+    });
+
+    reducer.store = {
+        getState: mockGetState,
+        dispatch: jest.fn()
+    };
+
+    reducer.updateWfModuleAction = jest.fn();
+
+    wrapper = mount(
+      <DragDropContextProvider backend={HTML5Backend}>
+        <AddNotificationButtonOpen/>
+      </DragDropContextProvider>
+    );
+  });
   afterEach(() => wrapper.unmount());    
 
   it('Renders', () => { 
@@ -29,7 +65,15 @@ describe('AddNotificationButtonOpen ', () => {
     expect(wrapper.find('.icon-notification')).toHaveLength(1);
   });
 
-  it('Card is draggable', () => { 
+  it('Finds correct wfmodule on click and dispatches change to notifications', () => {
+    var button = wrapper.find('.icon-notification');
+    button.simulate('click');
+    expect(reducer.store.getState.mock.calls.length).toBe(1);
+    expect(reducer.updateWfModuleAction.mock.calls[0][0]).toBe(1);
+    expect(reducer.updateWfModuleAction.mock.calls[0][1].notifications).toBe(true);
+  });
+
+  it('Card is draggable', () => {
     // search for property on the component that indicates drag-ability
     expect( Object.keys(wrapper.find('AddNotificationButtonOpen').props()).includes('connectDragSource') ).toBe(true);
   });
