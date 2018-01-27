af2213982f2fcbafa2c0e3ffdc42b68a6131b3e6
Pierre Conti <pierre@tablesdata.com>
2018-01-27 15:37:25 -0800

    Update test due to CSS class change

--- assets/js/AddNotificationButtonClosed.test.js
+++ assets/js/AddNotificationButtonClosed.test.js
@@ -3,7 +3,7 @@
  * -Renders icon
  * -Mouse events on icon will show/hide full card
  * -Icon and card can be dragged
- * 
+ *
  */
 
 import React from 'react'
@@ -15,7 +15,7 @@ import { DragDropContextProvider } from 'react-dnd'
 
 describe('AddNotificationButtonOpen ', () => {
 
-  var wrapper;  
+  var wrapper;
   var setOpenCategory = () => {};
 
   beforeEach(() => wrapper = mount(
@@ -23,17 +23,17 @@ describe('AddNotificationButtonOpen ', () => {
       <AddNotificationButtonClosed setOpenCategory={setOpenCategory}/>
     </DragDropContextProvider>
   ));
-  afterEach(() => wrapper.unmount());    
+  afterEach(() => wrapper.unmount());
 
-  it('Renders', () => { 
+  it('Renders', () => {
     expect(wrapper).toMatchSnapshot();
     // find matching icon
     expect(wrapper.find('.icon-notification')).toHaveLength(1);
   });
 
-  it('Mouse events on icon will show/hide full card', () => { 
+  it('Mouse events on icon will show/hide full card', () => {
     let icon = wrapper.find('.notification-button-closed');
-    let popout = wrapper.find('.notification-button-popout');
+    let popout = wrapper.find('.alert-closed-ML');
     // check that card not displayed initially (governed by 'display' property)
     expect(popout.props().style).toEqual({display:'none'});
     // simulate mouse enter
@@ -46,7 +46,7 @@ describe('AddNotificationButtonOpen ', () => {
     expect(popout.props().style).toEqual({display:'none'});
   });
 
-  it('Card is draggable', () => { 
+  it('Card is draggable', () => {
     // search for property on the component that indicates drag-ability
     expect( Object.keys(wrapper.find('AddNotificationButtonClosed').props()).includes('connectDragSource') ).toBe(true);
   });
