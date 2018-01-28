5b2cd981959f71cc0181b830f68b45209662793e
Pierre Conti <pierre@tablesdata.com>
2018-01-27 18:15:01 -0800

    URL standard fix from module to knowledge base

--- assets/js/WfModule.js
+++ assets/js/WfModule.js
@@ -365,7 +365,7 @@ class WfModule extends React.Component {
             onClick={this.toggleCollapsed} >
           </div>
           <a className='btn help-button d-flex align-items-center'
-             href={'https://intercom.help/tables/modules/' + this.module.id_name}>
+             href={'https://intercom.help/cjworkbench/modules/' + this.module.id_name}>
             <div className='icon-help'></div>
           </a>
           <div>{notesIcon}</div>
