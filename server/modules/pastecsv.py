645d0ac89fe9ea47a50962ccdf09dbda1c307438
jstray <jonathanstray@gmail.com>
2018-01-29 19:31:13 -0800

    Make Paste Data accept TSVs, which enables paste from spreadhseets

--- server/modules/pastecsv.py
+++ server/modules/pastecsv.py
@@ -5,7 +5,7 @@ import io
 
 
 # ---- PasteCSV ----
-# Lets the user paste in text which it interprets as a exce
+# Lets the user paste in text which it interprets as CSV or TSV
 class PasteCSV(ModuleImpl):
 
     def render(wf_module, table):
@@ -20,9 +20,17 @@ class PasteCSV(ModuleImpl):
 
         if (len(tablestr)==0):
             return('Paste data here')
+
+        # Guess at format by counting commas and tabs
+        commas = tablestr.count(',')
+        tabs = tablestr.count('\t')
+        if commas > tabs:
+            sep = ','
+        else:
+            sep = '\t'
+
         try:
-            table = pd.read_csv(io.StringIO(tablestr), header=header_row,
-                                skipinitialspace=True)
+            table = pd.read_table(io.StringIO(tablestr), header=header_row, skipinitialspace=True, sep=sep)
         except CParserError as e:
             return(str(e))
 
--- server/tests/modules/test_pastecsv.py
+++ server/tests/modules/test_pastecsv.py
@@ -1,7 +1,5 @@
-from django.test import TestCase
-from server.models import WfModule
 from server.tests.utils import *
-from server.views.WfModule import make_render_json
+from server.execute import execute_wfmodule
 import pandas as pd
 import io
 
@@ -10,10 +8,26 @@ import io
 class PasteCSVTests(LoggedInTestCase):
     def setUp(self):
         super(PasteCSVTests, self).setUp()  # log in
-        self.workflow = create_testdata_workflow()
-        self.wf_module = WfModule.objects.all().first()
+
+        self.wf_module = load_and_add_module('pastecsv')  # creates workflow too
+        self.csv_pval = get_param_by_id_name('csv')
+
+        self.count_csv = 'Month,Amount\nJan,10\nFeb,5\nMar,10\n'
+        self.count_tsv = 'Month\tAmount\nJan\t10\nFeb\t5\nMar\t10\n'
+
+        self.table = pd.read_csv(io.StringIO(self.count_csv))  # should be same as TSV-derived table
+
+    def test_empty(self):
+        set_string(self.csv_pval, '')
+        out = execute_wfmodule(self.wf_module)
+        self.assertTrue(out.equals(pd.DataFrame()))  # No input, no output
 
     def test_csv(self):
-        response = self.client.get('/api/wfmodules/%d/render' % self.wf_module.id)
-        table = pd.read_csv(io.StringIO(mock_csv_text))
-        self.assertEqual(response.content.decode('utf-8'), make_render_json(table))
+        set_string(self.csv_pval, self.count_csv)
+        out = execute_wfmodule(self.wf_module)
+        self.assertTrue(out.equals(self.table))
+
+    def test_tsv(self):
+        set_string(self.csv_pval, self.count_tsv)
+        out = execute_wfmodule(self.wf_module)
+        self.assertTrue(out.equals(self.table))
