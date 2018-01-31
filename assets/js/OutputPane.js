20cc1d14bb8b81805b44ff0cc2da4d27bae2cb3b
Matthew Gerring <gerring.matthew@gmail.com>
2018-01-30 20:56:45 -0800

    Cross-browser window width function

--- assets/js/OutputPane.js
+++ assets/js/OutputPane.js
@@ -128,6 +128,12 @@ export default class OutputPane extends React.Component {
     }
   }
 
+  getWindowWidth() {
+      return window.innerWidth
+        || document.documentElement.clientWidth
+        || document.body.clientWidth;
+  }
+
   resizePaneStart() {
     this.setState({
        pctBase: this.state.parentBase.clientWidth
