6b35d591983b887371a476e63cbac8a0f4497157
Matthew Gerring <gerring.matthew@gmail.com>
2018-02-02 18:10:42 -0800

    Remove dependency on Chartbuilder stores for updates in UI. Add changeparam action to workflow reducer in advance of redux changes.

--- assets/js/wfparameters/charts/ChartEditor.js
+++ assets/js/wfparameters/charts/ChartEditor.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import ColumnColorPicker from  './ColumnColorPicker'
 import DateScaleSettings from './DateScaleSettings'
+import { errorText } from './errors'
+import {changeParamAction, wfModuleStatusAction, store} from "../../workflow-reducer";
+import debounce from 'lodash/debounce'
+import { OutputIframeCtrl } from '../../OutputIframe'
 
 /* Flux stores */
 var ChartServerActions = require("chartbuilder/src/js/actions/ChartServerActions");
@@ -9,92 +14,187 @@ var ChartMetadataStore = require("chartbuilder/src/js/stores/ChartMetadataStore"
 var SessionStore = require("chartbuilder/src/js/stores/SessionStore");
 var ErrorStore = require("chartbuilder/src/js/stores/ErrorStore");
 
+
+var ChartViewActions = require("chartbuilder/src/js/actions/ChartViewActions");
+var chartConfig = require("chartbuilder/src/js/charts/chart-type-configs");
+
+// Overload the chartbuilder error messages so we can set our own
+var cbErrorText = require("chartbuilder/src/js/util/error-names");
+Object.keys(cbErrorText).map( (key) => {
+  cbErrorText[key].text = errorText[key].text;
+});
+
+// adapter, eventually obsolete with CSV format /input call, or direct edit of ChartBuilder data model
+function JSONtoCSV(d) {
+  if (d && d.length > 0) {
+    var colnames = Object.keys(d[0]).filter(key => key != 'index');
+    var text = colnames.join(',') + '\n';
+    for (var row of d) {
+      text += colnames.map((name) => {return "\"" + row[name] + "\""}).join(',') + '\n';
+    }
+    return text;
+  } else {
+    return '';
+  }
+}
+
+
 export default class ChartEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = this.getStateFromStores();
+    this.state = {
+      loading: true,
+      parsedChartState: null,
+      inputData: null
+    };
     this.onChangeChartSettings = this.onChangeChartSettings.bind(this);
     this.onChangeTitle = this.onChangeTitle.bind(this);
-    this.saveState = this.saveState.bind(this);
-    this.onStateChange = this.onStateChange.bind(this);
-    this.onErrorsChange = this.onErrorsChange.bind(this);
     this.onChangeDate = this.onChangeDate.bind(this);
     this.onChangePrefix = this.onChangePrefix.bind(this);
     this.onChangeSuffix = this.onChangeSuffix.bind(this);
     this.getStateFromStores = this.getStateFromStores.bind(this);
+    this.saveStateToDatabase = debounce(this.saveStateToDatabase, 1000);
+  }
+
+   // Turn ChartBuilder errors into module errors (so user gets the red light etc.)
+  parseErrors(errors) {
+    var errorMessage = errors.messages
+      .filter((m) => {
+        return m.type === 'error';
+      })
+      .map((m) => {
+        return m.text;
+      })
+      .join("\n\r");
+    if (errorMessage !== '') {
+      store.dispatch(wfModuleStatusAction(this.props.wfModuleId, 'error', errorMessage))
+    } else {
+      store.dispatch(wfModuleStatusAction(this.props.wfModuleId, 'ready'))
+    }
+}
+
+  loadChartProps(modelText, data) {
+    let model;
+    let defaults = chartConfig.xy.defaultProps;
+
+    defaults.chartProps.chartSettings[0].type = this.props.type;
+    defaults.chartProps.scale.typeSettings.maxLength = 7;
+
+    if (modelText !== "") {
+      model = JSON.parse(modelText);
+    } else {
+      model = defaults;
+    }
+    model.chartProps.input = {raw: data};
+    return model;
   }
 
   getStateFromStores() {
   	return {
   		chartProps: ChartPropertiesStore.getAll(),
   		metadata: ChartMetadataStore.getAll(),
-  		session: SessionStore.getAll()
+  		session: SessionStore.getAll(),
+      errors: ErrorStore.getAll()
   	};
   }
 
-  onErrorsChange() {
-    this.setState({errors: ErrorStore.getAll()})
+  parseChartState(data) {
+    let newModel = this.loadChartProps(this.props.modelText, data);
+    let parsedModel;
+    // In order to preserve the saved chart settings, we have to set up the initial chart model with the raw input data.
+    // However, if there are errors in the data, receiveModel won't find them. We need to call .updateInput so Chartbuilder's
+    // internal parser will find errors in the data.
+    ChartServerActions.receiveModel(newModel);
+    ChartViewActions.updateInput('input', {raw: data});
+    parsedModel = this.getStateFromStores();
+    if (parsedModel.errors.valid === false) {
+        this.parseErrors(parsedModel.errors);
+        return null;
+    }
+    return parsedModel;
   }
 
-  saveState(model) {
-    ChartServerActions.receiveModel(model);
+  loadChartState() {
+    this.setState({loading: true});
+    this.props.api.input(this.props.wfModuleId).then((json) => {
+      let inputData = JSONtoCSV(json.rows);
+      let parsedChartState = this.parseChartState(inputData);
+      this.setState({model: parsedChartState, inputData, loading: false});
+    })
   }
 
-  onStateChange() {
-    this.setState(this.getStateFromStores());
+  saveState(state) {
+    if (OutputIframeCtrl) {
+      OutputIframeCtrl.postMessage({model: state}, '*');
+    }
+    this.saveStateToDatabase(state);
+  }
+
+  saveStateToDatabase(state) {
+    // Don't save the input data to the database here, since we get it from the previous module
+    // Since we already have a copy we can remove the input data from the original
+    let stateCopy = this.deepCopyState(state);
+    Object.assign(stateCopy.chartProps, {data: undefined, input: undefined});
+    let newStateString = JSON.stringify(state);
+    store.dispatch(changeParamAction(this.props.wfModuleId, 'chart_editor', newStateString));
+  }
+
+  deepCopyState(state) {
+    return JSON.parse(JSON.stringify(state));
   }
 
   componentDidMount() {
-    ChartPropertiesStore.addChangeListener(this.onStateChange);
-    ChartMetadataStore.addChangeListener(this.onStateChange);
-    ErrorStore.addChangeListener(this.onErrorsChange);
-    SessionStore.addChangeListener(this.onStateChange);
+    this.loadChartState();
   }
 
-  componentWillUnmount() {
-		ChartPropertiesStore.removeChangeListener(this.onStateChange);
-		ChartMetadataStore.removeChangeListener(this.onStateChange);
-		ErrorStore.removeChangeListener(this.onErrorsChange);
-		SessionStore.removeChangeListener(this.onStateChange);
-	}
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.revision !== this.props.revision) {
+      this.loadChartState();
+    }
+  }
 
   onChangeChartSettings(state) {
-    let stateCopy = JSON.parse(JSON.stringify(this.state));
+    let stateCopy = this.deepCopyState(this.state.model);
     stateCopy.chartProps.chartSettings = state;
+    this.setState({model: stateCopy});
     this.saveState(stateCopy);
   }
 
   onChangeTitle(e) {
-    let stateCopy = JSON.parse(JSON.stringify(this.state));
+    let stateCopy = this.deepCopyState(this.state.model);
     stateCopy.metadata.title = e.target.value;
+    this.setState({model: stateCopy});
     this.saveState(stateCopy);
   }
 
   onChangePrefix(e) {
-    let stateCopy = JSON.parse(JSON.stringify(this.state));
+    let stateCopy = this.deepCopyState(this.state.model);
     stateCopy.chartProps.scale.primaryScale.prefix = e.target.value;
+    this.setState({model: stateCopy});
     this.saveState(stateCopy);
   }
 
   onChangeSuffix(e) {
-    let stateCopy = JSON.parse(JSON.stringify(this.state));
+    let stateCopy = this.deepCopyState(this.state.model);
     stateCopy.chartProps.scale.primaryScale.suffix = e.target.value;
+    this.setState({model: stateCopy});
     this.saveState(stateCopy);
   }
 
   onChangeDate(uh) {
-    let stateCopy = JSON.parse(JSON.stringify(this.state));
+    let stateCopy = this.deepCopyState(this.state.model);
     stateCopy.chartProps.scale = uh;
+    this.setState({model: stateCopy});
     this.saveState(stateCopy);
   }
 
   render() {
-    if (this.state.errors && this.state.errors.valid) {
+    if (this.state.model && this.state.model.errors && this.state.model.errors.valid) {
       return (
         <div>
           <div className="param-line-margin">
             <ColumnColorPicker
-              series={this.state.chartProps.chartSettings}
+              series={this.state.model.chartProps.chartSettings}
               saveState={this.onChangeChartSettings}/>
           </div>
           <div className="param-line-margin">
@@ -104,7 +204,7 @@ export default class ChartEditor extends React.Component {
             <input
               type="text"
               className="wfmoduleStringInput parameter-base t-d-gray content-2 text-field"
-              value={this.state.metadata.title}
+              value={this.state.model.metadata.title}
               onChange={this.onChangeTitle} />
           </div>
           <div className="paramX-line-margin">
@@ -116,7 +216,7 @@ export default class ChartEditor extends React.Component {
               <input
                 type="text"
                 className="wfmoduleStringInput t-d-gray parameter-base content-2 text-field"
-                value={this.state.chartProps.scale.primaryScale.prefix}
+                value={this.state.model.chartProps.scale.primaryScale.prefix}
                 onChange={this.onChangePrefix} />
             </div>
 
@@ -127,19 +227,12 @@ export default class ChartEditor extends React.Component {
               <input
                 type="text"
                 className="wfmoduleStringInput t-d-gray parameter-base content-2 text-field"
-                value={this.state.chartProps.scale.primaryScale.suffix}
+                value={this.state.model.chartProps.scale.primaryScale.suffix}
                 onChange={this.onChangeSuffix} />
             </div>
 
           </div>
 
-          {this.state.chartProps.scale.hasDate &&
-          <DateScaleSettings
-            scale={this.state.chartProps.scale}
-            nowOffset={this.state.session.nowOffset}
-            now={this.state.session.now}
-            onUpdate={this.onChangeDate}/>
-          }
         </div>
       )
     } else {
@@ -149,3 +242,12 @@ export default class ChartEditor extends React.Component {
     }
   }
 }
+
+ChartEditor.propTypes = {
+  isReadOnly: PropTypes.bool.isRequired,
+  revision:   PropTypes.number.isRequired,
+  wfModuleId: PropTypes.number.isRequired,
+  modelText:  PropTypes.string.isRequired,
+  type:       PropTypes.string.isRequired,
+  api:        PropTypes.object.isRequired
+};
