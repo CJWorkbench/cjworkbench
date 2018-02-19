58881d073e3624b99b8b00fd528ef227473d5c75
jstray <jonathanstray@gmail.com>
2018-02-19 15:33:08 -0800

    Nope, still need this /node_modules/chartbuilder exclusion

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,8 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
+        // chartbuilder and included modules need their jsx compiled, but most node-modules do not
+        exclude: /node_modules(?!([\\]+|\/)(react-tangle|chartbuilder))/,
         loader: 'babel-loader',
         query: {presets: ['es2015', 'react']}  // to transform JSX into JS
       },
