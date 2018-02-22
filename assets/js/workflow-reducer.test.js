519fa2cfb576411bf343e04f6075e11550260265
Matthew Gerring <gerring.matthew@gmail.com>
2018-02-21 18:30:42 -0800

    Test every action type. Fixes a bunch of bugs in the reducer.

--- assets/js/workflow-reducer.test.js
+++ assets/js/workflow-reducer.test.js
@@ -1,15 +1,33 @@
-import {mockAPI, mockStore, deleteModuleAction, setSelectedWfModuleAction} from './workflow-reducer'
+import {
+  workflowReducer
+} from './workflow-reducer'
 
 describe('Reducer actions', () => {
 
-  var store, api;
+  let api, store;
 
   // Stripped down workflow object, only what we need for testing actions
   const test_workflow = {
     id: 999,
     wf_modules: [
       {
-        id: 10
+        id: 10,
+        parameter_vals: [
+          {
+            id: 1,
+            id_name: 'load_data',
+            value: 'Some Data'
+          }
+        ],
+        versions: {
+          selected: "2018-02-21T03:09:20.214054Z",
+          versions: [
+            ["2018-02-21T03:09:20.214054Z", true],
+            ["2018-02-21T03:09:15.214054Z", false],
+            ["2018-02-21T03:09:10.214054Z", false]
+          ]
+        },
+        notification_count: 2
       },
       {
         id: 20
@@ -27,61 +45,215 @@ describe('Reducer actions', () => {
   };
 
   // Stub result to be returned by our stub loadWorkflow
-  const mock_load_workflow_result = { id: 1001 };
+  //const mock_load_workflow_result = { id: 1001 };
+  const mock_add_wf_module_result = { id: 40, insert_before: 2 };
 
-  // Mocks state and api globals in for this module
-  beforeEach(() => {
-    store = {
-      getState: () => test_state,
-      dispatch: jest.fn()
-    };
-    mockStore(store);
-
-    api = {
-      deleteModule: jest.fn().mockReturnValue(Promise.resolve()),   // api call returning nothing
-      loadWorkflow: jest.fn().mockReturnValue(Promise.resolve(mock_load_workflow_result))
-    };
-    mockAPI(api);
+  it('Returns the state if we feed garbage to the reducer', () => {
+    let state = workflowReducer(test_state, {
+      type: 'An ill-advised request',
+      payload: {
+        blob: 'malware.exe'
+      }
+    });
+    expect(state).toBe(test_state);
   });
 
-  // checks that removeModule() is called and selected module is not changed
-  it('Remove non-selected module', (done) => {
+  // ADD_MODULE
+  it('Adds a module', () => {
+    let state = workflowReducer(test_state, {
+      type: 'ADD_MODULE_FULFILLED',
+      payload: mock_add_wf_module_result,
+    });
+    expect(state.workflow.wf_modules[2].id).toEqual(40);
+    expect(state.selected_wf_module).toEqual(40);
+  });
 
-    var actionPromise = deleteModuleAction(30);
+  it('Deletes a module', () => {
+    let state = workflowReducer(test_state, {
+      type: 'DELETE_MODULE_PENDING',
+      payload: {
+        wf_module_id: 20
+      }
+    });
+    expect(state.workflow.wf_modules.length).toEqual(2);
+  });
 
-    // Remove module should have been called
-    expect(api.deleteModule.mock.calls.length).toBe(1);
-    expect(api.deleteModule.mock.calls[0][0]).toBe(30);
+  it('Sets the selected module to a module in state', () => {
+    let state = workflowReducer(test_state, {
+     type: 'SET_SELECTED_MODULE_PENDING',
+     payload: {
+       wf_module_id: 30
+     }
+    });
+    expect(state.selected_wf_module).toBe(30);
+  });
 
-    // actionPromise should eventually return reloadWorkflowAction(), which is a promise that resolves to reloaded wf
-    actionPromise.then( result => {
-      expect(result).toEqual({"type": "RELOAD_WORKFLOW", "workflow": mock_load_workflow_result});
-    }).then( () => {
-      done(); // need to tell jest that we are done with this async test
+  it('Sets the logged in user to the value we specify', () => {
+    let user = {
+      display_name: "Example User",
+      email: "example@example.org",
+      google_credentials: [1],
+      id: 1
+    }
+    let state = workflowReducer(test_state, {
+      type: 'GET_CURRENT_USER_FULFILLED',
+      payload: user
+    });
+    expect(state.loggedInUser).toBe(user);
+  });
+
+  it('Deletes the expected user credential', () => {
+    let user = {
+      display_name: "Example User",
+      email: "example@example.org",
+      google_credentials: [1],
+      id: 1
+    };
+
+    let state = workflowReducer(test_state, {
+      type: 'GET_CURRENT_USER_FULFILLED',
+      payload: user
     });
 
-  });
-
-  // check that removing the selected module selects the one above it
-  it('Remove selected module', (done) => {
-
-    var actionPromise = deleteModuleAction(20); // 20 is selected module, in middle of stack
-
-    // selected module should be set to previous, i.e. 10
-    expect(store.dispatch.mock.calls.length).toBe(1);
-    expect(store.dispatch.mock.calls[0][0]).toEqual(setSelectedWfModuleAction(10));
-
-    // Remove module should have been called
-    expect(api.deleteModule.mock.calls.length).toBe(1);
-    expect(api.deleteModule.mock.calls[0][0]).toBe(20);
-
-    // actionPromise should still resolve to a workflow reload
-    actionPromise.then( result => {
-      expect(result).toEqual({"type": "RELOAD_WORKFLOW", "workflow": mock_load_workflow_result});
-    }).then( () => {
-      done();
+    state = workflowReducer(state, {
+      type: 'DISCONNECT_CURRENT_USER_PENDING',
+      payload: {
+        credential_id: 1
+      }
     });
+
+    expect(state.loggedInUser.google_credentials.length).toEqual(0);
   });
+
+  it('Returns state if given a non-existent user credential', () => {
+    let user = {
+      display_name: "Example User",
+      email: "example@example.org",
+      google_credentials: [1],
+      id: 1
+    };
+
+    let state = workflowReducer(test_state, {
+      type: 'GET_CURRENT_USER_FULFILLED',
+      payload: user
+    });
+
+    let state2 = workflowReducer(state, {
+      type: 'DISCONNECT_CURRENT_USER_PENDING',
+      payload: {
+        credential_id: 2
+      }
+    });
+
+    expect(state2).toBe(state);
+  });
+
+  it('Updates the workflow module with the specified data', () => {
+    let state = workflowReducer(test_state, {
+      type: 'UPDATE_WF_MODULE_PENDING',
+      payload: {
+        id: 20,
+        data: {
+          notifications: true
+        }
+      }
+    });
+    expect(state.workflow.wf_modules[1].notifications).toBe(true);
+  });
+
+  it('Returns the state if we update a nonexistent wfmodule', () => {
+    let state = workflowReducer(test_state, {
+      type: 'UPDATE_WF_MODULE_PENDING',
+      payload: {
+        id: 40,
+        data: {
+          notifications: true
+        }
+      }
+    });
+    expect(state).toBe(test_state);
+  });
+
+  it('Sets the wfModule status', () => {
+    let state = workflowReducer(test_state, {
+      type: 'SET_WF_MODULE_STATUS',
+      payload: {
+        id: 20,
+        status: 'error',
+        error_msg: 'There was an error'
+      }
+    });
+
+    expect(state.workflow.wf_modules[1].status).toBe('error');
+
+    let state2 = workflowReducer(state, {
+      type: 'SET_WF_MODULE_STATUS',
+      payload: {
+        id: 20,
+        status: 'error',
+        error_msg: 'There was another error'
+      }
+    });
+
+    expect(state2.workflow.wf_modules[1].error_msg).toBe('There was another error');
+  });
+
+  it('Sets the module collapse state', () => {
+    let state = workflowReducer(test_state, {
+      type: 'SET_WF_MODULE_COLLAPSED_PENDING',
+      payload: {
+        wf_module_id: 20,
+        is_collapsed: true
+      }
+    });
+    expect(state.workflow.wf_modules[1].is_collapsed).toBe(true);
+  });
+
+  it('Sets the param value', () => {
+    let state = workflowReducer(test_state, {
+      type: 'SET_PARAM_VALUE_PENDING',
+      payload: {
+        wfModuleId: 10,
+        paramId: 1,
+        paramValue: "Other data",
+      }
+    });
+    expect(state.workflow.wf_modules[0].parameter_vals[0].value).toBe("Other data");
+  });
+
+  it('Sets the data version', () => {
+    let state = workflowReducer(test_state, {
+      type: 'SET_DATA_VERSION_PENDING',
+      payload: {
+        wfModuleId: 10,
+        selectedVersion: "2018-02-21T03:09:10.214054Z"
+      }
+    });
+    expect(state.workflow.wf_modules[0].versions.selected).toBe("2018-02-21T03:09:10.214054Z");
+  });
+
+  it('Marks the data versions read', () => {
+    let state = workflowReducer(test_state, {
+      type: 'MARK_DATA_VERSIONS_READ_PENDING',
+      payload: {
+        id: 10,
+        versions_to_update: ["2018-02-21T03:09:15.214054Z", "2018-02-21T03:09:10.214054Z"]
+      }
+    });
+    expect(state.workflow.wf_modules[0].versions.versions[1][1]).toBe(true);
+    expect(state.workflow.wf_modules[0].versions.versions[2][1]).toBe(true);
+  });
+
+  it('Clears the notification count', () => {
+    let state = workflowReducer(test_state, {
+      type: 'CLEAR_NOTIFICATIONS_PENDING',
+      payload: {
+        wfModuleId: 10
+      }
+    });
+    expect(state.workflow.wf_modules[0].notification_count).toBe(0);
+  });
+
 
 });
 
