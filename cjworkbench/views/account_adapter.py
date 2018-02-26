0bc5968d17ee822b375ec721ec49ee337034569f
Matthew Gerring <gerring.matthew@gmail.com>
2018-02-26 14:06:40 -0800

    Deal with existing users attemnpting to log in via a social account

--- cjworkbench/views/account_adapter.py
+++ cjworkbench/views/account_adapter.py
@@ -1,8 +1,16 @@
-from django.core.mail import EmailMultiAlternatives
+from django.core.mail import EmailMultiAlternatives, EmailMessage
 from django.template.loader import render_to_string
 from django.template.exceptions import TemplateDoesNotExist
+from django.shortcuts import redirect
 from cjworkbench import settings
 from allauth.account.adapter import DefaultAccountAdapter
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.socialaccount import app_settings
+from allauth.account import app_settings as account_settings
+from allauth.account.utils import user_email
+from allauth.utils import (
+    email_address_exists,
+)
 
 class WorkbenchAccountAdapter(DefaultAccountAdapter):
     def context_to_subs(self, ctx):
@@ -48,3 +56,25 @@ class WorkbenchAccountAdapter(DefaultAccountAdapter):
         msg.template_id = settings.SENDGRID_TEMPLATE_IDS[template_prefix]
         msg.substitutions = self.context_to_subs(context)
         return msg
+
+# This code is unused for now but may provide a more elegant solution
+# to the duplicate social account email issue down the road.
+class WorkbenchSocialAccountAdapter(DefaultSocialAccountAdapter):
+    def is_auto_signup_allowed(self, request, sociallogin):
+        # If email is specified, check for duplicate and if so, no auto signup.
+        auto_signup = app_settings.AUTO_SIGNUP
+        if auto_signup:
+            email = user_email(sociallogin.user)
+            # Let's check if auto_signup is really possible...
+            if email:
+                if account_settings.UNIQUE_EMAIL:
+                    if email_address_exists(email):
+                        auto_signup = False
+                        # FIXME: We redirect to signup form -- user will
+                        # see email address conflict only after posting
+                        # whereas we detected it here already.
+            elif app_settings.EMAIL_REQUIRED:
+                # Nope, email is required and we don't have it yet...
+                auto_signup = False
+
+        return auto_signup
