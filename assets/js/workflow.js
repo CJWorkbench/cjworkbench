b61f6fee3b8b00f1a4175f4689a8fe057857cd85
Whelpley <whelpley@gmail.com>
2018-02-26 16:12:46 -0800

    Revert previous (erroneous) bug fix

--- assets/js/workflow.js
+++ assets/js/workflow.js
@@ -17,7 +17,7 @@ class Workflow extends React.Component {
         isPublic: false,
         focus: false,
         overlapping: false, // Does the right pane overlap the left pane? Used to set focus, draw shadows, etc
-        libraryOpen: false
+        libraryOpen: false //(this.props.workflow && !this.props.workflow.module_library_collapsed)
     };
     this.setOverlapping = this.setOverlapping.bind(this);
     this.setLibraryOpen = this.setLibraryOpen.bind(this);
@@ -30,7 +30,8 @@ class Workflow extends React.Component {
     }
 
     this.setState({
-      isPublic: nextProps.workflow.public
+      isPublic: nextProps.workflow.public,
+      libraryOpen: (!nextProps.isReadOnly && !nextProps.workflow.module_library_collapsed)
     });
   }
 
