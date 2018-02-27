8eb5b109e33000a1feacc6dd5c98caa54bc41851
Matthew Gerring <gerring.matthew@gmail.com>
2018-02-26 16:29:08 -0800

    Fix integration tests

--- integrationtests/test_login.py
+++ integrationtests/test_login.py
@@ -1,14 +1,34 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.contrib.auth.models import User
+from allauth.account.models import EmailAddress
 from splinter import Browser
+from django.contrib.sites.models import Site
+import time
 
 class TestLogin(StaticLiveServerTestCase):
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+
+        cls.current_site = Site.objects.get_current()
+        cls.SocialApp1 = cls.current_site.socialapp_set.create(
+            provider="facebook",
+            name="Facebook",
+            client_id="1234567890",
+            secret="0987654321",
+        )
+        cls.SocialApp2 = cls.current_site.socialapp_set.create(
+            provider="google",
+            name="Google",
+            client_id="1234567890",
+            secret="0987654321",
+        )
+
         cls.password = 'password' # cannot be retrieved from User object, save it here
         cls.user = User.objects.create_user(username='username', email='user@users.com', password=cls.password)
+        cls.email = EmailAddress.objects.create(user=cls.user, email='user@users.com', primary=True, verified=True)
+
         cls.browser = Browser()
 
     @classmethod
@@ -19,11 +39,11 @@ class TestLogin(StaticLiveServerTestCase):
     def test_login(self):
         b = self.browser
         b.visit(self.live_server_url + '/account/login')
-        b.fill('email', self.user.email)
+        b.fill('login', self.user.email)
         b.fill('password', self.password)
 
         b.find_by_tag('button').click()
-
+        time.sleep(2)
         # if we logged in sucessfully, we should be at an empty Workflows screen
         self.assertTrue(b.url.endswith('/workflows/'))
         self.assertTrue(b.find_by_css('.new-workflow-button'))
--- integrationtests/utils.py
+++ integrationtests/utils.py
@@ -6,6 +6,8 @@ from splinter import Browser
 from server.initmodules import init_modules
 from server.tests.utils import *
 from server.models import ModuleVersion
+from allauth.account.models import EmailAddress
+from django.contrib.sites.models import Site
 
 # Derive from this to perform all tests logged in
 class LoggedInIntegrationTest(StaticLiveServerTestCase):
@@ -16,12 +18,29 @@ class LoggedInIntegrationTest(StaticLiveServerTestCase):
 
         init_modules() # the server should run with a least core modules loaded
 
+        cls.current_site = Site.objects.get_current()
+        cls.SocialApp1 = cls.current_site.socialapp_set.create(
+            provider="facebook",
+            name="Facebook",
+            client_id="1234567890",
+            secret="0987654321",
+        )
+        cls.SocialApp2 = cls.current_site.socialapp_set.create(
+            provider="google",
+            name="Google",
+            client_id="1234567890",
+            secret="0987654321",
+        )
+
         cls.password = 'password'
         cls.user = User.objects.create_user(username='username', email='user@users.com', password=cls.password)
+        # django-allauth uses a seperate model to keep track of verified emails. Without this, we can't log in.
+        cls.email = EmailAddress.objects.create(user=cls.user, email='user@users.com', primary=True, verified=True)
+
 
         b = Browser()
         b.visit(cls.live_server_url + '/account/login')
-        b.fill('email', cls.user.email)
+        b.fill('login', cls.user.email)
         b.fill('password', cls.password)
         b.find_by_tag('button').click()
 
@@ -49,16 +68,32 @@ class DummyWorkflowIntegrationTest(StaticLiveServerTestCase):
     def setUpClass(cls):
         super().setUpClass()
 
+        cls.current_site = Site.objects.get_current()
+        cls.SocialApp1 = cls.current_site.socialapp_set.create(
+            provider="facebook",
+            name="Facebook",
+            client_id="1234567890",
+            secret="0987654321",
+        )
+        cls.SocialApp2 = cls.current_site.socialapp_set.create(
+            provider="google",
+            name="Google",
+            client_id="1234567890",
+            secret="0987654321",
+        )
+
         init_modules() # the server should run with a least core modules loaded
 
         cls.password = 'password'
         cls.user = User.objects.create_user(username='username', email='user@users.com', password=cls.password)
+        # django-allauth uses a seperate model to keep track of verified emails. Without this, we can't log in.
+        cls.email = EmailAddress.objects.create(user=cls.user, email='user@users.com', primary=True, verified=True)
 
         cls.create_test_workflow()
 
         b = Browser()
         b.visit(cls.live_server_url + '/account/login?next=%2fworkflows%2f' + str(cls.wf.id) + '%2f')
-        b.fill('email', cls.user.email)
+        b.fill('login', cls.user.email)
         b.fill('password', cls.password)
         b.find_by_tag('button').click()
 
