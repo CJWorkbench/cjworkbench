c3268e4f776486433cdfbd7728a984b2bd5e15ca
Matthew Gerring <gerring.matthew@gmail.com>
2018-03-02 17:03:35 -0500

    Distinguish between date-only and time-only values. Add operations instead of just counting rows.

--- server/modules/counybydate.py
+++ server/modules/counybydate.py
@@ -1,9 +1,37 @@
 from .moduleimpl import ModuleImpl
 import pandas as pd
-import datetime
+import numpy as np
+from datetime import datetime
+from dateutil.parser import parse
 
 # ---- CountByDate ----
 # group column by unique value, discard all other columns
+
+ALT_DEFAULT_DATETIME = datetime(9999, 7, 1, 3, 17)
+
+def _guess_time_or_date(date):
+    DATE_ONLY = False
+    TIME_ONLY = False
+    today = datetime.now()
+
+    first_pass = parse(date)
+
+    if (first_pass.hour, first_pass.minute) == (0, 0):
+        # This might only have a date
+        second_pass = parse(date, default=ALT_DEFAULT_DATETIME)
+        if (second_pass.hour, second_pass.minute) == (ALT_DEFAULT_DATETIME.hour, ALT_DEFAULT_DATETIME.minute):
+            # This is definitely a date without a time
+            DATE_ONLY = True
+
+    if (first_pass.year, first_pass.month, first_pass.day) == (today.year, today.month, today.day):
+        # This might only have a time
+        second_pass = parse(date, default=ALT_DEFAULT_DATETIME)
+        if (second_pass.year, second_pass.month, second_pass.day) == (ALT_DEFAULT_DATETIME.year, ALT_DEFAULT_DATETIME.month, ALT_DEFAULT_DATETIME.day):
+            # This is definitely a time without a date
+            TIME_ONLY = True
+
+    return (DATE_ONLY, TIME_ONLY)
+
 
 class CountByDate(ModuleImpl):
     # Menu items, must match order in json
@@ -18,10 +46,33 @@ class CountByDate(ModuleImpl):
     QUARTER = 5
     YEAR = 6
 
+    COUNT = 0
+    AVERAGE = 1
+    SUM = 2
+    MIN = 3
+    MAX = 4
+
+
+    TIME_ONLY_COUNT = 0
+    DATE_ONLY_COUNT = 0
+    TIME_ONLY = False
+    DATE_ONLY = False
+
+    @staticmethod
     def render(wf_module, table):
-        col  = wf_module.get_param_column('column')
+        # reset these on every render
+
+        CountByDate.TIME_ONLY_COUNT = 0
+        CountByDate.DATE_ONLY_COUNT = 0
+        CountByDate.TIME_ONLY = False
+        CountByDate.DATE_ONLY = False
+
+        return_table = pd.DataFrame()
+        col = wf_module.get_param_column('column')
+        target = wf_module.get_param_column('targetcolumn')
         sortby = wf_module.get_param_menu_idx('sortby')
         groupby = wf_module.get_param_menu_idx('groupby')
+        operation = wf_module.get_param_menu_idx('operation')
         group_options = [
             "%Y-%m-%d %H:%M:%S",  # Seconds
             "%Y-%m-%d %H:%M",  # Minutes
@@ -30,6 +81,11 @@ class CountByDate(ModuleImpl):
             "%Y-%m",  # Months
             lambda d: "%d Q%d" % (d.year, d.quarter),  # Quarters
             "%Y",  # Years
+        ]
+        time_only_options = [
+            "%H:%M:%S",  # Seconds
+            "%H:%M",  # Minutes
+            "%H:00"  # Hours
         ]
 
         if col == '':
@@ -48,26 +104,86 @@ class CountByDate(ModuleImpl):
 
         # convert the date column to actual datetimes
         try:
-            table[col] = pd.to_datetime(table[col])
+            def inner_func(val):
+                date_only, time_only = _guess_time_or_date(val)
+                if date_only:
+                    CountByDate.DATE_ONLY_COUNT += 1
+                if time_only:
+                    CountByDate.TIME_ONLY_COUNT += 1
+                return pd.to_datetime(val)
+
+            return_table['date'] = table[col].apply(inner_func)
+
+            if CountByDate.DATE_ONLY_COUNT / len(return_table['date']) > .8:
+                CountByDate.DATE_ONLY = True
+
+            if CountByDate.TIME_ONLY_COUNT / len(return_table['date']) > .8:
+                CountByDate.TIME_ONLY = True
+
         except (ValueError, TypeError):
             return('Column %s does not seem to be dates' % col)
 
-        if table[col].dtype == 'int64':
+        # Figure out our groupby options and groupby
+        # behavior based on the input format.
+
+        if return_table['date'].dtype == 'int64':
             return('Column %s does not seem to be dates' % col)
 
-        if groupby is CountByDate.QUARTER:
-            table['groupcol'] = table[col].apply(group_options[groupby])
+        if CountByDate.TIME_ONLY == True:
+            try:
+                return_table['date'] = return_table['date'].dt.strftime(time_only_options[groupby])
+            except IndexError:
+                return 'Column %s only contains time values. Group by Hour, Minute or Second.' % col
+
+        elif CountByDate.DATE_ONLY == True and groupby in [0,1,2]:
+            return 'Column %s only contains date values. Group by Day, Month, Quarter or Year.' % col
+
+        elif groupby is CountByDate.QUARTER:
+            return_table['date'] = return_table['date'].apply(group_options[groupby])
+
         else:
-            table['groupcol'] = table[col].dt.strftime(group_options[groupby])
+            return_table['dates'] = return_table['date'].dt.strftime(group_options[groupby])
 
-        newtab = pd.DataFrame(table.groupby(table['groupcol']).size())
+        # We now have correctly formatted dates for our groupby operation in our new table.
+        # If we're just counting rows, we don't need any more columns.
+        if operation == 0:  # count
+            return_table = pd.DataFrame(return_table.groupby(return_table['date']).size())
+            result_column_name = 'count'
 
-        newtab.reset_index(level=0, inplace=True)  # turn index into a column, or we can't see the column names
-        newtab.columns = ['date', 'count']
+        # If there's a target column:
+        elif target != '':
+            return_table[target] = table[target]
+            result_column_name = target
+            # If the target column isn't numeric, attempt to convert it:
+            if return_table[target].dtype != np.float64 and return_table[target].dtype != np.int64:
+                try:
+                    return_table[target] = table[target].str.replace(',', '')
+                    return_table[target] = return_table[target].astype(float)
+                except ValueError:
+                    return 'Can\'t convert %s to numeric values' % target
+        # If we're trying to do something besides count and there's no target column, we're
+        # still waiting on user input.
+        else:
+            return table
+
+        if operation == 1:  # average
+            return_table = pd.DataFrame(return_table.groupby(return_table['date']).mean())
+
+        if operation == 2:  # sum
+            return_table = pd.DataFrame(return_table.groupby(return_table['date']).sum())
+
+        if operation == 3:  # min
+            return_table = pd.DataFrame(return_table.groupby(return_table['date']).min())
+
+        if operation == 4:  # max
+            return_table = pd.DataFrame(return_table.groupby(return_table['date']).max())
+
+        return_table.reset_index(level=0, inplace=True)  # turn index into a column, or we can't see the column names
+        return_table.columns = ['date', result_column_name]
 
         if sortby != CountByDate.SORT_BY_FREQ:
-            newtab = newtab.sort_values('date')
+            return_table = return_table.sort_values('date')
         else:
-            newtab = newtab.sort_values('count')
+            return_table = return_table.sort_values(result_column_name)
 
-        return newtab
+        return return_table
