881c50331dea1c45ea1491f6bf04992172dcc1c9
jstray <jonathanstray@gmail.com>
2018-03-07 20:21:26 -0500

    Increase SQLite timeout to 200ms to try to kill these locked errors

--- cjworkbench/settings.py
+++ cjworkbench/settings.py
@@ -109,7 +109,7 @@ else:
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
             'OPTIONS': {
-                'timeout': 20
+                'timeout': 200
             }
         }
     }
