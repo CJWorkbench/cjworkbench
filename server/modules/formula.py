ffffb865dcb68b50542d18d2835b3bf794a26f03
Matthew Gerring <gerring.matthew@gmail.com>
2018-04-12 17:31:36 -0700

    wip

--- server/modules/formula.py
+++ server/modules/formula.py
@@ -1,6 +1,7 @@
 from .moduleimpl import ModuleImpl
 from .utils import *
 import pandas as pd
+from formulas import Parser
 
 # ---- Formula ----
 
@@ -23,31 +24,43 @@ class Formula(ModuleImpl):
         if formula == '':
             return table    # nop if no formula
 
-        colnames = [x.replace(" ", "_") for x in table.columns]  # spaces to underscores in column names
-        newcol = pd.Series(np.zeros(len(table)))
+        syntax = wf_module.get_param_menu_idx('syntax')
 
-        # Catch errors with the formula and display to user
-        try:
-            code = compile(formula, '<string>', 'eval')
+        if syntax == 0: # Python
+            colnames = [x.replace(" ", "_") for x in table.columns]  # spaces to underscores in column names
+            newcol = pd.Series(np.zeros(len(table)))
 
-            # Much experimentation went into the form of this loop for good performance.
-            # Note we don't use iterrows or any pandas indexing, and construct the values dict ourselves
-            for i,row in enumerate(table.values):
-                newcol[i] = eval(code, custom_code_globals, dict(zip(colnames, row)))
-        except Exception as e:
-            return(str(e))
+            # Catch errors with the formula and display to user
+            try:
+                code = compile(formula, '<string>', 'eval')
 
-        # if no output column supplied, use result0, result1, etc.
-        out_column = wf_module.get_param_string('out_column')
-        if out_column == '':
-            if 'result' not in colnames:
-                out_column='result'
-            else:
-                n=0
-                while 'result' + str(n) in colnames:
-                    n+=1
-                out_column = 'result' + str(n)
-        table[out_column] = newcol
+                # Much experimentation went into the form of this loop for good performance.
+                # Note we don't use iterrows or any pandas indexing, and construct the values dict ourselves
+                for i,row in enumerate(table.values):
+                    newcol[i] = eval(code, custom_code_globals, dict(zip(colnames, row)))
+            except Exception as e:
+                return(str(e))
+
+            # if no output column supplied, use result0, result1, etc.
+            out_column = wf_module.get_param_string('out_column')
+            if out_column == '':
+                if 'result' not in colnames:
+                    out_column='result'
+                else:
+                    n=0
+                    while 'result' + str(n) in colnames:
+                        n+=1
+                    out_column = 'result' + str(n)
+            table[out_column] = newcol
+
+        if syntax == 1:
+            try:
+                code = Parser().ast(formula)[1].compile()
+            except Exception as e:
+                return "Couldn't parse formula: %s" % str(e)
+            
+            import pdb; pdb.set_trace()
+
 
         wf_module.set_ready(notify=False)
         return table
