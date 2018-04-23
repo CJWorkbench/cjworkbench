58642e0caa89f9b87bcbae323a0d6a9766d70e3f
Tiancheng Zhang <tz2309@columbia.edu>
2018-04-23 12:29:49 -0400

    Resolved database lock-up issue when changing columns.

--- assets/js/SortFromTable.js
+++ assets/js/SortFromTable.js
@@ -9,20 +9,12 @@ export function mockAPI(mock_api) {
   api = mock_api;
 }
 
-function updateSortModule(wfm, info) {
-    console.log(info);
-    var column = info.column;
-    var columnParam = findParamValByIdName(wfm, "column")
-    console.log(columnParam)
-    if(columnParam.value != column) {
-        api.onParamChanged(columnParam.id, {value: column});
-    }
-
-    // Sort direction options: "Select|Ascending|Descending"
+// Wrapper function for changing sort direction, since it needs to be used twice.
+function updateSortDirection(wfm, sortInfo) {
     var direction = 0; // Corresponds to "select"
-    if(info.direction == 'ASC') {
+    if(sortInfo.direction == 'ASC') {
         direction = 1;
-    } else if(info.direction == 'DESC') {
+    } else if(sortInfo.direction == 'DESC') {
         direction = 2
     }
     var directionParam = findParamValByIdName(wfm, "direction");
@@ -32,16 +24,32 @@ function updateSortModule(wfm, info) {
     }
 }
 
+function updateSortModule(wfm, sortInfo) {
+    //console.log(sortInfo);
+    var column = sortInfo.column;
+    var columnParam = findParamValByIdName(wfm, "column")
+    //console.log(columnParam)
+    if(columnParam.value != column) {
+        api.onParamChanged(columnParam.id, {value: column})
+            .then(() => {
+                updateSortDirection(wfm, sortInfo);
+            });
+    } else {
+        updateSortDirection(wfm, sortInfo);
+    }
+}
+
 export function updateSort(wfModuleId, sortInfo) {
     var state = store.getState();
     console.log(sortInfo);
 
     var existingSortModule = findModuleWithIdAndIdName(state, wfModuleId, 'sort-from-table')
     if(existingSortModule) {
-        console.log("Sort module exists.");
+        //console.log("Sort module exists.");
         updateSortModule(existingSortModule, sortInfo);
     } else {
-        console.log("New sort module should be created");
+        //console.log("New sort module should be created");
+
     }
 
     //console.log(state);
