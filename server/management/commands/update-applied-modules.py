48a4550d4d69565080a79014f126b61312189e02
jstray <jonathanstray@gmail.com>
2018-05-18 17:47:05 -0700

    Handle deleted modules correctly

--- server/management/commands/update-applied-modules.py
+++ server/management/commands/update-applied-modules.py
@@ -8,12 +8,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         totes=0
         for wfm in WfModule.objects.all():
-            module = wfm.module_version.module
 
             # update module if not missing and not internal
-            if module != None and module.link != '':
-
-                latest_version = ModuleVersion.objects.filter(module=module).order_by('-last_update_time').first()
+            if wfm.module_version != None and wfm.module_version.module.link != '':
+                latest_version = ModuleVersion.objects.filter(module=wfm.module_version.module).order_by('-last_update_time').first()
                 if wfm.module_version != latest_version:
                     print('Updating module %s from %s to %s' %
                           (str(wfm), wfm.module_version.source_version_hash, latest_version.source_version_hash))
@@ -21,5 +19,7 @@ class Command(BaseCommand):
                     totes += 1
 
         print('Updated %d applied modules to latest version' % totes)
+
+
 
 
