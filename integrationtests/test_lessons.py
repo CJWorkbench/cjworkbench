63aee29de7c260f18b8fe9d1501f492efd2ffede
Adam Hooper <adam@adamhooper.com>
2018-05-23 15:01:28 -0400

    Fix test_lessons.py

--- integrationtests/test_lessons.py
+++ integrationtests/test_lessons.py
@@ -3,20 +3,20 @@ from integrationtests.utils import LoggedInIntegrationTest
 class TestLessons(LoggedInIntegrationTest):
     def test_lesson_list(self):
         b = self.browser
-        b.visit(self.live_server_url + '/lessons/')
-        import time; time.sleep(5)
-        self.assertTrue(b.is_text_present('Load public data and make a chart'))
+        b.visit('/lessons/')
+        b.assert_element(
+            'h2',
+            text='Load public data and make a chart',
+            wait=True
+        )
 
     def test_lesson_detail(self):
         b = self.browser
-        b.visit(self.live_server_url + '/lessons/')
-        b.find_by_xpath('//h2[contains(text(),"Load public data")]').click()
+        b.visit('/lessons/')
+        b.click_whatever('h2', text='Load public data and make a chart', wait=True)
 
-
-
-        self.assertTrue(b.url.endswith('/lessons/load-public-data/'))
-        self.assertTrue(b.is_text_present('DROP MODULE HERE'))
-        self.assertTrue(b.is_text_present('Overview'))
-        self.assertFalse(b.is_text_present('Duplicate'))
-        self.assertFalse(b.is_text_present('Share'))
-        self.assertFalse(b.is_text_present('1. Load Public Data by URL'))
+        b.assert_element('.modulestack-empty', text='DROP MODULE HERE')
+        b.assert_element('h2', text='Overview')
+        b.assert_element('.current-and-total', text='1 of 4')
+        b.click_button('Next')
+        b.assert_element('h2', text='1. Load Public Data by URL')
