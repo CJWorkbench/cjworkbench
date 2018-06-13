b983be517d11cfdab4da977c550d2c4bb4ebb678
Adam Hooper <adam@adamhooper.com>
2018-06-13 14:17:04 -0400

    Fix login link

--- assets/js/WorkflowNavBar.js
+++ assets/js/WorkflowNavBar.js
@@ -196,7 +196,7 @@ export default class WorkflowNavBar extends React.Component {
           user={this.props.loggedInUser}
       />
     } else {
-      contextMenu = <a href="http://app.workbenchdata.com/account/login" className='nav--link t-white content-2'>Sign in</a>
+      contextMenu = <a href="/account/login" className='nav--link t-white content-2'>Sign in</a>
     }
 
     var duplicate = <button onClick={this.handleDuplicate} className='button-white--fill action-button test-duplicate-button'>
