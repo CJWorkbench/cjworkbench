79770dc0cf6fdb61a0d8a824175210689c69e7a2
Adam Hooper <adam@adamhooper.com>
2018-06-18 15:17:14 -0400

    Wait _at most_ 60s in background loop

--- server/management/commands/run-background-loop.py
+++ server/management/commands/run-background-loop.py
@@ -8,11 +8,16 @@ from server.utils import get_console_logger
 _logger = get_console_logger()
 
 
+_MaxDelay = 60 # seconds
+
+
 class Command(BaseCommand):
     help = 'Continually deletes expired anonymous workflows and polls wfmodules for updates'
 
     def handle(self, *args, **options):
         while True:
+            time1 = time.time()
+
             try:
                 update_wfm_data_scan()
             except Exception as err:
@@ -23,4 +28,7 @@ class Command(BaseCommand):
             except Exception as err:
                 _logger.exception(err)
 
-            time.sleep(60)
+            time2 = time.time()
+            duration = time2 - time1
+            delay = max(0, _MaxDelay - duration)
+            time.sleep(delay)
