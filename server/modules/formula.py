f931658832ff48d0c65b303acfcd67037ddde434
jstray <jonathanstray@gmail.com>
2018-06-24 21:29:11 -0700

    Add "apply to all rows" to Excel Formulas module

--- server/modules/formula.py
+++ server/modules/formula.py
@@ -3,7 +3,7 @@ from .utils import *
 import pandas as pd
 from formulas import Parser
 from formulas.errors import FormulaError
-import re
+import re, itertools
 from django.utils.translation import gettext as _
 
 # ---- Formula ----
@@ -23,58 +23,70 @@ def letter_ref_to_number(letter_ref):
 def python_formula(table, formula, newcol):
     colnames = [x.replace(" ", "_") for x in table.columns]  # spaces to underscores in column names
 
-    # Catch errors with the formula and display to user
-    try:
-        code = compile(formula, '<string>', 'eval')
+    code = compile(formula, '<string>', 'eval')
 
-        # Much experimentation went into the form of this loop for good performance.
-        # Note we don't use iterrows or any pandas indexing, and construct the values dict ourselves
-        for i,row in enumerate(table.values):
-            newcol[i] = eval(code, custom_code_globals, dict(zip(colnames, row)))
-    except Exception as e:
-        return str(e)
+    # Much experimentation went into the form of this loop for good performance.
+    # Note we don't use iterrows or any pandas indexing, and construct the values dict ourselves
+    for i,row in enumerate(table.values):
+        newcol[i] = eval(code, custom_code_globals, dict(zip(colnames, row)))
 
     return newcol
 
 
-def excel_formula(table, formula, newcol):
-    try:
-        # 0 is a list of tokens, 1 is the function builder object
-        code = Parser().ast(formula)[1].compile()
-    except Exception as e:
-        return _("Couldn't parse formula: %s" % str(e))
+def eval_excel_one_row(code, table):
 
-    col_idx = []
-
+    # Generate a list of input table values for each range in the expression
+    formula_args = []
     for token, obj in code.inputs.items():
-        # If the formula is valid but no object comes
-        # back it means we should have an uppercase string
+        ranges = obj.ranges
+        if len(ranges) != 1:
+            return _('Excel range must be a rectangular block of values')  # ...how to get here?
+        range = ranges[0]
+
+        # Unpack start/end row/col
+        r1 = int(range['r1'])-1
+        r2 = int(range['r2'])
+        c1 = int(range['n1'])-1
+        c2 = int(range['n2'])
+
+        nrows, ncols = table.shape
+        if r1<0 or r1>=nrows or c1<0 or c1>=ncols:
+            return '#REF!' # expression references non-existent data
+
+        table_part = list(table.iloc[r1:r2,c1:c2].values.flat)
+        formula_args.append(table_part)
+
+    # evaluate the formula just once
+    return code(*formula_args)
+
+
+def eval_excel_all_rows(code, table, newcol):
+    col_idx = []
+    for token, obj in code.inputs.items():
+        # If the formula is valid but no object comes back it means the reference is no good
+        # Missing row number?
         # with only A-Z. But just in case:
         if obj is None:
-            try:
-                to_index = [letter_ref_to_number(token)]
-            except FormulaError as e:
-                # args[1] is the 1st argument to the FormulaError constructor, which is the
-                # thing the parser choked on
-                return e.msg % (e.args[1] or '')
-        else:
-            ranges = obj.ranges
-            to_index = []
-            for rng in ranges:
-                # r1 and r2 refer to which rows are referenced by the range.
-                # cr shows up in the range object whenever the reference is to
-                # an entire row.
-                if (rng['r1'] != '1' and rng['r2'] != '1') and rng['cr'] != '1':
-                    return _(
-                        "Currently only references to entire columns or the first row of a column are supported for excel formulas")
+            raise _('Bad cell reference %s' % token)
 
-                col_first = rng['n1']
-                col_last = rng['n2']
+        ranges = obj.ranges
+        to_index = []
+        for rng in ranges:
+            # r1 and r2 refer to which rows are referenced by the range.
+            # cr shows up in the range object whenever the reference is to
+            # an entire row.
+            if (rng['r1'] != '1' and rng['r2'] != '1') and rng['cr'] != '1':
+                raise _(
+                    "Currently only references to entire columns or the first row of a column are supported for excel formulas")
 
-                if col_first != col_last:
-                    to_index.append([n for n in range((col_first - 1), col_last)])
-                else:
-                    to_index.append(col_first - 1)
+            col_first = rng['n1']
+            col_last = rng['n2']
+
+            if col_first != col_last:
+                to_index.append([n for n in range((col_first - 1), col_last)])
+            else:
+                to_index.append(col_first - 1)
+
         if len(to_index) == 1:
             col_idx.append(to_index[0])
         else:
@@ -83,19 +95,27 @@ def excel_formula(table, formula, newcol):
     for i, row in enumerate(table.values):
         args_to_excel = []
         for col in col_idx:
-            try:
-                if isinstance(col, list):
-                    args_to_excel.append([row[idx] for idx in col])
-                else:
-                    args_to_excel.append(row[col])
-            except IndexError as e:
-                return str(e)
-        try:
-            newcol[i] = code(*args_to_excel)
-        except Exception as e:
-            return str(e)
+            if isinstance(col, list):
+                args_to_excel.append([row[idx] for idx in col])
+            else:
+                args_to_excel.append(row[col])
+        newcol[i] = code(*args_to_excel)
 
     return newcol
+
+
+def excel_formula(table, formula, all_rows, newcol):
+    try:
+        # 0 is a list of tokens, 1 is the function builder object
+        code = Parser().ast(formula)[1].compile()
+    except Exception as e:
+        raise  _("Couldn't parse formula: %s" % str(e))
+
+    if all_rows:
+        return eval_excel_all_rows(code, table, newcol)
+    else:
+        newcol[0] = eval_excel_one_row(code, table)
+        return newcol
 
 
 class Formula(ModuleImpl):
@@ -105,24 +125,26 @@ class Formula(ModuleImpl):
         if table is None:
             return None     # no rows to process
 
+        newcol = pd.Series(list(itertools.repeat(None, len(table))))
+
         syntax = wf_module.get_param_menu_idx('syntax')
         if syntax== 0:
             formula = wf_module.get_param_string('formula_excel').strip()
+            if formula=='':
+                return table
+            all_rows = wf_module.get_param_checkbox('all_rows')
+            try:
+                newcol = excel_formula(table, formula, all_rows, newcol)
+            except Exception as e:
+                return str(e)
         else:
             formula = wf_module.get_param_string('formula_python').strip()
-        if formula == '':
-            return table    # nop if no formula
-
-        newcol = pd.Series(np.zeros(len(table)), dtype=np.dtype(object))
-
-        if syntax == 0:  # Excel
-            newcol = excel_formula(table, formula, newcol)
-
-        if syntax == 1:  # Python
-            newcol = python_formula(table, formula, newcol)
-
-        if isinstance(newcol, str):
-            return newcol
+            if formula=='':
+                return table
+            try:
+                newcol = python_formula(table, formula, newcol)
+            except Exception as e:
+                return str(e)
 
         # if no output column supplied, use result0, result1, etc.
         out_column = wf_module.get_param_string('out_column')
--- server/tests/modules/test_formula.py
+++ server/tests/modules/test_formula.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from server.execute import execute_nocache
 from server.tests.utils import *
 from server.modules.formula import letter_ref_to_number
+from server.sanitizedataframe import sanitize_dataframe
 
 # ---- Formula ----
 mock_csv_text = 'Month,Amount,Amount2,Name\nJan,10,11,Alicia Aliciason\nFeb,666,333,Fred Frederson'
@@ -14,8 +15,11 @@ class FormulaTests(LoggedInTestCase):
 
         self.syntax_pval = get_param_by_id_name('syntax')
         self.excel_pval = get_param_by_id_name('formula_excel')
+        self.all_rows_pval = get_param_by_id_name('all_rows')
         self.python_pval = get_param_by_id_name('formula_python')
-        self.rpval = get_param_by_id_name('out_column')
+        self.outcol_pval = get_param_by_id_name('out_column')
+        self.outcol_pval.value= 'output'
+        self.outcol_pval.save()
 
     def test_python_formula(self):
         # set up a formula to double the Amount column
@@ -23,8 +27,6 @@ class FormulaTests(LoggedInTestCase):
         self.python_pval.save()
         self.syntax_pval.set_value(1)
         self.syntax_pval.save()
-        self.rpval.value= 'output'
-        self.rpval.save()
         table = mock_csv_table.copy()
         table['output'] = table['Amount']*2
         table['output'] = table['output'].astype(object)
@@ -35,8 +37,8 @@ class FormulaTests(LoggedInTestCase):
         self.assertTrue(out.equals(table))
 
         # empty result parameter should produce 'result'
-        self.rpval.set_value('')
-        self.rpval.save()
+        self.outcol_pval.set_value('')
+        self.outcol_pval.save()
         table = mock_csv_table.copy()
         table['result'] = table['Amount']*2
         table['result'] = table['result'].astype(object)
@@ -72,6 +74,7 @@ class FormulaTests(LoggedInTestCase):
         table['formula output'] = table['formula output'].astype(object)
         self.assertTrue(out.equals(table))
 
+
     def test_ref_to_number(self):
         self.assertTrue(letter_ref_to_number('A') == 0)
         self.assertTrue(letter_ref_to_number('AA') == 26)
@@ -79,105 +82,87 @@ class FormulaTests(LoggedInTestCase):
         self.assertTrue(letter_ref_to_number('BA') == 52)
 
 
-    def test_excel_formula(self):
-        # We have custom range handling logic and syntax, so this test exercises many types of ranges
+    def _set_excel_formula(self, formula, all_rows=True):
         self.syntax_pval.set_value(0)
         self.syntax_pval.save()
-        table = mock_csv_table.copy()
-
-        # simple single-column reference
-        self.excel_pval.set_value('=B*2')
+        self.all_rows_pval.set_value(all_rows)
+        self.all_rows_pval.save()
+        self.excel_pval.set_value(formula)
         self.excel_pval.save()
 
-        # empty result parameter should produce 'result'
-        self.rpval.value = ''
-        self.rpval.save()
+
+    def _assert_module_result(self, correct_result, correct_status=WfModule.READY):
+        out = execute_nocache(self.wfmodule)
+        self.wfmodule.refresh_from_db()
+        self.assertEqual(self.wfmodule.status, correct_status)
+        self.assertTrue(out.equals(correct_result))
+
+
+    def test_excel_formula_no_output_col_name(self):
+        # if no output column name specified, store to a column named 'result'
+        self._set_excel_formula('=B1*2', all_rows=True)
+        self.outcol_pval.value = ''
+        self.outcol_pval.save()
+
+        table = mock_csv_table.copy()
         table['result'] = table['Amount'] * 2
         table['result'] = table['result'].astype(object)
-        out = execute_nocache(self.wfmodule)
-        self.wfmodule.refresh_from_db()
-        self.assertEqual(self.wfmodule.status, WfModule.READY)
-        self.assertTrue(out.equals(table))
 
-        # simple single-column reference
-        self.excel_pval.set_value('=B*2')
-        self.excel_pval.save()
+        self._assert_module_result(table)
 
+
+    def test_excel_range_types(self):
+        # We have custom range handling logic and syntax, so this test exercises many types of ranges
+        # All of these tests operate on all rows
         table = mock_csv_table.copy()
-        self.rpval.value = 'output'
-        self.rpval.save()
-
         table['output'] = table['Amount'] * 2
         table['output'] = table['output'].astype(object)
-        out = execute_nocache(self.wfmodule)
-        self.wfmodule.refresh_from_db()
-        self.assertEqual(self.wfmodule.status, WfModule.READY)
-        self.assertTrue(out.equals(table))
 
-        # simple single-column reference
-        self.excel_pval.set_value('=B1*2')
-        self.excel_pval.save()
-        out = execute_nocache(self.wfmodule)
-        self.wfmodule.refresh_from_db()
-        self.assertEqual(self.wfmodule.status, WfModule.READY)
-        self.assertTrue(out.equals(table))
+        # formula: single-column reference
+        self._set_excel_formula('=B1*2', all_rows=True)
+        self._assert_module_result(table)
 
-        # formula with range should grab the right values and compute them
-        self.excel_pval.set_value('=SUM(B:C)')
-        self.excel_pval.save()
+        # formula: horizontal range
+        self._set_excel_formula('=SUM(B1:C1)', all_rows=True)
         table['output'] = table['Amount'] + table['Amount2']
         table['output'] = table['output'].astype(object)
-        out = execute_nocache(self.wfmodule)
-        self.wfmodule.refresh_from_db()
-        self.assertEqual(self.wfmodule.status, WfModule.READY)
-        self.assertTrue(out.equals(table))
+        self._assert_module_result(table)
 
-        # same formula with B1 and C1 should still work
-        self.excel_pval.set_value('=SUM(B1:C1)')
-        self.excel_pval.save()
-        out = execute_nocache(self.wfmodule)
-        self.wfmodule.refresh_from_db()
-        self.assertEqual(self.wfmodule.status, WfModule.READY)
-        self.assertTrue(out.equals(table))
+        # formula: horizontal range, without row numbers
+        self._set_excel_formula('=SUM(B:C)', all_rows=True)
+        self._assert_module_result(table)
 
-        # same formula with B and C1 should still work
-        self.excel_pval.set_value('=SUM(B:C1)')
-        self.excel_pval.save()
-        out = execute_nocache(self.wfmodule)
-        self.wfmodule.refresh_from_db()
-        self.assertEqual(self.wfmodule.status, WfModule.READY)
-        self.assertTrue(out.equals(table))
 
-        # text formula
-        self.excel_pval.set_value('=LEFT(D,5)')
-        self.excel_pval.save()
+    def test_excel_text_formula(self):
+        self._set_excel_formula('=LEFT(D1,5)', all_rows=True)
+        table = mock_csv_table.copy()
         table['output'] = table['Name'].apply(lambda x: x[:5])
         table['output'] = table['output'].astype(object)
-        out = execute_nocache(self.wfmodule)
-        self.wfmodule.refresh_from_db()
-        self.assertEqual(self.wfmodule.status, WfModule.READY)
-        self.assertTrue(out.equals(table))
+        self._assert_module_result(table)
+
+
+    def test_bad_excel_formulas(self):
+        # column without row number
+        self._set_excel_formula('=B*2', all_rows=True)
+        self._assert_module_result(mock_csv_table, WfModule.ERROR)
 
         # bad formula should produce error
-        self.excel_pval.set_value('=SUM B:C')
-        self.excel_pval.save()
-        out = execute_nocache(self.wfmodule)
-        self.wfmodule.refresh_from_db()
-        self.assertEqual(self.wfmodule.status, WfModule.ERROR)
-        self.assertTrue(out.equals(mock_csv_table))  # NOP on error
+        self._set_excel_formula('=SUM B:C', all_rows=True)
+        self._assert_module_result(mock_csv_table, WfModule.ERROR)
 
         # out of range selector should produce error
-        self.excel_pval.set_value('=SUM(B:ZZ)')
-        self.excel_pval.save()
-        out = execute_nocache(self.wfmodule)
-        self.wfmodule.refresh_from_db()
-        self.assertEqual(self.wfmodule.status, WfModule.ERROR)
-        self.assertTrue(out.equals(mock_csv_table))  # NOP on error
+        self._set_excel_formula('=SUM(B:ZZ)', all_rows=True)
+        self._assert_module_result(mock_csv_table, WfModule.ERROR)
 
         # selector with a 0 should produce an error
-        self.excel_pval.set_value('=SUM(B0)')
-        self.excel_pval.save()
-        out = execute_nocache(self.wfmodule)
-        self.wfmodule.refresh_from_db()
-        self.assertEqual(self.wfmodule.status, WfModule.ERROR)
-        self.assertTrue(out.equals(mock_csv_table))  # NOP on error
+        self._set_excel_formula('=SUM(B0)', all_rows=True)
+        self._assert_module_result(mock_csv_table, WfModule.ERROR)
+
+
+    # Formula which writes only to a single row
+    def test_excel_single_row(self):
+        self._set_excel_formula('=SUM(B1:B2)', all_rows=False)
+        table = mock_csv_table.copy()
+        table['output'] = pd.Series([sum(table['Amount']), None], dtype=object)
+        sanitize_dataframe(table)  # force consistent representation of an int in an otherwise empty column
+        self._assert_module_result(table)
