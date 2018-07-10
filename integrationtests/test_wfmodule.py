6fa8b028210a287e4a145d2cb4c85914926bffd4
Adam Hooper <adam@adamhooper.com>
2018-07-09 20:42:54 -0400

    Nix redundant integration test

--- integrationtests/test_wfmodule.py
+++ integrationtests/test_wfmodule.py
@@ -1,33 +1,15 @@
 from integrationtests.utils import LoggedInIntegrationTest
 
-# WfModule expand/collapse, notes, context menu, export, delete
+
 class TestWfModule(LoggedInIntegrationTest):
     def setUp(self):
         super().setUp()
 
         b = self.browser
-        b.click_button('Create Workflow') # navigate to a workflow page
+        b.click_button('Create Workflow')  # navigate to a workflow page
 
         # wait for page load
         b.assert_element('input[name="name"][value="New Workflow"]', wait=True)
-
-
-    def test_paste_csv_workflow(self):
-        self.add_csv_data_module()
-
-        b = self.browser
-
-        b.assert_element('label', text='Has header row')
-
-        # output table with correct values
-        # Wait for the table to load
-        b.assert_element('.react-grid-HeaderCell', text='Month', wait=500)
-        b.assert_element('.react-grid-Cell', text='Jan')
-        b.assert_element('.react-grid-Cell', text='Feb')
-        b.assert_element('.react-grid-HeaderCell', text='Name')
-        b.assert_element('.react-grid-Cell', text='Alicia Aliciason')
-        b.assert_element('.react-grid-Cell', text='Fred Frederson')
-
 
     def test_module_buttons_exist(self):
         b = self.browser
@@ -37,11 +19,10 @@ class TestWfModule(LoggedInIntegrationTest):
         # Wait for wfmodule to appear
         b.hover_over_element('.module-card-header', wait=True)
 
-        b.assert_element('.icon-sort-down') # should be uncollapsed
+        b.assert_element('.icon-sort-down')  # should be uncollapsed
         b.assert_element('.icon-help')
         b.assert_element('.icon-note')
         b.assert_element('button[title=more]')
-
 
     def test_export(self):
         self.add_csv_data_module()
@@ -55,16 +36,12 @@ class TestWfModule(LoggedInIntegrationTest):
         b.assert_element('a[download][href$=json]')
         # TODO actually test the export.
 
-
-    # Delete module test fails because it depends on websockets, and manage.py does not run channels server
-
-    # zzz to ensure this test runs last
     # def test_zzz_delete_module(self):
     #     b = self.browser
     #
-    #     self.assertTrue(b.is_element_present_by_css('.wf-card')) # module is there
+    #     self.assertTrue(b.is_element_present_by_css('.wf-card'))
     #
     #     b.find_by_css('.module-context-menu--icon').click()
     #     b.find_by_text('Delete').first.click()
     #
-    #     self.assertFalse(b.is_element_present_by_css('.wf-card')) # now it's gone
+    #     self.assertFalse(b.is_element_present_by_css('.wf-card'))
