aaed48443c53a855739251d56059104aaf4a4fe6
Adam Hooper <adam@adamhooper.com>
2018-07-23 16:53:55 -0400

    JS style

--- assets/js/wfparameters/charts/ChartColors.js
+++ assets/js/wfparameters/charts/ChartColors.js
@@ -1,12 +1,12 @@
-export const defaultColors =  [
+export const defaultColors = [
   '#E24F4A',
   '#FBAA6D',
   '#48C8D7',
   '#2DAAA8',
   '#769BB0',
-  '#A2A2A2',
-];
+  '#A2A2A2'
+]
 
-export function getColor(idx) {
-  return defaultColors[idx];
+export function getColor (idx) {
+  return defaultColors[idx % defaultColors.length]
 }
