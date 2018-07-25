3a0b74c29ddca528151151d5afc7002df2578dbd
Adam Hooper <adam@adamhooper.com>
2018-07-25 11:54:28 -0400

    Unit-test utf-8 invalid-char replacement
    [finishes #158603478]

--- server/tests/modules/test_utils.py
+++ server/tests/modules/test_utils.py
@@ -1,5 +1,8 @@
-from server.modules.utils import build_globals_for_eval
+import io
 import unittest
+import pandas
+from server.modules.types import ProcessResult
+from server.modules.utils import build_globals_for_eval, parse_bytesio
 
 
 class SafeExecTest(unittest.TestCase):
@@ -14,3 +17,18 @@ class SafeExecTest(unittest.TestCase):
 ret = sorted(list([1, 2, sum([3, 4])]))
 """)
         self.assertEqual(env['ret'], [1, 2, 7])
+
+
+class ParseBytesIoTest(unittest.TestCase):
+    def test_parse_utf8_csv(self):
+        result = parse_bytesio(io.BytesIO(b'A\ncaf\xc3\xa9'),
+                               'text/csv', 'utf-8')
+        expected = ProcessResult(pandas.DataFrame({'A': ['café']}))
+        self.assertEqual(result, expected)
+
+    def test_replace_invalid_utf8(self):
+        # \xe9 is ISO-8859-1 and we select 'utf-8' to test Workbench's recovery
+        result = parse_bytesio(io.BytesIO(b'A\ncaf\xe9'),
+                               'text/csv', 'utf-8')
+        expected = ProcessResult(pandas.DataFrame({'A': ['caf�']}))
+        self.assertEqual(result, expected)
