d361e02494bd3210df5efbd5556ddd493985b7e0
Adam Hooper <adam@adamhooper.com>
2018-08-03 17:46:43 -0400

    refine: use str categories

--- server/modules/refine.py
+++ server/modules/refine.py
@@ -1,114 +1,271 @@
+from typing import Any, Dict, List, Union
 import pandas as pd
 import numpy as np
 from .moduleimpl import ModuleImpl
 from .types import ProcessResult
 import json
-from server.sanitizedataframe import safe_column_to_string
 
 
-# This will perform poorly if many values are not convertible
-def to_numeric(val):
-    try:
-        return pd.to_numeric(val)
-    except:
-        return None
+def _str_categories(series):
+    """Build a Categories series (casting everything to category)."""
+    # 1. Exit quickly when our input is sane
+    if hasattr(series, 'cat'):
+        return series
+
+    # 2. Cast non-str to str
+    if series.dtype != object:
+        t = series.astype(str)
+        t[series.isna()] = np.nan
+        series = t
+
+    # 3. Categorize str
+    return series.astype('category')
 
 
-def to_timestamp(val):
-    try:
-        return pd.Timestamp(val)
-    except ValueError:
-        return None
+class RefineSpec:
+    """Describe how to modify a column.
 
+    This is a set of instructions:
 
-select_colname = 'temp_backend_selected'
+    1. `renames` renames every key to its value, _not_ recursively.
+    2. If `blacklist` is non-empty, the results from step 2 are filtered so
+       blacklisted values are missing.
+    """
 
+    def __init__(self, renames: Dict[str, str]={}, blacklist: List[str]=[]):
+        self.renames = renames
+        self.blacklist = blacklist
 
-def do_edit(table, colname, from_val, to_val):
-    # Performs an edit on the table in place.
-    # If the "to" facet exists, update the selection status of the "from" facet
-    # to that of the "to" facet.
-    if to_val in table[colname].tolist():
-        to_val_selected = table.loc[table[colname] == to_val, select_colname] \
-                .tolist()[0]
-        table.loc[table[colname] == from_val, select_colname] = to_val_selected
-    table.loc[table[colname] == from_val, colname] = to_val
+    def apply_renames(self, series: pd.Series) -> pd.Series:
+        """Build a series with changed categories."""
+        # 1. Set new categories
+        # 2. Copy all values that are in old _and_ new category list, leaving
+        #    the rest as NA
+        # (this is a one-liner in pandas)
+        new_categories = set(series.cat.categories) \
+            .difference(self.renames.keys()) \
+            .union(self.renames.values())
 
+        # Sort categories, simply to make unit tests pass. (They can't predict
+        # category order otherwise, and assert_frame_equal() treats different
+        # orders as different data frames.)
+        #
+        # This may be slow in some datasets.
+        new_categories = sorted(new_categories)
 
-def do_select(table, colname, val):
-    # Performs a selection on the table in place.
-    table.loc[table[colname] == val, select_colname] \
-            = (~table.loc[table[colname] == val, select_colname])
+        ret = series.cat.set_categories(new_categories)
 
+        # 3. "Rename" by setting old-valued data to new values.
+        #
+        # This algorithm _should_ be fairly quick, but it hasn't been
+        # benchmarked. For every "group" we're renaming _to_, select matching
+        # _from_ rows from the original series and set their value.
+        #
+        # This amounts to G renames, each costing O(N). Total O(NG).
+        grouped_renames = {}
+        for k, v in self.renames.items():
+            try:
+                grouped_renames[v].append(k)
+            except KeyError:
+                grouped_renames[v] = [k]
 
-def apply_edit(table, edit):
-    colname = edit['column']
-    ctype = table[colname].dtype
+        for new_value, old_values in grouped_renames.items():
+            ret[series.isin(old_values)] = new_value
 
-    if edit['type'] == 'change':
-        from_val_raw = edit['content']['fromVal']
-        to_val_raw = edit['content']['toVal']
-        if ctype == np.int64 or ctype == np.float64:
-            from_val = to_numeric(from_val_raw)
-            to_val = to_numeric(to_val_raw)
-            if (from_val is not None) and (to_val is not None):
-                do_edit(table, colname, from_val, to_val)
-            else:
-                table[colname] = safe_column_to_string(table[colname])
-                do_edit(table, colname, from_val, to_val)
-        elif ctype == 'datetime64[ns]':
-            from_val = to_timestamp(from_val_raw)
-            to_val = to_timestamp(to_val_raw)
-            if (from_val is not None) and (to_val is not None):
-                do_edit(table, colname, from_val, to_val)
-            else:
-                table[colname] = safe_column_to_string(table[colname])
-                do_edit(table, colname, from_val_raw, to_val_raw)
+        return ret
+
+    def filter_table(self, table: pd.DataFrame,
+                     series: pd.Series) -> pd.DataFrame:
+        """
+        Filter dataframe by blacklist.
+
+        Arguments:
+
+        table -- table to filter
+        series -- already-renamed categories series
+        """
+        if self.blacklist:
+            mask = ~series.isin(self.blacklist)
+            table[series.name] = series.cat.remove_categories(self.blacklist)
+            return table[mask]
         else:
-            do_edit(table, colname, from_val_raw, to_val_raw)
-    elif edit['type'] == 'select':
-        val_raw = edit['content']['value']
-        if ctype == np.int64 or ctype == np.float64:
-            val = to_numeric(val_raw)
-            if val is not None:
-                do_select(table, colname, val)
+            # common case
+            return table
+
+    def apply(self, table, column):
+        # 1. Turn into categories
+        series = _str_categories(table[column])
+
+        # 2. Rename all the categories
+        series = self.apply_renames(series)
+        table[column] = series
+
+        # 3. Filter
+        return self.filter_table(table, series)
+
+    @staticmethod
+    def parse_v0(column: str, arr: List[Dict[str, Any]]) -> 'RefineSpec':
+        """
+        Parse deprecated refine parameters.
+
+        This deprecated format was an Array of instructions which were applied
+        in sequence. That made for complex interactions: renaming A to B and
+        then B to C would result in A being renamed to C; but renaming B to C
+        and then A to B would not. There was no blacklist: there was a "select"
+        operation, which toggles: the first appearance of a value meant
+        "blacklist" and the second appearance meant "whitelist". Order
+        mattered: [toggle A; rename A to B; toggle B] would wouldadvance
+
+        Input looks like this:
+
+        [
+            {'type': 'select', 'column': 'A', 'content': {'value': 'x'}},
+            {'type': 'change', 'column': 'A',
+             'content': {'fromVal': 'x', 'toVal': 'y'}},
+        ]
+
+        Rules:
+
+        * Ignore anything with the wrong 'column'
+        * 'select' toggles a value onto and then off the blacklist
+        * 'change' edits values -- and the blacklist applies to the new value,
+          not the old one
+        """
+        renames = {}
+        blacklist = set()
+
+        for item in arr:
+            if not isinstance(item, dict):
+                raise ValueError('Not a dict')
+
+            try:
+                item_column = str(item['column'])
+            except KeyError:
+                raise ValueError('Change is missing "column" key')
+            if item_column != column:
+                continue
+
+            try:
+                item_type = str(item['type'])
+            except KeyError:
+                raise ValueError('Change is missing "type" key')
+
+            try:
+                # raise ValueError on non-dict
+                item_content = dict(item['content'])
+            except KeyError:
+                raise ValueError('Change is missing "content" key')
+
+            if item_type == 'change':
+                try:
+                    from_val = str(item_content['fromVal'])
+                    to_val = str(item_content['toVal'])
+                except KeyError:
+                    raise ValueError('Missing "content.fromVal" or '
+                                     '"content.toVal"')
+
+                renames2 = dict(renames)  # shallow copy
+                # Edit every previous rename (x => y, y => z becomes x => z)
+                for k, v in renames.items():
+                    if v == from_val:
+                        if k == to_val:
+                            del renames2[k]
+                        else:
+                            renames2[k] = to_val
+                # Then include the user's rename
+                renames2[from_val] = to_val
+
+                renames = renames2
+
+                # Bug: this sequence:
+                # 1. Toggle 'A' into blacklist
+                # 2. Rename 'A' to 'B', where 'B' is not in the blacklist
+                # Expected results: is 'B' blacklisted? Before, it would depend
+                # on the table: if 'B' was present in the table and not
+                # blacklisted, no. If 'B' was missing before the rename, then
+                # yes.
+                #
+                # We don't look at the table any more, so we can't
+                # differentiate. The best approach is probably, 'if B is
+                # blacklisted, keep it blacklisted; otherwise don't.'
+                blacklist.discard(from_val)  # adopt to_val's state
+
+            elif item_type == 'select':
+                try:
+                    value = str(item_content['value'])
+                except KeyError:
+                    raise ValueError('Missing "content.value"')
+
+                # Toggle
+                try:
+                    blacklist.remove(value)
+                except KeyError:
+                    blacklist.add(value)
+
             else:
-                table[colname] = safe_column_to_string(table[colname])
-                do_select(table, colname, val_raw)
-        elif ctype == 'datetime64[ns]':
-            val = to_timestamp(val_raw)
-            if val is not None:
-                do_select(table, colname, val)
-            else:
-                table[colname] = safe_column_to_string(table[colname])
-                do_select(table, colname, val_raw)
+                raise ValueError(f'Invalid "type": {item_type}')
+
+        return RefineSpec(renames, list(blacklist))
+
+    @staticmethod
+    def parse_v1(edits: Dict[str, Any]) -> 'RefineSpec':
+        """Parse a RefineSpec from JSON-decoded data."""
+        try:
+            renames = edits['renames']
+        except KeyError:
+            raise ValueError('Missing "renames"')
+
+        try:
+            blacklist = edits['blacklist']
+        except KeyError:
+            raise ValueError('Missing "blacklist"')
+
+        try:
+            renames = dict([(str(k), str(v)) for k, v in renames.items()])
+        except AttributeError:
+            raise ValueError('"renames" must be a dict from old value to new')
+
+        try:
+            blacklist = list([str(v) for v in blacklist])
+        except TypeError:
+            raise ValueError('"blacklist" must be a list of values')
+
+        return RefineSpec(renames, blacklist)
+
+    @staticmethod
+    def parse(column: str, edits: Union[Dict[str, Any],
+                                        List[Dict[str, Any]]]) -> 'RefineSpec':
+        """
+        Parse a RefineSpec from JSON-decoded data.
+
+        If 'edits' is a List, then it's a v0 spec and we'll use `parse_v0()`.
+        """
+        if isinstance(edits, list):
+            return RefineSpec.parse_v0(column, edits)
         else:
-            do_select(table, colname, val_raw)
+            return RefineSpec.parse_v1(edits)
 
 
 class Refine(ModuleImpl):
     def render(wf_module, table):
         # 'refine' holds the edits
         edits_json = wf_module.get_param_raw('refine', 'custom')
-        column_param = wf_module.get_param_column('column')
-        if edits_json == '':
+        column = wf_module.get_param_column('column')
+
+        if not edits_json or not column:
+            # No user input yet
             return ProcessResult(table)
 
-        # Add a hidden column for facet selection tracking
-        # TODO don't.
-        table[select_colname] = [True for _ in range(len(table.index))]
+        try:
+            edits = json.loads(edits_json)
+        except ValueError:
+            return ProcessResult(error='Internal error: Invalid JSON')
 
-        edits = json.loads(edits_json)
-        for edit in edits:
-            if (edit['column'] != column_param) \
-               or (edit['column'] not in table.columns):
-                continue
+        try:
+            spec = RefineSpec.parse(column, edits)
+        except ValueError as err:
+            return ProcessResult(error=f'Internal error: {str(err)}')
 
-            apply_edit(table, edit)
-
-        # Keep only rows where the hidden column has value True
-        table = table[table[select_colname]]
-        # Drop the hidden column
-        table = table.drop(select_colname, 1)
+        table = spec.apply(table, column)
 
         return ProcessResult(table)
