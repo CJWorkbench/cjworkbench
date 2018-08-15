8f6763f56ab49c485667b7a561ed5ee1ddb39332
Adam Hooper <adam@adamhooper.com>
2018-08-15 11:38:19 -0400

    Rewrite sortfromtable tests

--- server/tests/modules/test_sortfromtable.py
+++ server/tests/modules/test_sortfromtable.py
@@ -1,20 +1,18 @@
-from server.tests.utils import LoggedInTestCase, create_testdata_workflow, \
-        load_and_add_module, get_param_by_id_name
-from server.execute import execute_nocache
-from server.modules.types import ProcessResult
+import datetime
+import unittest
 import pandas as pd
 import numpy as np
-import io
 from server.modules.sortfromtable import SortFromTable
+from server.modules.types import ProcessResult
 
 
-class MockModule:
+class MockWfModule:
     # A mock module that stores parameter data,
     # for directly testing a module's render() function
     # We can move this into test utils if it's more widely applicable
 
-    def __init__(self, params):
-        self.params = params
+    def __init__(self, **kwargs):
+        self.params = kwargs
 
     def get_param_raw(self, name, ptype):
         if name in self.params:
@@ -32,170 +30,97 @@ class MockModule:
         return ''
 
 
-test_csv = '\n'.join([
-    'name,date,count,float'
-    'Dolores,2018-04-22,3,13.5'
-    'Bernard,2018-04-23,not_a_count,2.0'
-    'Ford,2016-10-02,5,'
-    'Dolores,0_not_a_date,4,2.8'
-])
-reference_table = pd.read_csv(io.StringIO(test_csv), dtype={
-    'name': object,
-    'date': object,
-    'count': object,
-    'float': np.float64
-})
-
-
-def ordered_result(row_numbers, table=reference_table):
-    pieces = [table[i:i+1] for i in row_numbers]
-    return ProcessResult(pd.concat(pieces))
-
-
-class SortFromTableTests(LoggedInTestCase):
+class SortFromTableTests(unittest.TestCase):
     # Current dtype choices: "String|Number|Date"
     # Current direction choices: "Select|Ascending|Descending"
     # If the position of the values change, tests will need to be updated
 
     # NaN and NaT always appear last as the policy in SortFromTable dictates
 
-    def setUp(self):
-        super(SortFromTableTests, self).setUp()
-        # A reference table for correctness checking
-        # Performing date conversion here does not help tests as default test
-        # WF does not parse dates
-        self.table = reference_table.copy()
-        self.dates_table = self.table.copy()
-        self.dates_table['date'] = pd.to_datetime(self.dates_table['date'],
-                                                  errors="coerce")
-        self.workflow = create_testdata_workflow(csv_text=test_csv)
-        self.wf_module = load_and_add_module('sort', workflow=self.workflow)
-        self.column_pval = get_param_by_id_name('column')
-        self.dtype_pval = get_param_by_id_name('dtype')
-        self.direction_pval = get_param_by_id_name('direction')
+    def test_order_missing_direction(self):
+        table = pd.DataFrame({'A': ['a', 'c', 'b'], 'B': [1, 2, 3]})
+        # no-op because no direction is specified
+        # TODO nix the very _possibility_ of no direction. Why would anybody
+        # ever want to sort by no direction?
+        wf_module = MockWfModule(column='A', dtype=0, direction=0)
+        result = SortFromTable.render(wf_module, table)
+        expected = ProcessResult(
+            pd.DataFrame({'A': ['a', 'c', 'b'], 'B': [1, 2, 3]})
+        )
+        self.assertEqual(result, expected)
 
-    def test_str_ordering(self):
-        # Tests ordering of a string column as strings
-        self.column_pval.value = 'name'
-        self.column_pval.save()
-        # dtype is string
-        self.dtype_pval.value = 0
-        self.dtype_pval.save()
+    def test_order_str_as_str_ascending(self):
+        table = pd.DataFrame({'A': ['a', 'c', 'b'], 'B': [1, 2, 3]})
+        wf_module = MockWfModule(column='A', dtype=0, direction=1)
+        result = SortFromTable.render(wf_module, table)
+        expected = ProcessResult(
+            pd.DataFrame({'A': ['a', 'b', 'c'], 'B': [1, 3, 2]})
+        )
+        self.assertEqual(result, expected)
 
-        # If direction is "Select", NOP
-        self.direction_pval.value = 0
-        self.direction_pval.save()
-        result = execute_nocache(self.wf_module)
-        self.assertEqual(result, ordered_result([0, 1, 2, 3]))
+    def test_order_cat_str_as_str_ascending(self):
+        table = pd.DataFrame({'A': ['a', 'c', 'b'], 'B': [1, 2, 3]})
+        table['A'] = table['A'].astype('category')
+        wf_module = MockWfModule(column='A', dtype=0, direction=1)
+        result = SortFromTable.render(wf_module, table)
+        expected = ProcessResult(
+            pd.DataFrame({'A': ['a', 'b', 'c'], 'B': [1, 3, 2]})
+        )
+        expected.dataframe['A'] = expected.dataframe['A'].astype('category')
+        self.assertEqual(result, expected)
 
-        # If direction is "Ascending"
-        self.direction_pval.value = 1
-        self.direction_pval.save()
-        result = execute_nocache(self.wf_module)
-        self.assertEqual(result, ordered_result([1, 0, 3, 2]))
+    def test_order_str_as_str_descending(self):
+        table = pd.DataFrame({'A': ['a', 'c', 'b'], 'B': [1, 2, 3]})
+        wf_module = MockWfModule(column='A', dtype=0, direction=2)
+        result = SortFromTable.render(wf_module, table)
+        expected = ProcessResult(
+            pd.DataFrame({'A': ['c', 'b', 'a'], 'B': [2, 3, 1]})
+        )
+        self.assertEqual(result, expected)
 
-        # If direction is "Descending"
-        self.direction_pval.value = 2
-        self.direction_pval.save()
-        result = execute_nocache(self.wf_module)
-        self.assertEqual(result, ordered_result([2, 0, 3, 1]))
+    def test_order_number_as_str(self):
+        table = pd.DataFrame({'A': ['a', 'b', 'c'], 'B': [1, 3, 22]})
+        wf_module = MockWfModule(column='B', dtype=0, direction=1)
+        result = SortFromTable.render(wf_module, table)
+        expected = ProcessResult(
+            pd.DataFrame({'A': ['a', 'c', 'b'], 'B': [1, 22, 3]})
+        )
+        self.assertEqual(result, expected)
 
-        # Tests ordering of a numeric column as strings
-        self.column_pval.value = 'float'
-        self.column_pval.save()
-        # dtype is string
-        self.dtype_pval.value = 0
-        self.dtype_pval.save()
-        # We only test Ascending here; others have been covered above
-        self.direction_pval.value = 1
-        self.direction_pval.save()
-        result = execute_nocache(self.wf_module)
-        self.assertEqual(result, ordered_result([0, 1, 3, 2]))
+    def test_order_number_as_number_ascending(self):
+        table = pd.DataFrame({'A': [3.0, np.nan, 2.1], 'B': ['a', 'b', 'c']})
+        wf_module = MockWfModule(column='A', dtype=1, direction=1)
+        result = SortFromTable.render(wf_module, table)
+        expected = ProcessResult(
+            pd.DataFrame({'A': [2.1, 3.0, np.nan], 'B': ['c', 'a', 'b']})
+        )
+        self.assertEqual(result, expected)
 
-        # Test ordering of a date column as string,
-        # using SortFromTable's render() directly
-        mock_sort = MockModule({
-            'column': 'date',
-            'dtype': 0,
-            # We only test Ascending here; others have been covered above
-            'direction': 1
-        })
-        result = SortFromTable.render(mock_sort, self.dates_table.copy())
-        self.assertEqual(result,
-                         ordered_result([2, 0, 1, 3], self.dates_table))
+    def test_order_number_as_number_descending(self):
+        table = pd.DataFrame({'A': [3.0, np.nan, 2.1], 'B': ['a', 'b', 'c']})
+        wf_module = MockWfModule(column='A', dtype=1, direction=2)
+        result = SortFromTable.render(wf_module, table)
+        expected = ProcessResult(
+            pd.DataFrame({'A': [3.0, 2.1, np.nan], 'B': ['a', 'c', 'b']})
+        )
+        self.assertEqual(result, expected)
 
-    def test_numeric_ordering(self):
-        # Test ordering of a numeric column as numeric
-        self.column_pval.value = 'float'
-        self.column_pval.save()
-        # dtype is number
-        self.dtype_pval.value = 1
-        self.dtype_pval.save()
+    def test_order_str_as_number(self):
+        table = pd.DataFrame({'A': ['30', '..nan', '4'], 'B': ['a', 'b', 'c']})
+        wf_module = MockWfModule(column='A', dtype=1, direction=1)
+        result = SortFromTable.render(wf_module, table)
+        expected = ProcessResult(
+            pd.DataFrame({'A': ['4', '30', '..nan'], 'B': ['c', 'a', 'b']})
+        )
+        self.assertEqual(result, expected)
 
-        # If direction is "Select", NOP
-        self.direction_pval.value = 0
-        self.direction_pval.save()
-        result = execute_nocache(self.wf_module)
-        self.assertEqual(result, ordered_result([0, 1, 2, 3]))
-
-        # If direction is "Ascending"
-        self.direction_pval.value = 1
-        self.direction_pval.save()
-        result = execute_nocache(self.wf_module)
-        self.assertEqual(result, ordered_result([1, 3, 0, 2]))
-
-        # If direction is "Descending"
-        self.direction_pval.value = 2
-        self.direction_pval.save()
-        result = execute_nocache(self.wf_module)
-        self.assertEqual(result, ordered_result([0, 3, 1, 2]))
-
-        # Test ordering of a string column as numeric
-        self.column_pval.value = 'count'
-        self.column_pval.save()
-        # dtype is number
-        self.dtype_pval.value = 1
-        self.dtype_pval.save()
-        # We only test Ascending here; others have been covered above
-        self.direction_pval.value = 1
-        self.direction_pval.save()
-        result = execute_nocache(self.wf_module)
-        self.assertEqual(result, ordered_result([0, 3, 2, 1]))
-
-    def test_date_ordering(self):
-        # Test ordering of a string column as date
-        self.column_pval.value = 'date'
-        self.column_pval.save()
-        # dtype is date
-        self.dtype_pval.value = 2
-        self.dtype_pval.save()
-
-        # If direction is "Select", NOP
-        self.direction_pval.value = 0
-        self.direction_pval.save()
-        result = execute_nocache(self.wf_module)
-        self.assertEqual(result, ordered_result([0, 1, 2, 3]))
-
-        # If direction is "Ascending"
-        self.direction_pval.value = 1
-        self.direction_pval.save()
-        result = execute_nocache(self.wf_module)
-        self.assertEqual(result, ordered_result([2, 0, 1, 3]))
-
-        # If direction is "Descending"
-        self.direction_pval.value = 2
-        self.direction_pval.save()
-        result = execute_nocache(self.wf_module)
-        self.assertEqual(result, ordered_result([1, 0, 2, 3]))
-
-        # Test ordering of a date column as date,
-        # using SortFromTable's render() directly
-        mock_sort = MockModule({
-            'column': 'date',
-            'dtype': 2,
-            # We only test Ascending here; others have been covered above
-            'direction': 1
-        })
-        result = SortFromTable.render(mock_sort, self.dates_table.copy())
-        self.assertEqual(result,
-                         ordered_result([2, 0, 1, 3], self.dates_table))
+    def test_order_date(self):
+        d1 = datetime.datetime(2018, 8, 15, 1, 23, 45)
+        d2 = datetime.datetime(2018, 8, 15, 1, 34, 56)
+        table = pd.DataFrame({'A': [d2, d1], 'B': ['a', 'b']})
+        wf_module = MockWfModule(column='A', dtype=2, direction=1)
+        result = SortFromTable.render(wf_module, table)
+        expected = ProcessResult(
+            pd.DataFrame({'A': [d1, d2], 'B': ['b', 'a']})
+        )
+        self.assertEqual(result, expected)
