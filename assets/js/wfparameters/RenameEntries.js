fc35377d2ef906976715a1f7b517cb2362637861
Adam Hooper <adam@adamhooper.com>
2018-08-20 18:08:27 -0400

    JS style

--- assets/js/wfparameters/RenameEntries.js
+++ assets/js/wfparameters/RenameEntries.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {store, deleteModuleAction} from "../workflow-reducer";
+import {store, deleteModuleAction} from "../workflow-reducer"
 
 function parseJsonOrEmpty(s) {
   if (s) {
@@ -15,177 +15,177 @@ function isEmptyJson(s) {
 }
 
 export class RenameEntry extends React.Component {
-    static propTypes = {
-        colname: PropTypes.string.isRequired,
-        newColname: PropTypes.string.isRequired,
-        onColRename: PropTypes.func.isRequired,
-        onEntryDelete: PropTypes.func.isRequired,
-        isReadOnly: PropTypes.bool.isRequired
-    };
+  static propTypes = {
+    colname: PropTypes.string.isRequired,
+    newColname: PropTypes.string.isRequired,
+    onColRename: PropTypes.func.isRequired,
+    onEntryDelete: PropTypes.func.isRequired,
+    isReadOnly: PropTypes.bool.isRequired
+  }
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            inputValue: this.props.newColname
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleKeyPress = this.handleKeyPress.bind(this);
-        this.handleBlur = this.handleBlur.bind(this);
-        this.handleFocus = this.handleFocus.bind(this);
-        this.handleDelete = this.handleDelete.bind(this);
+    this.state = {
+      inputValue: this.props.newColname
     }
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.newColname != this.state.inputValue) {
-            this.setState({inputValue: nextProps.newColname});
-        }
-    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+    this.handleBlur = this.handleBlur.bind(this)
+    this.handleFocus = this.handleFocus.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
+  }
 
-    handleChange(event) {
-        //this.props.onColRename(this.props.colname, event.target.value);
-        this.setState({inputValue: event.target.value});
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.newColname != this.state.inputValue) {
+      this.setState({inputValue: nextProps.newColname})
     }
+  }
 
-    handleBlur() {
-        if(this.state.inputValue != this.props.newColname) {
-            this.props.onColRename(this.props.colname, this.state.inputValue);
-        }
-    }
+  handleChange(event) {
+    //this.props.onColRename(this.props.colname, event.target.value)
+    this.setState({inputValue: event.target.value})
+  }
 
-    handleKeyPress(event) {
-        if((event.key == 'Enter') && (this.state.inputValue != this.props.newColname)) {
-            this.props.onColRename(this.props.colname, this.state.inputValue);
-        }
+  handleBlur() {
+    if(this.state.inputValue != this.props.newColname) {
+      this.props.onColRename(this.props.colname, this.state.inputValue)
     }
+  }
 
-    handleFocus(event) {
-        event.target.select();
+  handleKeyPress(event) {
+    if((event.key == 'Enter') && (this.state.inputValue != this.props.newColname)) {
+      this.props.onColRename(this.props.colname, this.state.inputValue)
     }
+  }
 
-    handleDelete() {
-        this.props.onEntryDelete(this.props.colname);
-    }
+  handleFocus(event) {
+    event.target.select()
+  }
 
-    render() {
-        // The class names below are used in testing.
-        // Changing them would require updating the tests accordingly.
-        return (
-            <div className="wf-parameter rename-entry" data-column-name={this.props.colname}>
-                <div className={'rename-column'}>{this.props.colname}</div>
-                <div className="rename-container">
-                  <input
-                      className={'rename-input'}
-                      type={'text'}
-                      value={this.state.inputValue}
-                      onChange={this.handleChange}
-                      onBlur={this.handleBlur}
-                      onKeyPress={this.handleKeyPress}
-                      onFocus={this.handleFocus}
-                      disabled={this.props.isReadOnly}
-                  />
-                  <button
-                      className={'rename-delete icon-close'}
-                      onClick={this.handleDelete}
-                      disabled={this.props.isReadOnly}
-                  ></button>
-                </div>
-            </div>
-        )
-    }
+  handleDelete() {
+    this.props.onEntryDelete(this.props.colname)
+  }
+
+  render() {
+    // The class names below are used in testing.
+    // Changing them would require updating the tests accordingly.
+    return (
+      <div className="wf-parameter rename-entry" data-column-name={this.props.colname}>
+      <div className={'rename-column'}>{this.props.colname}</div>
+      <div className="rename-container">
+      <input
+      className={'rename-input'}
+      type={'text'}
+      value={this.state.inputValue}
+      onChange={this.handleChange}
+      onBlur={this.handleBlur}
+      onKeyPress={this.handleKeyPress}
+      onFocus={this.handleFocus}
+      disabled={this.props.isReadOnly}
+      />
+      <button
+      className={'rename-delete icon-close'}
+      onClick={this.handleDelete}
+      disabled={this.props.isReadOnly}
+      ></button>
+      </div>
+      </div>
+    )
+  }
 }
 
 export default class RenameEntries extends React.Component {
-    static propTypes = {
-        fetchInputColumns: PropTypes.func.isRequired, // func() => Promise[Array[String]]
-        api: PropTypes.shape({
-          onParamChanged: PropTypes.func.isRequired,
-        }).isRequired,
-        entriesJsonString: PropTypes.string.isRequired,
-        wfModuleId: PropTypes.number.isRequired,
-        inputLastRelevantDeltaId: PropTypes.number,
-        paramId: PropTypes.number.isRequired,
-        isReadOnly: PropTypes.bool.isRequired
-    };
+  static propTypes = {
+    fetchInputColumns: PropTypes.func.isRequired, // func() => Promise[Array[String]]
+    api: PropTypes.shape({
+      onParamChanged: PropTypes.func.isRequired,
+    }).isRequired,
+    entriesJsonString: PropTypes.string.isRequired,
+    wfModuleId: PropTypes.number.isRequired,
+    inputLastRelevantDeltaId: PropTypes.number,
+    paramId: PropTypes.number.isRequired,
+    isReadOnly: PropTypes.bool.isRequired
+  }
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            entries: parseJsonOrEmpty(this.props.entriesJsonString),
-        };
+    this.state = {
+      entries: parseJsonOrEmpty(this.props.entriesJsonString),
     }
+  }
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.entriesJsonString !== this.props.entriesJsonString) {
-            this.setState({ entries: parseJsonOrEmpty(this.props.entriesJsonString) });
-        }
+  componentDidUpdate(prevProps) {
+    if (prevProps.entriesJsonString !== this.props.entriesJsonString) {
+      this.setState({ entries: parseJsonOrEmpty(this.props.entriesJsonString) })
     }
+  }
 
-    componentDidMount() {
-        if (isEmptyJson(this.props.entriesJsonString)) {
-            // This is a brand-new module. Load an initial state from the
-            // server, renaming all columns.
-            //
-            // This `entries` will not be in the Redux store until we write it;
-            // that'll be the first onColRename/onEntryDelete.
-            this.props.fetchInputColumns(this.props.wfModuleId)
-                .then(columns => {
-                    // avoid race: if we've already renamed something, clearly
-                    // we don't want these values from the server.
-                    if (!isEmptyJson(this.props.entriesJsonString)) return;
+  componentDidMount() {
+    if (isEmptyJson(this.props.entriesJsonString)) {
+      // This is a brand-new module. Load an initial state from the
+      // server, renaming all columns.
+      //
+      // This `entries` will not be in the Redux store until we write it
+      // that'll be the first onColRename/onEntryDelete.
+      this.props.fetchInputColumns(this.props.wfModuleId)
+        .then(columns => {
+          // avoid race: if we've already renamed something, clearly
+          // we don't want these values from the server.
+          if (!isEmptyJson(this.props.entriesJsonString)) return
 
-                    const entries = {};
-                    for (const colname of columns) {
-                        entries[colname] = colname;
-                    }
-                    this.setState({ entries });
-                });
-        }
+          const entries = {}
+          for (const colname of columns) {
+            entries[colname] = colname
+          }
+          this.setState({ entries })
+        })
     }
+  }
 
-    onColRename = (prevName, nextName) => {
-        var newEntries = Object.assign({}, this.state.entries);
-        newEntries[prevName] = nextName;
-        this.props.api.onParamChanged(this.props.paramId, {value: JSON.stringify(newEntries)});
-    }
+  onColRename = (prevName, nextName) => {
+    var newEntries = Object.assign({}, this.state.entries)
+    newEntries[prevName] = nextName
+    this.props.api.onParamChanged(this.props.paramId, {value: JSON.stringify(newEntries)})
+  }
 
-    onEntryDelete = (prevName) => {
-        var newEntries = Object.assign({}, this.state.entries);
-        if (prevName in newEntries) {
-            delete newEntries[prevName];
-            if (Object.keys(newEntries).length == 0) {
-                // I am intermixing actions and API calls here because somehow other combinations
-                // of them do not work
-                store.dispatch(deleteModuleAction(this.props.wfModuleId));
-            } else {
-                this.props.api.onParamChanged(this.props.paramId, {value: JSON.stringify(newEntries)});
-            }
-        }
+  onEntryDelete = (prevName) => {
+    var newEntries = Object.assign({}, this.state.entries)
+    if (prevName in newEntries) {
+      delete newEntries[prevName]
+      if (Object.keys(newEntries).length == 0) {
+        // I am intermixing actions and API calls here because somehow other combinations
+        // of them do not work
+        store.dispatch(deleteModuleAction(this.props.wfModuleId))
+      } else {
+        this.props.api.onParamChanged(this.props.paramId, {value: JSON.stringify(newEntries)})
+      }
     }
+  }
 
-    renderEntries() {
-        var entries = [];
-        for(let col in this.state.entries) {
-            entries.push(
-                <RenameEntry
-                    key={col}
-                    colname={col}
-                    newColname={this.state.entries[col]}
-                    onColRename={this.onColRename}
-                    onEntryDelete={this.onEntryDelete}
-                    isReadOnly={this.props.isReadOnly}
-                />
-            );
-        }
-        return entries;
+  renderEntries() {
+    var entries = []
+    for(let col in this.state.entries) {
+      entries.push(
+        <RenameEntry
+        key={col}
+        colname={col}
+        newColname={this.state.entries[col]}
+        onColRename={this.onColRename}
+        onEntryDelete={this.onEntryDelete}
+        isReadOnly={this.props.isReadOnly}
+        />
+      )
     }
+    return entries
+  }
 
-    render() {
-        const entries = this.renderEntries();
-        return (
-            <div className="RenameEntries--container">{entries}</div>
-        )
-    }
+  render() {
+    const entries = this.renderEntries()
+    return (
+      <div className="RenameEntries--container">{entries}</div>
+    )
+  }
 }
--- assets/js/wfparameters/RenameEntries.test.js
+++ assets/js/wfparameters/RenameEntries.test.js
@@ -1,203 +1,203 @@
 import React from 'react'
 import RenameEntries, {RenameEntry} from './RenameEntries'
 import {mount} from 'enzyme'
-import {jsonResponseMock} from '../test-utils';
+import {jsonResponseMock} from '../test-utils'
 
 jest.mock('../workflow-reducer')
-import { store, deleteModuleAction } from '../workflow-reducer';
+import { store, deleteModuleAction } from '../workflow-reducer'
 
 describe('RenameEntries rendering and interactions', () => {
-    const testEntries = {
-        'name': 'host_name',
-        'narrative': 'nrtv'
-    };
+  const testEntries = {
+    'name': 'host_name',
+    'narrative': 'nrtv'
+  }
 
-    const columns = ['name', 'build_year', 'narrative', 'cornerstone'];
+  const columns = ['name', 'build_year', 'narrative', 'cornerstone']
 
-    const WFM_ID = 1;
-    const PARAM_ID = 2;
+  const WFM_ID = 1
+  const PARAM_ID = 2
 
-    let api = null;
-    let fetchInputColumns = null
+  let api = null
+  let fetchInputColumns = null
 
-    beforeEach(() => {
-        fetchInputColumns = jsonResponseMock(columns)
-        api = {
-            onParamChanged: jest.fn().mockReturnValue(Promise.resolve()),
-        };
-    });
+  beforeEach(() => {
+    fetchInputColumns = jsonResponseMock(columns)
+    api = {
+      onParamChanged: jest.fn().mockReturnValue(Promise.resolve()),
+    }
+  })
 
-    it('Adds all columns when initialized empty', (done) => {
-        // This test corresponds to behavior when added from module library.
-        let tree = mount(<RenameEntries
-            fetchInputColumns={fetchInputColumns}
-            api={api}
-            entriesJsonString={''}
-            wfModuleId={WFM_ID}
-            paramId={PARAM_ID}
-            isReadOnly={false}
-        />);
+  it('Adds all columns when initialized empty', (done) => {
+    // This test corresponds to behavior when added from module library.
+    let tree = mount(<RenameEntries
+      fetchInputColumns={fetchInputColumns}
+      api={api}
+      entriesJsonString={''}
+      wfModuleId={WFM_ID}
+      paramId={PARAM_ID}
+      isReadOnly={false}
+      />)
 
-        setImmediate(() => {
-            expect(fetchInputColumns).toHaveBeenCalledWith(WFM_ID);
+    setImmediate(() => {
+      expect(fetchInputColumns).toHaveBeenCalledWith(WFM_ID)
 
-            // Got the tip to call .update() in this thread:
-            // https://github.com/airbnb/enzyme/issues/1233#issuecomment-343449560
-            tree.update();
+      // Got the tip to call .update() in this thread:
+      // https://github.com/airbnb/enzyme/issues/1233#issuecomment-343449560
+      tree.update()
 
-            expect(tree.find('.rename-input')).toHaveLength(4);
-            expect(tree.find('.rename-input').get(0).props.value).toEqual('name');
-            expect(tree.find('.rename-input').get(1).props.value).toEqual('build_year');
-            tree.unmount();
-            done();
-        });
-    });
+      expect(tree.find('.rename-input')).toHaveLength(4)
+      expect(tree.find('.rename-input').get(0).props.value).toEqual('name')
+      expect(tree.find('.rename-input').get(1).props.value).toEqual('build_year')
+      tree.unmount()
+      done()
+    })
+  })
 
-    it('Displays all columns in entries', (done) => {
-        let tree = mount(<RenameEntries
-            fetchInputColumns={fetchInputColumns}
-            api={api}
-            entriesJsonString={JSON.stringify(testEntries)}
-            wfModuleId={1}
-            paramId={2}
-            isReadOnly={false}
-        />);
+  it('Displays all columns in entries', (done) => {
+    let tree = mount(<RenameEntries
+      fetchInputColumns={fetchInputColumns}
+      api={api}
+      entriesJsonString={JSON.stringify(testEntries)}
+      wfModuleId={1}
+      paramId={2}
+      isReadOnly={false}
+      />)
 
-        setImmediate(() => {
-            // Got the tip to call .update() in this thread:
-            // https://github.com/airbnb/enzyme/issues/1233#issuecomment-343449560
-            tree.update();
-            expect(tree.find('.rename-input')).toHaveLength(2);
-            expect(tree.find('.rename-input').get(0).props.value).toEqual('host_name');
-            expect(tree.find('.rename-input').get(1).props.value).toEqual('nrtv');
-            tree.unmount();
-            done();
-        });
-    });
+    setImmediate(() => {
+      // Got the tip to call .update() in this thread:
+      // https://github.com/airbnb/enzyme/issues/1233#issuecomment-343449560
+      tree.update()
+      expect(tree.find('.rename-input')).toHaveLength(2)
+      expect(tree.find('.rename-input').get(0).props.value).toEqual('host_name')
+      expect(tree.find('.rename-input').get(1).props.value).toEqual('nrtv')
+      tree.unmount()
+      done()
+    })
+  })
 
-    it('Updates parameter upon input completion via blur', (done) => {
-        let tree = mount(<RenameEntries
-            fetchInputColumns={fetchInputColumns}
-            api={api}
-            entriesJsonString={JSON.stringify(testEntries)}
-            wfModuleId={WFM_ID}
-            paramId={PARAM_ID}
-            isReadOnly={false}
-        />);
+  it('Updates parameter upon input completion via blur', (done) => {
+    let tree = mount(<RenameEntries
+      fetchInputColumns={fetchInputColumns}
+      api={api}
+      entriesJsonString={JSON.stringify(testEntries)}
+      wfModuleId={WFM_ID}
+      paramId={PARAM_ID}
+      isReadOnly={false}
+      />)
 
-        setImmediate(() => {
-            tree.update();
-            expect(tree.find('input[value="host_name"]')).toHaveLength(1);
-            let yearInput = tree.find('input[value="host_name"]');
-            yearInput.simulate('change', {target: {value: 'hn'}});
-            yearInput.simulate('blur');
-            setImmediate(() => {
-                expect(api.onParamChanged.mock.calls).toHaveLength(1);
-                expect(api.onParamChanged.mock.calls[0]).toHaveLength(2);
-                expect(api.onParamChanged.mock.calls[0][0]).toBe(PARAM_ID);
-                let updatedEntries = JSON.parse(api.onParamChanged.mock.calls[0][1].value);
-                expect(updatedEntries['name']).toBe('hn');
-                expect(updatedEntries['narrative']).toBe('nrtv');
-                tree.unmount();
-                done();
-            });
-        });
-    });
+    setImmediate(() => {
+      tree.update()
+      expect(tree.find('input[value="host_name"]')).toHaveLength(1)
+      let yearInput = tree.find('input[value="host_name"]')
+      yearInput.simulate('change', {target: {value: 'hn'}})
+      yearInput.simulate('blur')
+      setImmediate(() => {
+        expect(api.onParamChanged.mock.calls).toHaveLength(1)
+        expect(api.onParamChanged.mock.calls[0]).toHaveLength(2)
+        expect(api.onParamChanged.mock.calls[0][0]).toBe(PARAM_ID)
+        let updatedEntries = JSON.parse(api.onParamChanged.mock.calls[0][1].value)
+        expect(updatedEntries['name']).toBe('hn')
+        expect(updatedEntries['narrative']).toBe('nrtv')
+        tree.unmount()
+        done()
+      })
+    })
+  })
 
-    it('Updates parameter upon input completion via enter key', (done) => {
-        let tree = mount(<RenameEntries
-            fetchInputColumns={fetchInputColumns}
-            api={api}
-            entriesJsonString={JSON.stringify(testEntries)}
-            wfModuleId={WFM_ID}
-            paramId={PARAM_ID}
-            isReadOnly={false}
-        />);
+  it('Updates parameter upon input completion via enter key', (done) => {
+    let tree = mount(<RenameEntries
+      fetchInputColumns={fetchInputColumns}
+      api={api}
+      entriesJsonString={JSON.stringify(testEntries)}
+      wfModuleId={WFM_ID}
+      paramId={PARAM_ID}
+      isReadOnly={false}
+      />)
 
-        setImmediate(() => {
-            tree.update();
-            expect(tree.find('input[value="host_name"]')).toHaveLength(1);
-            let nameInput = tree.find('input[value="host_name"]');
-            nameInput.simulate('change', {target: {value: 'host'}});
-            nameInput.simulate('keypress', {key: 'Enter'});
-            setImmediate(() => {
-                expect(api.onParamChanged.mock.calls).toHaveLength(1);
-                expect(api.onParamChanged.mock.calls[0]).toHaveLength(2);
-                expect(api.onParamChanged.mock.calls[0][0]).toBe(PARAM_ID);
-                let updatedEntries = JSON.parse(api.onParamChanged.mock.calls[0][1].value);
-                expect(updatedEntries['name']).toBe('host');
-                expect(updatedEntries['narrative']).toBe('nrtv');
-                tree.unmount();
-                done();
-            });
-        });
-    });
+    setImmediate(() => {
+      tree.update()
+      expect(tree.find('input[value="host_name"]')).toHaveLength(1)
+      let nameInput = tree.find('input[value="host_name"]')
+      nameInput.simulate('change', {target: {value: 'host'}})
+      nameInput.simulate('keypress', {key: 'Enter'})
+      setImmediate(() => {
+        expect(api.onParamChanged.mock.calls).toHaveLength(1)
+        expect(api.onParamChanged.mock.calls[0]).toHaveLength(2)
+        expect(api.onParamChanged.mock.calls[0][0]).toBe(PARAM_ID)
+        let updatedEntries = JSON.parse(api.onParamChanged.mock.calls[0][1].value)
+        expect(updatedEntries['name']).toBe('host')
+        expect(updatedEntries['narrative']).toBe('nrtv')
+        tree.unmount()
+        done()
+      })
+    })
+  })
 
-    it('Updates parameter upon deleting an entry', (done) => {
-        let tree = mount(<RenameEntries
-            fetchInputColumns={fetchInputColumns}
-            api={api}
-            entriesJsonString={JSON.stringify(testEntries)}
-            wfModuleId={WFM_ID}
-            paramId={PARAM_ID}
-            isReadOnly={false}
-        />);
+  it('Updates parameter upon deleting an entry', (done) => {
+    let tree = mount(<RenameEntries
+      fetchInputColumns={fetchInputColumns}
+      api={api}
+      entriesJsonString={JSON.stringify(testEntries)}
+      wfModuleId={WFM_ID}
+      paramId={PARAM_ID}
+      isReadOnly={false}
+      />)
 
-        setImmediate(() => {
-            tree.update();
-            expect(tree.find('RenameEntry')).toHaveLength(2);
-            // Should be the "name" entry that we will delete next
-            let nameEntry = tree.find('RenameEntry').first();
-            expect(nameEntry.find('.rename-delete')).toHaveLength(1);
-            let deleteBtn = nameEntry.find('.rename-delete');
-            deleteBtn.simulate('click');
-            setImmediate(() => {
-                expect(api.onParamChanged.mock.calls).toHaveLength(1);
-                expect(api.onParamChanged.mock.calls[0]).toHaveLength(2);
-                expect(api.onParamChanged.mock.calls[0][0]).toBe(PARAM_ID);
-                let updatedEntries = JSON.parse(api.onParamChanged.mock.calls[0][1].value);
-                expect(updatedEntries['name']).toBeUndefined();
-                expect(updatedEntries['narrative']).toBe('nrtv');
-                tree.unmount();
-                done();
-            });
-        });
-    });
+    setImmediate(() => {
+      tree.update()
+      expect(tree.find('RenameEntry')).toHaveLength(2)
+      // Should be the "name" entry that we will delete next
+      let nameEntry = tree.find('RenameEntry').first()
+      expect(nameEntry.find('.rename-delete')).toHaveLength(1)
+      let deleteBtn = nameEntry.find('.rename-delete')
+      deleteBtn.simulate('click')
+      setImmediate(() => {
+        expect(api.onParamChanged.mock.calls).toHaveLength(1)
+        expect(api.onParamChanged.mock.calls[0]).toHaveLength(2)
+        expect(api.onParamChanged.mock.calls[0][0]).toBe(PARAM_ID)
+        let updatedEntries = JSON.parse(api.onParamChanged.mock.calls[0][1].value)
+        expect(updatedEntries['name']).toBeUndefined()
+        expect(updatedEntries['narrative']).toBe('nrtv')
+        tree.unmount()
+        done()
+      })
+    })
+  })
 
-    it('Deletes itself if all entries are deleted', (done) => {
-        const state = {
-            workflow: {
-                wf_modules: [
-                    {
-                        id: WFM_ID - 1
-                    },
-                    {
-                        id: WFM_ID
-                    }
-                ]
-            }
-        };
-        store.getState.mockImplementation(() => state)
-        deleteModuleAction.mockImplementation((...args) => [ 'deleteModuleAction', ...args ])
+  it('Deletes itself if all entries are deleted', (done) => {
+    const state = {
+      workflow: {
+        wf_modules: [
+          {
+            id: WFM_ID - 1
+          },
+          {
+            id: WFM_ID
+          }
+        ]
+      }
+    }
+    store.getState.mockImplementation(() => state)
+    deleteModuleAction.mockImplementation((...args) => [ 'deleteModuleAction', ...args ])
 
-        let tree = mount(<RenameEntries
-            fetchInputColumns={fetchInputColumns}
-            api={api}
-            entriesJsonString={JSON.stringify({'name': 'host_name'})}
-            wfModuleId={WFM_ID}
-            paramId={PARAM_ID}
-            isReadOnly={false}
-        />);
+    let tree = mount(<RenameEntries
+      fetchInputColumns={fetchInputColumns}
+      api={api}
+      entriesJsonString={JSON.stringify({'name': 'host_name'})}
+      wfModuleId={WFM_ID}
+      paramId={PARAM_ID}
+      isReadOnly={false}
+      />)
 
-        setImmediate(() => {
-            tree.update();
-            expect(tree.find('RenameEntry')).toHaveLength(1);
-            let firstEntry = tree.find('RenameEntry').first();
-            expect(firstEntry.find('.rename-delete')).toHaveLength(1);
-            firstEntry.find('.rename-delete').simulate('click');
-            expect(store.dispatch).toHaveBeenCalledWith([ 'deleteModuleAction', WFM_ID ]);
-            tree.unmount();
-            done();
-        })
-    });
-});
+    setImmediate(() => {
+      tree.update()
+      expect(tree.find('RenameEntry')).toHaveLength(1)
+      let firstEntry = tree.find('RenameEntry').first()
+      expect(firstEntry.find('.rename-delete')).toHaveLength(1)
+      firstEntry.find('.rename-delete').simulate('click')
+      expect(store.dispatch).toHaveBeenCalledWith([ 'deleteModuleAction', WFM_ID ])
+      tree.unmount()
+      done()
+    })
+  })
+})
