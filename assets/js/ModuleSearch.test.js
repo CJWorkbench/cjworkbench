36bf308117d261a7e08744dedc62088eb2504010
emrig <emrig@users.noreply.github.com>
2018-09-04 17:46:05 -0400

    Sort module search alphabetically [Delivers #159837619]

--- assets/js/ModuleSearch.test.js
+++ assets/js/ModuleSearch.test.js
@@ -85,6 +85,62 @@ describe('ModuleSearch', () => {
       expect(wrapper.find('li[data-module-name="Load from Enigma"]').hasClass('lesson-highlight')).toBe(true)
       expect(wrapper.find('li[data-module-name="Filter by Text"]').hasClass('lesson-highlight')).toBe(false)
     })
+
+    it('should sort modules in alphabetical order', () => {
+      wrapper.unmount()
+      const modules = {
+        4: {
+          id: 4,
+          name: "Z",
+          category: "Add data",
+          icon: "url",
+          isLessonHighlight: true,
+        },
+        1: {
+          id: 1,
+          name: "Y",
+          category: "Filter",
+          icon: "filter",
+          isLessonHighlight: false,
+        },
+        6: {
+          id: 6,
+          name: "H",
+          category: "Filter",
+          icon: "filter",
+          isLessonHighlight: false,
+        },
+        7: {
+          id: 7,
+          name: "A",
+          category: "Add data",
+          icon: "filter",
+          isLessonHighlight: false,
+        },
+        5: {
+          id: 5,
+          name: "A",
+          category: "Filter",
+          icon: "filter",
+          isLessonHighlight: false,
+        }
+      }
+      const modulesArray = Object.keys(modules).map(id => modules[id])
+
+      let defaultProps = {
+        onClickModuleId: jest.fn(),
+        onCancel: jest.fn(),
+        index: 2,
+        modules: modulesArray,
+        isLessonHighlight: false
+      }
+
+      wrapper = mount(<ModuleSearch {...defaultProps}/>)
+
+      let resultList = wrapper.prop('modules').map(x => x.name)
+      let expectedResult = ['A', 'A', 'H', 'Y', 'Z']
+      expect(resultList).toEqual(expectedResult)
+    })
   })
     
   it('should highlight search box based on isLessonHighlight', () => {
