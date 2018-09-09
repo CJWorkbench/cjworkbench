acddb4f5414282ed456c95a607261d2c52d9b187
emrig <emrig@users.noreply.github.com>
2018-09-09 17:12:49 -0400

    Added paste option to rename column module [Delivers #160366944]

--- server/modules/rename.py
+++ server/modules/rename.py
@@ -1,21 +1,47 @@
 from .moduleimpl import ModuleImpl
 import json
 
+s_map = [',', ';', '\t', '\n']
 
-class RenameFromTable(ModuleImpl):
+def parse_list(wf_module, table):
+    list_string = wf_module.get_param_string('list_string')
+    if not list_string:
+        return table
 
-    # Rename entry structure: Dictionary of {old_name: new_name}
+    table_width = len(table.columns)
+    separator_counts = [list_string.count(x) for x in s_map]
 
-    @staticmethod
-    def render(wf_module, table):
-        entries_json = wf_module.get_param_raw('rename-entries', 'custom')
-        entries = {}
-        try:
-            entries = json.loads(entries_json)
-        except:
-            return table
+    if sum(separator_counts) == 0:
+        return (table, 'Separator between names not detected.')
 
-        og_columns = table.columns.tolist()
-        new_columns = [entries.get(col, col) for col in og_columns]
+    separator = s_map[separator_counts.index(max(separator_counts))]
+    new_columns = list_string.split(separator)
+
+    if table_width != len(new_columns):
+        return (table, f"Length of input list ({len(new_columns)}) does not match width of table ({table_width}).")
+
+    try:
         table.columns = new_columns
         return table
+    except Exception as e:
+        return(table, str(e.args[0]))
+
+class RenameFromTable(ModuleImpl):
+    # Rename entry structure: Dictionary of {old_name: new_name}
+    @staticmethod
+    def render(wf_module, table):
+        custom_list = wf_module.get_param_checkbox('custom_list')
+        if not custom_list:
+            entries_json = wf_module.get_param_raw('rename-entries', 'custom')
+            entries = {}
+            try:
+                entries = json.loads(entries_json)
+            except:
+                return table
+
+            og_columns = table.columns.tolist()
+            new_columns = [entries.get(col, col) for col in og_columns]
+            table.columns = new_columns
+            return table
+        else:
+            return parse_list(wf_module, table)
--- server/tests/modules/test_rename.py
+++ server/tests/modules/test_rename.py
@@ -30,9 +30,13 @@ class RenameFromTableTests(LoggedInTestCase):
         self.workflow = create_testdata_workflow(csv_text=test_csv)
         self.wf_module = load_and_add_module('rename', workflow=self.workflow)
         self.entries_pval = get_param_by_id_name('rename-entries')
+        self.custom_list = get_param_by_id_name('custom_list')
+        self.list_string = get_param_by_id_name('list_string')
 
     def test_rename_empty_str(self):
         # Should only happen when a module is first created. Return table
+        self.custom_list.value = False
+        self.custom_list.save()
         self.entries_pval.value = ' '
         self.entries_pval.save()
         result = execute_wfmodule(self.wf_module)
@@ -40,12 +44,16 @@ class RenameFromTableTests(LoggedInTestCase):
 
     def test_rename_empty(self):
         # If there are no entries, return table
+        self.custom_list.value = False
+        self.custom_list.save()
         self.entries_pval.value = json.dumps({})
         self.entries_pval.save()
         result = execute_wfmodule(self.wf_module)
         self.assertEqual(result, ProcessResult(reference_table))
 
-    def test_rename(self):
+    def test_rename_from_table(self):
+        self.custom_list.value = False
+        self.custom_list.save()
         self.entries_pval.value = json.dumps({
             'name': 'name1',
             'count': 'CNT'
@@ -55,3 +63,45 @@ class RenameFromTableTests(LoggedInTestCase):
         expected_table = reference_table.copy()
         expected_table.columns = ['name1', 'date', 'CNT', 'float']
         self.assertEqual(result, ProcessResult(expected_table))
+
+    def test_rename_custom_list_separator1(self):
+        # new line separator
+        self.custom_list.value = True
+        self.custom_list.save()
+        self.list_string.value = '1\n2\n3\n4'
+        self.list_string.save()
+        result = execute_wfmodule(self.wf_module)
+        expected_table = reference_table.copy()
+        expected_table.columns = ['1', '2', '3', '4']
+        self.assertEqual(result, ProcessResult(expected_table))
+
+    def test_rename_custom_list_separator2(self):
+        # comma separator
+        self.custom_list.value = True
+        self.custom_list.save()
+        self.list_string.value = 'a,b,c,d'
+        self.list_string.save()
+        expected_table = reference_table.copy()
+        expected_table.columns = ['a', 'b', 'c', 'd']
+        result = execute_wfmodule(self.wf_module)
+        self.assertEqual(result, ProcessResult(expected_table))
+
+    def test_errors_sep_not_detected(self):
+        # No separator
+        self.custom_list.value = True
+        self.custom_list.save()
+        self.list_string.value = '1234567'
+        self.list_string.save()
+        result = execute_wfmodule(self.wf_module)
+        self.assertEqual(result, ProcessResult(reference_table,
+                                               'Separator between names not detected.'))
+
+    def test_errors_invalid_length(self):
+        # Incorrect number of values
+        self.custom_list.value = True
+        self.custom_list.save()
+        self.list_string.value = '1\n2\n3\n4\n5'
+        self.list_string.save()
+        result = execute_wfmodule(self.wf_module)
+        self.assertEqual(result, ProcessResult(reference_table,
+                                    'Length of input list (5) does not match width of table (4).'))
