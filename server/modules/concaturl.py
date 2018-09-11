10e723f15fc33a7cfeea31270ee4577defaf270d
emrig <emrig@users.noreply.github.com>
2018-09-10 23:41:30 -0400

    Initial commit. Concat module first pass and more robust join module. TODO: Unit tests

---
+++ server/modules/concaturl.py
@@ -0,0 +1,53 @@
+from .moduleimpl import ModuleImpl
+from .types import ProcessResult
+from .utils import store_external_workflow, get_id_from_url
+import pandas as pd
+
+_type_map = "Only include this workflow's columns|Only include matching columns|Include columns from both workflows".lower().split('|')
+_join_type_map = 'outer|inner|outer'.split('|')
+_source_column_name = 'Source Workflow'
+
+class ConcatURL(ModuleImpl):
+    @staticmethod
+    def render(wf_module, table):
+        try:
+            right_table = wf_module.retrieve_fetched_table()
+        except Exception:
+            right_table = None
+
+        if right_table is None or right_table.empty:
+            return ProcessResult(table,
+                                 wf_module.error_msg)
+
+        type = wf_module.get_param_menu_idx('type')
+        url = wf_module.get_param_string('url').strip()
+        source_columns = wf_module.get_param_checkbox('source_columns')
+
+        try:
+            right_id = get_id_from_url(url)
+            concat_table = pd.concat([table, right_table], keys=['Current', right_id], join=_join_type_map[type])
+        except Exception as err:
+            return ProcessResult(table, error=str(err.args[0]))
+
+        if type == 0:
+            concat_table = concat_table[table.columns]
+
+        if source_columns:
+            # Allow duplicates set to True because sanitize handles name collisions
+            source_series = concat_table.reset_index()['level_0']
+            concat_table.insert(0, _source_column_name, source_series.values, allow_duplicates=True)
+
+        return ProcessResult(concat_table)
+
+    # Load external workflow and store
+    @staticmethod
+    def event(wf_module, **kwargs):
+        url = wf_module.get_param_string('url').strip()
+        if not url:
+            return
+
+        try:
+            store_external_workflow(wf_module, url)
+        except Exception as err:
+            ModuleImpl.commit_result(wf_module, ProcessResult(error=str(err.args[0])))
+            return
--- server/modules/utils.py
+++ server/modules/utils.py
@@ -13,6 +13,12 @@ import cchardet as chardet
 from server.sanitizedataframe import autocast_dtypes_in_place
 from django.conf import settings
 from django.db.models.fields.files import FieldFile
+from django.db import transaction
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+from server.models.Workflow import Workflow
+from .moduleimpl import ModuleImpl
+
 
 _TextEncoding = Optional[str]
 
@@ -286,3 +292,57 @@ def parse_bytesio(bytesio: io.BytesIO, mime_type: str,
         return _safe_parse(bytesio, parser, text_encoding)
     else:
         return ProcessResult(error=f'Unhandled MIME type "{mime_type}"')
+
+# Utility functions for workflow import modules
+
+def _get_wf_module_by_id(wf_id):
+    try:
+        with transaction.atomic():
+            return Workflow.objects.select_for_update(nowait=False).get(id=wf_id)
+    except Workflow.DoesNotExist:
+        raise Exception('Target workflow does not exist')
+    except Exception as e:
+        raise e
+
+def _validate_url(url):
+    try:
+        validate = URLValidator()
+        validate(url)
+    except ValidationError:
+        return ValidationError('Invalid URL')
+
+def get_id_from_url(url):
+    #TODO: Environment check
+    path = url.split('/')
+    try:
+        _validate_url(url)
+        _id = int(path[path.index('workflows') + 1])
+        return _id
+    except ValueError:
+        raise ValueError(f'Error fetching {url}: '
+                'Invalid workflow URL')
+
+def store_external_workflow(wf_module, url):
+    right_wf_id = get_id_from_url(url)
+    # fetching could take a while so notify clients/users we're working
+    wf_module.set_busy()
+    right_wf_module = _get_wf_module_by_id(right_wf_id)
+
+    # Check to see if workflow_id the same
+    if wf_module.workflow_id == right_wf_module.id:
+        raise Exception('Cannot import the current workflow')
+
+    # Make sure _this_ workflow's owner has access permissions to the _other_ workflow
+    user = wf_module.workflow.owner
+    if not right_wf_module.user_session_authorized_read(user, None):
+        raise Exception('Access denied to the target workflow')
+
+    right_wf_module = right_wf_module.wf_modules.last()
+
+    # Always pull the cached result, so we can't execute() an infinite loop
+    right_result = right_wf_module.get_cached_render_result().result
+    result = ProcessResult(dataframe=right_result.dataframe)
+
+    result.truncate_in_place_if_too_big()
+    result.sanitize_in_place()
+    ModuleImpl.commit_result(wf_module, result)
