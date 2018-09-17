e1cbc4f5f8e3288a553703167c9d4f140b736455
Pierre Conti <pierre@tablesdata.com>
2018-09-17 16:54:23 -0400

    Integration test

--- integrationtests/utils.py
+++ integrationtests/utils.py
@@ -72,11 +72,11 @@ class WorkbenchBase(unittest.TestCase):
 
         if position is None:
             with b.scope('.in-between-modules:last-child'):
-                b.click_button('Add step')
+                b.click_button('ADD STEP')
         else:
             i = position * 2 + 1
             with b.scope(f'.in-between-modules:nth-child({i})'):
-                b.click_button('Add step')
+                b.click_button('ADD STEP')
 
         # Search. That way, we won't need to worry about overflow:auto
         b.fill_in('moduleQ', name)
