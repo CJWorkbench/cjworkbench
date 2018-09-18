a56540d226f7a9e276eb2f22239754cbc1e9aa3a
emrig <emrig@users.noreply.github.com>
2018-09-17 22:09:01 -0400

    Select columns with range input [Delivers #160525893]

--- server/modules/selectcolumns.py
+++ server/modules/selectcolumns.py
@@ -1,28 +1,113 @@
 from .moduleimpl import ModuleImpl
 from server.modules.types import ProcessResult
+import pandas as pd
+from pandas import IntervalIndex
+from typing import Any, Dict, Tuple
+import re
 
+commas = re.compile('\\s*,\\s*')
+numbers = re.compile('(?P<first>[1-9]\d*)(?:-(?P<last>[1-9]\d*))?')
 
 class SelectColumns(ModuleImpl):
     def render(wf_module, table):
-        drop_or_keep = wf_module.get_param_radio_idx("drop_or_keep")
-        cols = wf_module.get_param_string('colnames').split(',')
-        cols = [c.strip() for c in cols]
+        drop_or_keep = wf_module.get_param_radio_idx('drop_or_keep')
+        select_range = wf_module.get_param_checkbox('select_range')
 
-        # if no column has been selected, keep the columns
-        if cols == [] or cols == ['']:
-            return ProcessResult(table)
+        if not select_range:
+            cols = wf_module.get_param_string('colnames').split(',')
+            cols = [c.strip() for c in cols]
+            return select_columns_by_name(table, cols, drop_or_keep)
 
-        # ensure we do not change the order of the columns, even if they are
-        # listed in another order this also silently removes any nonexistent
-        # columns (can happen when re-ordering module, etc.)
-        existing = list(table.columns)
-        newcols = []
-        for c in existing:
-            if c in cols:
-                newcols.append(c)
-
-        if drop_or_keep == 1:
-            newtab = table[newcols]
         else:
-            newtab = table.drop(newcols, axis=1)
-        return ProcessResult(newtab)
+            str_col_nums = wf_module.get_param_string('column_numbers')
+            return select_columns_by_number(table, str_col_nums, drop_or_keep)
+
+
+def select_columns_by_name(table, cols, drop_or_keep):
+    # if no column has been selected, keep the columns
+    if cols == [] or cols == ['']:
+        return ProcessResult(table)
+
+    # ensure we do not change the order of the columns, even if they are
+    # listed in another order this also silently removes any nonexistent
+    # columns (can happen when re-ordering module, etc.)
+    existing = list(table.columns)
+    newcols = []
+    for c in existing:
+        if c in cols:
+            newcols.append(c)
+
+    if drop_or_keep == 1:
+        newtab = table[newcols]
+    else:
+        newtab = table.drop(newcols, axis=1)
+    return ProcessResult(newtab)
+
+def select_columns_by_number(table, str_col_nums, drop_or_keep):
+    try:
+        form = Form.parse(str_col_nums)
+        if not form:
+            return ProcessResult(table)
+    except ValueError as err:
+        return ProcessResult(table, error=str(err))
+
+    table_col_nums = list(range(0, len(table.columns)))
+
+    try:
+        mask = form.index.get_indexer(table_col_nums) != -1
+    except KeyError:
+        return ProcessResult(table, error='There are overlapping numbers in input range')
+    except Exception as err:
+        return ProcessResult(table, error=str(err.args[0]))
+
+    if drop_or_keep == 0:
+        new_col_nums = [x[0] for x in enumerate(mask) if not x[1]]
+    else:
+        new_col_nums = [x[0] for x in enumerate(mask) if x[1]]
+    ret = table.iloc[:, new_col_nums]
+    return ProcessResult(ret)
+
+def parse_interval(s: str) -> Tuple[int, int]:
+    """
+    Parse a string 'interval' into a tuple
+    >>> parse_interval('1')
+    (0, 1)
+    >>> parse_interval('1-3')
+    (0, 2)
+    >>> parse_interval('5')
+    (4, 4)
+    >>> parse_interval('hi')
+    Traceback (most recent call last):
+        ...
+    ValueError: Rows must look like "1-2", "5" or "1-2, 5"; got "hi"
+    """
+    match = numbers.fullmatch(s)
+    if not match:
+        raise ValueError(
+            f'Rows must look like "1-2", "5" or "1-2, 5"; got "{s}"'
+        )
+
+    first = int(match.group('first'))
+    last = int(match.group('last') or first)
+    return (first - 1, last - 1)
+
+# Copied from droprowsbypoition.py and modified for columns
+class Form:
+    def __init__(self, index: IntervalIndex):
+        self.index = index
+
+    @staticmethod
+    def parse_v1(column_numbers: str) -> 'Form':
+        """Parse 'column_numbers', or raise ValueError"""
+        tuples = [parse_interval(s)
+                  for s in commas.split(column_numbers.strip())]
+        return Form(IntervalIndex.from_tuples(tuples, closed='both'))
+
+    @staticmethod
+    def parse(str_col_nums: str) -> 'Form':
+        try:
+            if not str_col_nums:
+                return None
+            return Form.parse_v1(str_col_nums)
+        except KeyError:
+            return None
--- server/tests/modules/test_selectcolumns.py
+++ server/tests/modules/test_selectcolumns.py
@@ -9,9 +9,11 @@ DROP = 0
 
 
 class MockWfModule:
-    def __init__(self, colnames, drop_or_keep):
+    def __init__(self, colnames, drop_or_keep, select_range, column_numbers):
         self.colnames = colnames
         self.drop_or_keep = drop_or_keep
+        self.select_range = select_range
+        self.column_numbers = column_numbers
 
     def get_param_radio_idx(self, name):
         return getattr(self, name)
@@ -19,9 +21,11 @@ class MockWfModule:
     def get_param_string(self, name):
         return getattr(self, name)
 
+    def get_param_checkbox(self, name):
+        return getattr(self, name)
 
-def render(table, colnames, drop_or_keep) -> ProcessResult:
-    wf_module = MockWfModule(colnames, drop_or_keep)
+def render(table, colnames, drop_or_keep, select_range, column_numbers) -> ProcessResult:
+    wf_module = MockWfModule(colnames, drop_or_keep, select_range, column_numbers)
     result = SelectColumns.render(wf_module, table)
     return result
 
@@ -32,23 +36,36 @@ class SelectColumnsTests(unittest.TestCase):
         self.table = pd.DataFrame({'A': [1, 2], 'B': [2, 3], 'C': [3, 4]})
 
     def test_render_single_column(self):
-        result = render(self.table, 'A', 1)
+        result = render(self.table, 'A', 1, False, '')
         self.assertEqual(result, ProcessResult(pd.DataFrame({'A': [1, 2]})))
 
     def test_render_strip_whitespace(self):
-        result = render(self.table, 'A ', 1)
+        result = render(self.table, 'A ', 1, False, '')
         self.assertEqual(result, ProcessResult(pd.DataFrame({'A': [1, 2]})))
 
     def test_render_maintain_input_column_order(self):
-        result = render(self.table, 'B,A', 1)
+        result = render(self.table, 'B,A', 1, False, '')
         self.assertEqual(result, ProcessResult(
             pd.DataFrame({'A': [1, 2], 'B': [2, 3]})
         ))
 
     def test_render_ignore_invalid_column_name(self):
-        result = render(self.table, 'A,X', 1)
+        result = render(self.table, 'A,X', 1, False, '')
         self.assertEqual(result, ProcessResult(pd.DataFrame({'A': [1, 2]})))
 
     def test_render_drop_columns(self):
-        result = render(self.table, 'B,C', 0)
+        result = render(self.table, 'B,C', 0, False, '')
         self.assertEqual(result, ProcessResult(pd.DataFrame({'A': [1, 2]})))
+
+    def test_render_range_input(self):
+        result = render(self.table.copy(), '', 1, True, '1,3')
+        self.assertEqual(result, ProcessResult(self.table[['A','C']]))
+
+        result = render(self.table.copy(), '', 0, True, '2-3')
+        self.assertEqual(result, ProcessResult(pd.DataFrame(self.table[['A']])))
+
+        result = render(self.table.copy(), '', 0, True, '2-3,2')
+        self.assertEqual(result, ProcessResult(self.table, error='There are overlapping numbers in input range'))
+
+        result = render(self.table.copy(), '', 0, True, '2-3,giraffe')
+        self.assertEqual(result, ProcessResult(self.table, error='Rows must look like "1-2", "5" or "1-2, 5"; got "giraffe"'))
