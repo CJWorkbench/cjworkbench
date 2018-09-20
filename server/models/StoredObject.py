49c06c892ca0fc929b0830bc3338bdca1fb304e9
Adam Hooper <adam@adamhooper.com>
2018-09-20 11:16:10 -0400

    StoredObject: PEP8-ize

--- server/models/StoredObject.py
+++ server/models/StoredObject.py
@@ -9,11 +9,13 @@ import pandas as pd
 from server.pandas_util import hash_table
 from server import parquet
 
+
 # StoredObject is our persistence layer.
 # Allows WfModules to store keyed, versioned binary objects
 class StoredObject(models.Model):
     # delete stored data if WfModule deleted
-    wf_module = models.ForeignKey('WfModule', related_name='stored_objects', on_delete=models.CASCADE)
+    wf_module = models.ForeignKey('WfModule', related_name='stored_objects',
+                                  on_delete=models.CASCADE)
 
     # identification for file backing store
     file = models.FileField()
@@ -29,7 +31,8 @@ class StoredObject(models.Model):
     read = models.BooleanField(default=False)
 
     # filename combines wf module id and our object id
-    # (self.id is sufficient but putting wfm.id in the filename helps debugging)
+    # (self.id is sufficient but putting wfm.id in the filename helps
+    # debugging)
     @staticmethod
     def _storage_filename(wfm_id):
         fname = f'{wfm_id}-{uuid.uuid1()}-fetch.dat'
@@ -41,18 +44,22 @@ class StoredObject(models.Model):
             return StoredObject.__create_empty_table(wf_module, metadata)
         else:
             hash = hash_table(table)
-            return StoredObject.__create_table_internal(wf_module, table, metadata, hash)
+            return StoredObject.__create_table_internal(wf_module, table,
+                                                        metadata, hash)
 
-    # Create a new StoredObject if it's going to store different data than the previous one. Otherwise null
-    # Fast; checks hash without loading file contents
+    # Create a new StoredObject if it's going to store different data than the
+    # previous one. Otherwise null Fast; checks hash without loading file
+    # contents
     @staticmethod
     def create_table_if_different(wf_module, old_so, table, metadata=None):
         if old_so is None:
-            return StoredObject.create_table(wf_module, table, metadata=metadata)
+            return StoredObject.create_table(wf_module, table,
+                                             metadata=metadata)
 
         hash = hash_table(table)
         if hash != old_so.hash:
-            return StoredObject.create_table(wf_module, table, metadata=metadata)
+            return StoredObject.create_table(wf_module, table,
+                                             metadata=metadata)
         else:
             return None
 
@@ -69,7 +76,8 @@ class StoredObject(models.Model):
             hash=hash
         )
 
-    # why store an empty table? so we don't have to re-render to know that the output was empty
+    # why store an empty table? so we don't have to re-render to know that the
+    # output was empty
     @staticmethod
     def __create_empty_table(wf_module, metadata):
         return StoredObject.objects.create(
@@ -91,7 +99,9 @@ class StoredObject(models.Model):
     def duplicate(self, to_wf_module):
         if to_wf_module == self.wf_module:
             # Filename would clash, therefore we can't do that
-            raise ValueError("Cannot duplicate a StoredObject to same WfModule")
+            raise ValueError(
+                'Cannot duplicate a StoredObject to same WfModule'
+            )
 
         srcname = default_storage.path(self.file.name)
         new_path = StoredObject._storage_filename(to_wf_module.id)
@@ -100,8 +110,8 @@ class StoredObject(models.Model):
                                              stored_at=self.stored_at,
                                              hash=self.hash,
                                              metadata=self.metadata,
-                                             file = new_path,
-                                             size = self.size)
+                                             file=new_path,
+                                             size=self.size)
         return new_so
 
 
