ff03e44af259c28583978962a00e2256a84426a6
Adam Hooper <adam@adamhooper.com>
2018-09-21 12:18:38 -0400

    Nix React warning about missing key

--- assets/js/wfparameters/RadioParam.js
+++ assets/js/wfparameters/RadioParam.js
@@ -18,7 +18,7 @@ export default class RadioParam extends React.PureComponent {
     var itemDivs = items.map( (name, idx) => {
       const strIdx = idx.toString()
       return (
-        <label className='t-d-gray content-3'>
+        <label key={idx} className='t-d-gray content-3'>
           <input
             className='radio-button'
             type='radio'
