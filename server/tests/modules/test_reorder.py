d1c317f5a7c677e14a0ca5f60111fbc15ef747b4
Adam Hooper <adam@adamhooper.com>
2018-09-24 13:04:59 -0400

    Speed up test_reorder.py

--- server/tests/modules/test_reorder.py
+++ server/tests/modules/test_reorder.py
@@ -1,51 +1,41 @@
-from server.tests.utils import LoggedInTestCase, create_testdata_workflow, \
-        load_and_add_module, get_param_by_id_name
-from server.execute import execute_wfmodule
-from server.modules.types import ProcessResult
 import json
+import unittest
 import pandas as pd
-import numpy as np
-import io
+from server.modules.reorder import ReorderFromTable
+from server.modules.types import ProcessResult
 
-test_csv = (
-    'name,date,count,float\n'
-    + 'Dolores,2018-04-22,3,13.5\n'
-    + 'Bernard,2018-04-23,not_a_count,2.0\n'
-    + 'Ford,2016-10-02,5,\n'
-    + 'Dolores,0_not_a_date,4,2.8'
-)
-# A reference table for correctness checking
-reference_table = pd.read_csv(io.StringIO(test_csv), dtype={
-    'name': object,
-    'date': object,
-    'count': object,
-    'float': np.float64
+
+a_table = pd.DataFrame({
+    'name': [1, 2],
+    'date': [2, 3],
+    'count': [3, 4],
+    'float': [4.0, 5.0]
 })
 
 
-def reordered_result(colnames):
-    return ProcessResult(reference_table[colnames])
+class MockWfModule:
+    def __init__(self, reorder_history_json: str):
+        self.reorder_history = reorder_history_json
+
+    def get_param_raw(self, key, _unused):
+        return getattr(self, key.replace('-', '_'))
 
 
-class ReorderFromTableTests(LoggedInTestCase):
-    # Current dtype choices: "String|Number|Date"
-    # Current direction choices: "Select|Ascending|Descending"
-    # If the position of the values change, tests will need to be updated
+def fake_result(colnames):
+    return ProcessResult(a_table[colnames])
 
-    # NaN and NaT always appear last as the policy in SortFromTable dictates
 
-    def setUp(self):
-        super(ReorderFromTableTests, self).setUp()
-        self.workflow = create_testdata_workflow(csv_text=test_csv)
-        self.wf_module = load_and_add_module('reorder', workflow=self.workflow)
-        self.history_pval = get_param_by_id_name('reorder-history')
+def render(reorder_history_json, table):
+    wf_module = MockWfModule(reorder_history_json)
+    return ProcessResult.coerce(ReorderFromTable.render(wf_module,
+                                                        table.copy()))
 
+
+class ReorderTest(unittest.TestCase):
     def test_reorder_empty(self):
-        self.history_pval.value = ' '
-        self.history_pval.save()
-        result = execute_wfmodule(self.wf_module)
+        result = render(' ', a_table)
         self.assertEqual(result,
-                         reordered_result(['name', 'date', 'count', 'float']))
+                         fake_result(['name', 'date', 'count', 'float']))
 
     def test_reorder(self):
         # In chronological order, starting with
@@ -67,11 +57,9 @@ class ReorderFromTableTests(LoggedInTestCase):
                 'to': 1
             },  # gives ['count', 'float', 'date', 'name']
         ]
-        self.history_pval.value = json.dumps(reorder_ops)
-        self.history_pval.save()
-        result = execute_wfmodule(self.wf_module)
+        result = render(json.dumps(reorder_ops), a_table)
         self.assertEqual(result,
-                         reordered_result(['count', 'float', 'date', 'name']))
+                         fake_result(['count', 'float', 'date', 'name']))
 
     def test_missing_column(self):
         # If an input column is removed (e.g. via select columns)
@@ -99,8 +87,6 @@ class ReorderFromTableTests(LoggedInTestCase):
                 'to': 2
             },  # gives ['count', 'name', 'float', 'date']
         ]
-        self.history_pval.value = json.dumps(reorder_ops)
-        self.history_pval.save()
-        result = execute_wfmodule(self.wf_module)
+        result = render(json.dumps(reorder_ops), a_table)
         self.assertEqual(result,
-                         reordered_result(['count', 'name', 'float', 'date']))
+                         fake_result(['count', 'name', 'float', 'date']))
