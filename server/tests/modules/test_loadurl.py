2536368943a0ff79185657fa9b39bac1d621c222
Adam Hooper <adam@adamhooper.com>
2018-09-27 16:44:19 -0400

    test_loadurl: make it a unit test

--- server/tests/modules/test_loadurl.py
+++ server/tests/modules/test_loadurl.py
@@ -1,18 +1,20 @@
+import asyncio
 from collections import OrderedDict
 import io
 import json
 import os
 import tempfile
+import unittest
 from unittest.mock import patch
+from asgiref.sync import async_to_sync
 from django.conf import settings
-from django.test import override_settings
 import requests
 import pandas as pd
 from server.execute import execute_wfmodule
 from server.models import ParameterVal
+from server.modules.loadurl import LoadURL
 from server.modules.types import ProcessResult
-from server.tests.utils import LoggedInTestCase, load_and_add_module, \
-        mock_xlsx_path
+from server.tests.utils import mock_xlsx_path
 
 XLSX_MIME_TYPE = \
         'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
@@ -48,117 +50,107 @@ def mock_bytes_response(b, content_type):
     return response
 
 
+def respond(str_or_bytes, content_type):
+    def r(*args, **kwargs):
+        if isinstance(str_or_bytes, str):
+            return mock_text_response(str_or_bytes, content_type)
+        else:
+            return mock_bytes_response(str_or_bytes, content_type)
+
+    return r
+
+
 def mock_404_response(text):
-    response = requests.Response()
-    response.encoding = 'utf-8'
-    response.headers['Content-Type'] = 'text/plain; charset=utf-8'
-    response.raw = io.BytesIO(text.encode('utf-8'))
-    response.reason = 'Not Found'
-    response.status_code = 404
-    return response
+    def r(*args, **kwargs):
+        response = requests.Response()
+        response.encoding = 'utf-8'
+        response.headers['Content-Type'] = 'text/plain; charset=utf-8'
+        response.raw = io.BytesIO(text.encode('utf-8'))
+        response.reason = 'Not Found'
+        response.status_code = 404
+        return response
+    return r
 
 
-# ---- LoadURL ----
-@override_settings(MEDIA_ROOT=tempfile.gettempdir())
-class LoadFromURLTests(LoggedInTestCase):
+class MockWfModule:
+    def __init__(self, **kwargs):
+        self.url = kwargs.get('url', '')
+        self.has_header = kwargs.get('has_header', True)
+
+    def get_param_string(self, param):
+        return getattr(self, param)
+
+    def get_param_checkbox(self, param):
+        return getattr(self, param)
+
+    def retrieve_fetched_table(self):
+        return self.fetched_table
+
+
+def run_event(wf_module):
+    async_to_sync(LoadURL.event)(wf_module)
+
+
+class LoadFromURLTests(unittest.TestCase):
     def setUp(self):
-        super().setUp()  # log in
-        self.wfmodule = load_and_add_module('loadurl')
+        super().setUp()
+        self.wf_module = MockWfModule()
 
-        # save references to our parameter values so we can tweak them later
-        self.url_pval = ParameterVal.objects.get(parameter_spec__id_name='url')
-        self.fetch_pval = ParameterVal.objects.get(
-            parameter_spec__id_name='version_select'
-        )
+        self.commit_result_patch = \
+            patch('server.modules.moduleimpl.ModuleImpl.commit_result')
+        self.commit_result = self.commit_result_patch.start()
+        # Mock commit_result by making it return None, asynchronously
+        future = asyncio.Future()
+        future.set_result(None)
+        self.commit_result.return_value = future
 
-    # send fetch event to button to load data
-    def press_fetch_button(self):
-        self.client.post('/api/parameters/%d/event' % self.fetch_pval.id)
-        self.wfmodule.refresh_from_db()  # last_relevant_delta_id changed
+    def tearDown(self):
+        self.commit_result_patch.stop()
+
+        super().tearDown()
 
     def test_load_csv(self):
-        url = 'http://test.com/the.csv'
-        self.url_pval.set_value(url)
-        self.url_pval.save()
-
-        # should be no data saved yet, no Deltas on the workflow
-        self.assertIsNone(self.wfmodule.get_fetched_data_version())
-        self.assertIsNone(self.wfmodule.retrieve_fetched_table())
-        self.assertIsNone(self.wfmodule.workflow.last_delta)
+        self.wf_module.url = 'http://test.com/the.csv'
 
         # success case
-        with patch('requests.get') as get:
-            get.return_value = mock_text_response(mock_csv_text, 'text/csv')
-            self.press_fetch_button()
-            result = execute_wfmodule(self.wfmodule)
-            self.assertEqual(result, ProcessResult(mock_csv_table))
+        with patch('requests.get', respond(mock_csv_text, 'text/csv')):
+            run_event(self.wf_module)
 
-            # should create a new data version on the WfModule, and a new delta
-            # representing the change
-            self.wfmodule.refresh_from_db()
-            self.wfmodule.workflow.refresh_from_db()
-            first_version = self.wfmodule.get_fetched_data_version()
-            first_delta = self.wfmodule.workflow.last_delta
-            first_check_time = self.wfmodule.last_update_check
-            self.assertIsNotNone(first_version)
-            self.assertIsNotNone(first_delta)
+        self.commit_result.assert_called_with(
+            self.wf_module,
+            ProcessResult(mock_csv_table)
+        )
 
-        # retrieving exactly the same data should not create a new data version
-        # or delta, should update check time
-        with patch('requests.get') as get:
-            get.return_value = mock_text_response(mock_csv_text, 'text/csv')
-            self.press_fetch_button()
-
-            self.wfmodule.refresh_from_db()
-            self.wfmodule.workflow.refresh_from_db()
-            self.assertEqual(self.wfmodule.get_fetched_data_version(),
-                             first_version)
-            self.assertEqual(self.wfmodule.workflow.last_delta, first_delta)
-            second_check_time = self.wfmodule.last_update_check
-            self.assertNotEqual(second_check_time, first_check_time)
-
-        # Retrieving different data should create a new data version and delta
-        with patch('requests.get') as get:
-            get.return_value = mock_text_response(mock_csv_text2, 'text/csv')
-            self.press_fetch_button()
-            result = execute_wfmodule(self.wfmodule)
-            self.assertEqual(result, ProcessResult(mock_csv_table2))
-
-            self.wfmodule.refresh_from_db()
-            self.wfmodule.workflow.refresh_from_db()
-            self.assertNotEqual(self.wfmodule.get_fetched_data_version(),
-                                first_version)
-            self.assertNotEqual(self.wfmodule.workflow.last_delta, first_delta)
-            self.assertNotEqual(self.wfmodule.last_update_check,
-                                second_check_time)
-
+    def test_load_invalid_csv(self):
         # malformed CSV should put module in error state
-        with patch('requests.get') as get:
-            get.return_value = mock_text_response('a,b\n"1', 'text/csv')
-            self.press_fetch_button()
-            self.wfmodule.refresh_from_db()
-            self.assertEqual(
-                self.wfmodule.fetch_error,
+        self.wf_module.url = 'http://test.com/the.csv'
+        with patch('requests.get', respond('a,b\n"1', 'text/csv')):
+            run_event(self.wf_module)
+
+        self.commit_result.assert_called_with(
+            self.wf_module,
+            ProcessResult(error=(
                 'Error tokenizing data. C error: EOF inside string '
                 'starting at line 1'
-            )
+            ))
+        )
 
-    def test_load_csv_bad_content_type(self):
+    def test_load_csv_use_ext_given_bad_content_type(self):
         # return text/plain type and rely on filename detection, as
         # https://raw.githubusercontent.com/ does
-        url = 'https://raw.githubusercontent.com/user/repo/branch/the.csv'
-        self.url_pval.set_value(url)
-        self.url_pval.save()
-        with patch('requests.get') as get:
-            get.return_value = mock_text_response(mock_csv_text, 'text/plain')
-            self.press_fetch_button()
-            result = execute_wfmodule(self.wfmodule)
-            self.assertEqual(result, ProcessResult(mock_csv_table))
+        self.wf_module.url = \
+            'https://raw.githubusercontent.com/user/repo/branch/the.csv'
+
+        with patch('requests.get', respond(mock_csv_text, 'text/plain')):
+            run_event(self.wf_module)
+
+        self.commit_result.assert_called_with(
+            self.wf_module,
+            ProcessResult(mock_csv_table)
+        )
 
     def test_load_json(self):
-        url = 'http://test.com/the.json'
-        self.url_pval.set_value(url)
-        self.url_pval.save()
+        self.wf_module.url = 'http://test.com/the.json'
 
         # use a complex example with nested data
         fname = os.path.join(settings.BASE_DIR,
@@ -167,78 +159,75 @@ class LoadFromURLTests(LoggedInTestCase):
         # OrderedDict otherwise cols get sorted
         sfpd_table = pd.DataFrame(json.loads(sfpd_json,
                                              object_pairs_hook=OrderedDict))
+
         expected = ProcessResult(sfpd_table)
         expected.sanitize_in_place()
 
-        with patch('requests.get') as get:
-            get.return_value = mock_text_response(sfpd_json,
-                                                  'application/json')
-            self.press_fetch_button()
-            result = execute_wfmodule(self.wfmodule)
-            self.assertEqual(result, expected)
+        with patch('requests.get', respond(sfpd_json, 'application/json')):
+            run_event(self.wf_module)
 
-    def test_load_json_bad_content(self):
-        url = 'http://test.com/the.json'
-        self.url_pval.set_value(url)
-        self.url_pval.save()
+        self.commit_result.assert_called_with(self.wf_module, expected)
+
+    def test_load_json_invalid_json(self):
+        self.wf_module.url = 'http://test.com/the.json'
 
         # malformed json should put module in error state
-        with patch('requests.get') as get:
-            get.return_value = mock_text_response('not json',
-                                                  'application/json')
-            self.press_fetch_button()
-            self.wfmodule.refresh_from_db()
-            self.assertEqual(self.wfmodule.fetch_error,
-                             'Expecting value: line 1 column 1 (char 0)')
+        with patch('requests.get', respond('not json', 'application/json')):
+            run_event(self.wf_module)
+
+        self.commit_result.assert_called_with(
+            self.wf_module,
+            ProcessResult(error='Expecting value: line 1 column 1 (char 0)')
+        )
 
     def test_load_xlsx(self):
-        url = 'http://test.com/the.xlsx'
-        self.url_pval.set_value(url)
-        self.url_pval.save()
+        self.wf_module.url = 'http://test.com/the.xlsx'
 
         xlsx_bytes = open(mock_xlsx_path, "rb").read()
         xlsx_table = pd.read_excel(mock_xlsx_path)
+        response = respond(xlsx_bytes, XLSX_MIME_TYPE)
 
-        with patch('requests.get') as get:
-            get.return_value = mock_bytes_response(xlsx_bytes, XLSX_MIME_TYPE)
-            self.press_fetch_button()
-            result = execute_wfmodule(self.wfmodule)
-            self.assertEqual(result, ProcessResult(xlsx_table))
+        with patch('requests.get', response):
+            run_event(self.wf_module)
+
+        self.commit_result.assert_called_with(self.wf_module,
+                                              ProcessResult(xlsx_table))
 
     def test_load_xlsx_bad_content(self):
-        url = 'http://test.com/the.xlsx'
-        self.url_pval.set_value(url)
-        self.url_pval.save()
+        self.wf_module.url = 'http://test.com/the.xlsx'
+
+        response = respond(b'hi', XLSX_MIME_TYPE)
 
         # malformed file  should put module in error state
-        with patch('requests.get') as get:
-            get.return_value = mock_bytes_response(b'hi', XLSX_MIME_TYPE)
-            self.press_fetch_button()
-            self.wfmodule.refresh_from_db()
-            self.assertEqual(
-                self.wfmodule.fetch_error,
+        with patch('requests.get', response):
+            run_event(self.wf_module)
+
+        self.commit_result.assert_called_with(
+            self.wf_module,
+            ProcessResult(error=(
                 "Error reading Excel file: Unsupported format, or corrupt "
                 "file: Expected BOF record; found b'hi'"
-            )
+            ))
+        )
 
     def test_load_404(self):
-        url = 'http://test.com/the.csv'
-        self.url_pval.set_value(url)
-        self.url_pval.save()
+        self.wf_module.url = 'http://test.com/the.csv'
 
         # 404 error should put module in error state
-        with patch('requests.get') as get:
-            get.return_value = mock_404_response('Foobar')
-            self.press_fetch_button()
-            self.wfmodule.refresh_from_db()
-            self.assertEqual(self.wfmodule.fetch_error,
-                             'Error 404 fetching url')
+        with patch('requests.get', mock_404_response('Foobar')):
+            run_event(self.wf_module)
+
+        self.commit_result.assert_called_with(
+            self.wf_module,
+            ProcessResult(error='Error 404 fetching url')
+        )
 
     def test_bad_url(self):
-        url = 'not a url'
-        self.url_pval.set_value(url)
-        self.url_pval.save()
+        self.wf_module.url = 'not a url'
 
-        self.press_fetch_button()
-        self.wfmodule.refresh_from_db()
-        self.assertEqual(self.wfmodule.fetch_error, 'Invalid URL')
+        run_event(self.wf_module)
+
+        self.commit_result.assert_called_with(
+            self.wf_module,
+            ProcessResult(error='Invalid URL')
+        )
