cfeb0fad813314128f3051fa65d7810c61607278
emrig <emrig@users.noreply.github.com>
2018-10-18 19:37:27 -0400

    additional tests

--- assets/js/workflows.test.js
+++ assets/js/workflows.test.js
@@ -5,28 +5,31 @@ const Utils = require('./utils');
 import { okResponseMock, jsonResponseMock } from './test-utils'
 
 describe('Workflow list page', () => {
-  const testWorkflows = {
+  var testWorkflows = {
     owned: [
       {
         id: 1,
-        name: "Charting",
+        name: "Cleaning",
         owner_name: 'Fred Frederson',
         public: true,
-        is_owner: true
+        is_owner: true,
+        last_update: '2010-10-18T00:30:00'
       },
       {
         id: 2,
-        name: "Analysis",
+        name: "Charting",
         owner_name: 'Fred Frederson',
         public: false,
-        is_owner: true
+        is_owner: true,
+        last_update: '2010-10-18T00:20:00'
       },
       {
         id: 3,
-        name: "Cleaning",
+        name: "Analysis",
         owner_name: 'Fred Frederson',
         public: false,
-        is_owner: true
+        is_owner: true,
+        last_update: '2010-10-18T07:45:00'
       }
     ],
     shared: [
@@ -35,14 +38,16 @@ describe('Workflow list page', () => {
         name: "Messy data cleanup",
         owner_name: 'John Johnson',
         public: false,
-        is_owner: false
+        is_owner: false,
+        last_update: '2010-10-18T00:30:00'
       },
       {
         id: 8,
         name: "Document search",
         owner_name: 'Sally Sallerson',
         public: true,
-        is_owner: false
+        is_owner: false,
+        last_update: '2010-10-18T00:45:00'
       }
     ],
     templates: [
@@ -51,7 +56,8 @@ describe('Workflow list page', () => {
         name: "Demo 1",
         owner_name: 'Workbench',
         public: false,
-        is_owner: false
+        is_owner: false,
+        last_update: '2010-10-18T00:30:00'
       }
     ]
   }
@@ -70,8 +76,12 @@ describe('Workflow list page', () => {
     public: false,
   }
 
-  var api;
-  var wrapper;
+  const api = {
+    newWorkflow: jsonResponseMock(addResponse),
+    duplicateWorkflow: jsonResponseMock(dupResponse),
+    deleteWorkflow: okResponseMock()
+  }
+  const wrapper = (props) => mount(<Workflows api={api} {...props} />)
 
   let globalGoToUrl
   beforeEach(() => {
@@ -92,61 +102,67 @@ describe('Workflow list page', () => {
     global.confirm = globalConfirm
   })
 
-  // Load the component and give it a list of workflows, before each test
-  beforeEach( () => {
-    api = {
-      newWorkflow: jsonResponseMock(addResponse),
-      duplicateWorkflow: jsonResponseMock(dupResponse),
-      deleteWorkflow: okResponseMock()
-    }
-
-    wrapper = mount(<Workflows api={api} workflows={testWorkflows}/>)
+  // testWorkflows somehow gets modified during testing (even when const), must reset
+  beforeEach(() => {
+    testWorkflows.owned = testWorkflows.owned.filter(wf => wf.id !== 666)
   })
-  afterEach(() => wrapper.unmount())
 
   it('renders correctly', (done) => {
-
     // postpone until promise resolves and our workflows load
     setImmediate( () => {
-      wrapper.update()
-      expect(wrapper).toMatchSnapshot();
+      const w = wrapper({workflows: testWorkflows })
+      expect(w).toMatchSnapshot();
 
       // Make sure there is a context menu for each workflow
-      var menus = wrapper.find('.menu-test-class');
+      var menus = w.find('.menu-test-class');
       expect(menus).toHaveLength(6)
 
       // Make sure there is a metadata line for each workflow in the list
-      menus = wrapper.find('.wf-meta--id');
+      menus = w.find('.wf-meta--id');
       expect(menus).toHaveLength(6)
+
+      // owned tab should have 3 workflows
+      w.find('.nav-link').findWhere(node => node.props().children === 'My workflows').simulate('click')
+      expect(w.find('.tab-pane.active').find('.workflow-item')).toHaveLength(3)
+
+      // shared tab should have 2 workflows
+      w.find('.nav-link').findWhere(node => node.props().children === 'Shared with me').simulate('click')
+      expect(w.find('.tab-pane.active').find('.workflow-item')).toHaveLength(2)
+
+      // template tab should have 1 workflow1
+      w.find('.nav-link').findWhere(node => node.props().children === 'Templates').simulate('click')
+      expect(w.find('.tab-pane.active').find('.workflow-item')).toHaveLength(1)
 
       done();
     })
   });
 
   it('delete a workflow', (done) => {
+    const w = wrapper({workflows: testWorkflows })
     global.confirm.mockReturnValue(true) // pretend the user clicked OK
     // We've clicked delete and now we have to wait for everything to update.
     // see https://facebook.github.io/jest/docs/asynchronous.html
     setImmediate(() => {
       // Shared tab should start with 2 workflows and have 1 after delete
-      wrapper.update()
-      expect(wrapper.find('.tab-pane.active').find('.workflow-item')).toHaveLength(3)
-      wrapper.instance().deleteWorkflow(3)
+      w.update()
+      expect(w.find('.tab-pane.active').find('.workflow-item')).toHaveLength(3)
+      w.instance().deleteWorkflow(3)
 
       setImmediate(() => {
-        wrapper.update()
+        w.update()
         expect(api.deleteWorkflow.mock.calls.length).toBe(1)
         expect(api.deleteWorkflow.mock.calls[0][0]).toBe(3)
-        expect(wrapper.find('.tab-pane.active').find('.workflow-item')).toHaveLength(2) // one fewer workflow
+        expect(w.find('.tab-pane.active').find('.workflow-item')).toHaveLength(2) // one fewer workflow
         done()
       })
     })
   })
 
   it('new workflow button', (done) => {
+    const w = wrapper({workflows: testWorkflows })
     // let 4 workflows load
     setImmediate( () => {
-      var newButton = wrapper.find('.new-workflow-button');
+      var newButton = w.find('.new-workflow-button');
       newButton.first().simulate('click');
 
       setImmediate(() => {
@@ -160,27 +176,88 @@ describe('Workflow list page', () => {
   it('duplicate workflow callback', (done) => {
     // let 2 workflows load in user's shared tab, duplicate 1 and activeTab should get set
     // to owned list with +1 worflow
+    const w = wrapper({workflows: testWorkflows })
     setImmediate( () => {
-      wrapper.update()
-
+      w.update()
       // Owned list should start with 3 WFs, shared with 2
-      expect(wrapper.find('.tab-pane.active').find('.workflow-item')).toHaveLength(3)
-      let sharedTab = wrapper.find('.nav-link').findWhere(node => node.props().children === 'Shared with me')
+      expect(w.find('.tab-pane.active').find('.workflow-item')).toHaveLength(3)
+      let sharedTab = w.find('.nav-link').findWhere(node => node.props().children === 'Shared with me')
       sharedTab.simulate('click')
-      expect(wrapper.find('.tab-pane.active').find('.workflow-item')).toHaveLength(2)
+      expect(w.find('.tab-pane.active').find('.workflow-item')).toHaveLength(2)
 
-      wrapper.instance().duplicateWorkflow(7)
+      w.instance().duplicateWorkflow(7)
 
       // should be a new item at the top of the owned list
       setImmediate(() => {
-        wrapper.update()
+        w.update()
         expect(api.duplicateWorkflow.mock.calls.length).toBe(1)
         expect(api.duplicateWorkflow.mock.calls[0][0]).toBe(7)
 
         // Expect owned tab to now have 4 workflows
-        expect(wrapper.find('.tab-pane.active').find('.workflow-item')).toHaveLength(4)
+        expect(w.find('.tab-pane.active').find('.workflow-item')).toHaveLength(4)
         done()
       })
+    })
+  })
+
+  it('owned pane should have create workflow link when no workflows', (done) => {
+    let modifiedWorkflows = Object.assign({}, testWorkflows)
+    modifiedWorkflows['owned'] = {}
+    const w = wrapper({workflows: modifiedWorkflows})
+    setImmediate( () => {
+      w.update()
+      // Owned list should have no workflows, instead a create workflow link
+      expect(w.find('.tab-pane.active').find('.workflow-item')).toHaveLength(0)
+      expect(w.find('.tab-pane.active').find('.new-workflow-link')).toHaveLength(1)
+      done()
+    })
+  })
+
+  it('shared and template panes should have a placeholder when no workflows', (done) => {
+    let modifiedWorkflows = Object.assign({}, testWorkflows)
+    modifiedWorkflows['shared'] = {}
+    modifiedWorkflows['templates'] = {}
+    const w = wrapper({workflows: modifiedWorkflows})
+    setImmediate( () => {
+      w.update()
+      w.find('.nav-link').findWhere(node => node.props().children === 'Shared with me').simulate('click')
+      expect(w.find('.tab-pane.active').find('.workflow-item')).toHaveLength(0)
+      expect(w.find('.tab-pane.active').find('.placeholder')).toHaveLength(1)
+      w.find('.nav-link').findWhere(node => node.props().children === 'Templates').simulate('click')
+      expect(w.find('.tab-pane.active').find('.workflow-item')).toHaveLength(0)
+      expect(w.find('.tab-pane.active').find('.placeholder')).toHaveLength(1)
+      done()
+    })
+  })
+
+  it('should sort properly by date and name', (done) => {
+    const w = wrapper({workflows: testWorkflows})
+    setImmediate( () => {
+      // sort by date ascending
+      w.update()
+      w.find('button.test-sort-date-ascending').simulate('click')
+      expect(w.find('.tab-pane.active').find('.workflow-item').get(0).key).toEqual('2')
+      expect(w.find('.tab-pane.active').find('.workflow-item').get(1).key).toEqual('1')
+      expect(w.find('.tab-pane.active').find('.workflow-item').get(2).key).toEqual('3')
+      // sort by date descending
+      w.update()
+      w.find('button.test-sort-date-descending').simulate('click')
+      expect(w.find('.tab-pane.active').find('.workflow-item').get(0).key).toEqual('3')
+      expect(w.find('.tab-pane.active').find('.workflow-item').get(1).key).toEqual('1')
+      expect(w.find('.tab-pane.active').find('.workflow-item').get(2).key).toEqual('2')
+      // sort by name ascending
+      w.update()
+      w.find('button.test-sort-name-ascending').simulate('click')
+      expect(w.find('.tab-pane.active').find('.workflow-item').get(0).key).toEqual('3')
+      expect(w.find('.tab-pane.active').find('.workflow-item').get(1).key).toEqual('2')
+      expect(w.find('.tab-pane.active').find('.workflow-item').get(2).key).toEqual('1')
+      // sort by name descending
+      w.update()
+      w.find('button.test-sort-name-descending').simulate('click')
+      expect(w.find('.tab-pane.active').find('.workflow-item').get(0).key).toEqual('1')
+      expect(w.find('.tab-pane.active').find('.workflow-item').get(1).key).toEqual('2')
+      expect(w.find('.tab-pane.active').find('.workflow-item').get(2).key).toEqual('3')
+      done()
     })
   })
 })
