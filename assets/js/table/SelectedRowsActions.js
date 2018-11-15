72abc8f939d6deb5eff5ec8642b2c2f7f2d0723f
Pierre Conti <pierre@tablesdata.com>
2018-11-15 15:52:55 -0800

    Table action button visible at all time

--- assets/js/table/SelectedRowsActions.js
+++ assets/js/table/SelectedRowsActions.js
@@ -93,7 +93,14 @@ export class SelectedRowsActions extends React.PureComponent {
   render () {
     const { selectedRowIndexes, wfModuleId, rowActionModules } = this.props
 
-    if (!wfModuleId || selectedRowIndexes.length === 0) return null
+    if (!wfModuleId || selectedRowIndexes.length === 0)
+    return (
+      <UncontrolledDropdown>
+        <DropdownToggle title='menu' className="table-action">
+          No selection
+        </DropdownToggle>
+      </UncontrolledDropdown>
+    )
 
     const actions = rowActionModules.map(({ id, title }) => (
       <Action key={id} id={id} title={title} onClick={this.onClickAction} />
