ac5700206ea44ecba98c3d1f7366b14f2b1ffeb0
emrig <emrig@users.noreply.github.com>
2018-11-20 21:22:16 -0500

    separate withFetchedData

--- assets/js/wfparameters/Refine.js
+++ assets/js/wfparameters/Refine.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import RefineModal from '../refine/RefineModal'
+import { withFetchedData } from './FetchedData'
 
 const NumberFormatter = new Intl.NumberFormat()
 
@@ -628,66 +629,4 @@ export class Refine extends React.PureComponent {
   }
 }
 
-// https://reactjs.org/docs/higher-order-components.html
-//
-// Let's keep this generic and maybe make it into a reusable util later.
-export default function withFetchedData(WrappedComponent, dataName) {
-  return class extends React.PureComponent {
-    static propTypes = {
-      fetchData: PropTypes.func.isRequired, // fn() => Promise[data]
-      fetchDataCacheId: PropTypes.string.isRequired, // unique string: when it changes, call fetchData()
-    }
-
-    state = {
-      data: null,
-      loading: false
-    }
-
-    _reload () {
-      const fetchDataCacheId = this.props.fetchDataCacheId
-
-      // Keep state.data unchanged, until we solve
-      // https://www.pivotaltracker.com/story/show/158034731. After that, we
-      // should probably change this to setState({data: null})
-      this.setState({ loading: true })
-
-      this.props.fetchData()
-        .then(data => {
-          if (this.unmounted) return
-          if (this.props.fetchDataCacheId !== fetchDataCacheId) return // ignore wrong response in race
-
-          this.setState({
-            loading: false,
-            data
-          })
-        })
-    }
-
-    componentDidMount () {
-      this._reload()
-    }
-
-    componentWillUnmount () {
-      this.unmounted = true
-    }
-
-    componentDidUpdate (prevProps) {
-      if (prevProps.fetchDataCacheId !== this.props.fetchDataCacheId) {
-        this._reload()
-      }
-    }
-
-    render () {
-      const props = Object.assign({
-        [dataName]: this.state.data,
-        loading: this.state.loading
-      }, this.props)
-      delete props.fetchData
-      delete props.fetchDataCacheId
-
-      return <WrappedComponent {...props} />
-    }
-  }
-}
-
-//export default withFetchedData(Refine, 'valueCounts')
+export default withFetchedData(Refine, 'valueCounts')
