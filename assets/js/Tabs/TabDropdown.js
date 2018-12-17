698f7fa70796b7904c6ee9b92888e740e86480f9
Adam Hooper <adam@adamhooper.com>
2018-12-17 12:33:45 -0500

    Fix tab-dropdown placement and icons

--- assets/js/Tabs/TabDropdown.js
+++ assets/js/Tabs/TabDropdown.js
@@ -9,9 +9,9 @@ export default function TabContextMenu ({ onClickRename, onClickDelete }) {
   return (
     <UncontrolledDropdown direction='up'>
       <DropdownToggle caret color='link'></DropdownToggle>
-      <DropdownMenu positionFixed>
-        <DropdownItem onClick={onClickRename}><i className='icon-rename'></i> Rename</DropdownItem>
-        <DropdownItem onClick={onClickDelete}><i className='icon-delete'></i> Delete</DropdownItem>
+      <DropdownMenu positionFixed right>
+        <DropdownItem onClick={onClickRename}><i className='icon-edit'></i> Rename</DropdownItem>
+        <DropdownItem onClick={onClickDelete}><i className='icon-removec'></i> Delete</DropdownItem>
       </DropdownMenu>
     </UncontrolledDropdown>
   )
