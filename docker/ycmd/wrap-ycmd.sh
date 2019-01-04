7f8db80309d997a06a9af178b8c54b5ef686a67b
Adam Hooper <adam@adamhooper.com>
2019-01-04 13:05:31 -0500

    wrap-ycmd.sh: only in _this_ directory

--- docker/ycmd/wrap-ycmd.sh
+++ docker/ycmd/wrap-ycmd.sh
@@ -3,7 +3,7 @@
 set -e
 
 # If the CWD isn't cjworkbench's, don't use it
-if [[ "$(pwd)" =~ /cjworkbench/ ]]; then
+if [[ "$(basename "$(pwd)")" = 'cjworkbench' ]]; then
   # YouCompleteMe tries to run "python /path/to/ycmd ...args".
   # Make it exec "docker run ycmd ...args" -- that is, nix /path/to/ycmd.
   if expr match "$1" ".*/ycmd$" >/dev/null; then
