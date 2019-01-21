7f3817ca9e219bd7cd9f3a895b554b2e1e2a64e8
Adam Hooper <adam@adamhooper.com>
2019-01-21 11:30:43 -0500

    Subfilter.js: handle no colnames
    [finishes #163211913]

--- assets/js/params/Custom/Filters/Subfilter.js
+++ assets/js/params/Custom/Filters/Subfilter.js
@@ -56,7 +56,7 @@ export default class Subfilter extends React.PureComponent {
 
   render () {
     const { isReadOnly, inputColumns, name, index, value, onDelete, onSubmit } = this.props
-    const column = inputColumns.find(c => c.name === value.colname) || null
+    const column = (inputColumns || []).find(c => c.name === value.colname) || null
     const needValue = column !== null && (
       value.condition !== 'cell_is_empty' && value.condition !== 'cell_is_not_empty'
     )
