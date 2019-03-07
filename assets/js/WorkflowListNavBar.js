0ef10fa17d009b10c3e9c1df2fb325077d23b77b
Adam Hooper <adam@adamhooper.com>
2019-03-07 11:36:17 -0800

    Fix navbar links from course pages
    Also, simplify styles. Lots.

--- assets/js/WorkflowListNavBar.js
+++ assets/js/WorkflowListNavBar.js
@@ -1,30 +1,21 @@
 import React from 'react'
 import WfHamburgerMenu from './WfHamburgerMenu'
 
+function getActiveSection () {
+  return window.location.pathname.startsWith('/workflows') ? 'workflows' : 'lessons'
+}
+
+function propsForLink (activeSection, name) {
+  if (name === activeSection) {
+    return { href: null, className: 'active' }
+  } else {
+    return { href: `/${name}`, className: 'inactive' }
+  }
+}
+
 export default class WorkflowListNavBar extends React.Component {
   render() {
-    // For now, we're hiding Lessons; so there are no links on the page. Only
-    // show the links when we're on the /lessons/ page.
-    const hideLinksBecauseFeatureIsNotFinished = window.location.href.indexOf('/lessons/') === -1
-
-    const activeLink = window.location.href.indexOf('/lessons') === -1 ? 'workflows' : 'lessons'
-    const propsForLink = (name) => {
-      const activeProps = { href: null, className: 'active' }
-      const inactiveProps = { className: 'inactive' }
-      return activeLink === name ? activeProps : inactiveProps
-    }
-    const links = (
-      <div className="links">
-        <a href="/workflows/" {...propsForLink('workflows')} className="WF-toggle--link">
-          <div>WORKFLOWS</div>
-          <div className='WF-link--under'></div>
-        </a>
-        <a href="/lessons/" {...propsForLink('lessons')} className="LS-toggle--link">
-          <div>Tutorials</div>
-          <div className='LS-link--under'></div>
-        </a>
-      </div>
-    )
+    const activeSection = getActiveSection()
 
     return (
       <div>
@@ -33,7 +24,10 @@ export default class WorkflowListNavBar extends React.Component {
             <img src={`${window.STATIC_URL}images/logo.svg`} className="logo"/>
             <img src={`${window.STATIC_URL}images/logo-text.svg`} className="logo-text"/>
           </div>
-          {links}
+          <div className="links">
+            <a {...propsForLink(activeSection, 'workflows')}>WORKFLOWS</a>
+            <a {...propsForLink(activeSection, 'lessons')}>TUTORIALS</a>
+          </div>
           <WfHamburgerMenu />
         </nav>
       </div>
