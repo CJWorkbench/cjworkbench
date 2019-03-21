7c5d721c7f33e0a5c9b0af8c78a059024de33a27
Adam Hooper <adam@adamhooper.com>
2019-03-21 13:29:10 -0400

    ModalFooter: de-reactstrap

--- assets/js/components/Modal/ModalFooter.js
+++ assets/js/components/Modal/ModalFooter.js
@@ -1,2 +1,11 @@
-import ModalFooter from 'reactstrap/lib/ModalFooter'
-export default ModalFooter
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function ModalFooter ({ children }) {
+  return (
+    <div className='modal-footer' children={children} />
+  )
+}
+ModalFooter.propTypes = {
+  children: PropTypes.node.isRequired
+}
