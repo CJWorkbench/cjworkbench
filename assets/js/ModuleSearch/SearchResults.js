f8556c24bc99033af95a4aeba7a12801d0a43dfd
Adam Hooper <adam@adamhooper.com>
2019-03-22 14:19:58 -0400

    ModuleSearch: refactor SearchResults, too
    [refs #164609157] because now we're ready to begin!

---
+++ assets/js/ModuleSearch/SearchResults.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import memoize from 'memoize-one'
+import { ModulePropType } from './PropTypes'
+import SearchResultGroup from './SearchResultGroup'
+
+const GroupOrder = {
+  // dont use 0 -- we use the "||" operator to detect misses
+  'Add data': 1,
+  'Scrape': 2,
+  'Clean': 3,
+  'Analyze': 4,
+  'Visualize': 5,
+  'Code': 6,
+}
+
+function compareGroups(a, b) {
+  const ai = GroupOrder[a.name] || 99;
+  const bi = GroupOrder[b.name] || 99;
+  return ai - bi;
+}
+
+// Function to sort modules in alphabetical order
+function compareModules(a, b) {
+  if (a.name.toLowerCase() < b.name.toLowerCase()) return -1
+  else if (a.name.toLowerCase() > b.name.toLowerCase()) return 1
+  else return 0
+}
+
+/**
+ * Return [ { name: 'Clean', modules: [ ... ] }, ... ]
+ */
+function groupModules(items) {
+  const ret = []
+  const temp = {}
+  items.sort(compareModules)
+
+  items.forEach(item => {
+    if (temp[item.category]) {
+      temp[item.category].push(item)
+    } else {
+      const obj = { name: item.category, modules: [ item ] }
+      temp[item.category] = obj.modules
+      ret.push(obj)
+    }
+  })
+
+  ret.sort(compareGroups)
+
+  return ret
+}
+
+const escapeRegexCharacters = (str) => {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+export default class SearchResults extends React.PureComponent {
+  static propTypes = {
+    search: PropTypes.string.isRequired, // search string, to filter modules
+    modules: PropTypes.arrayOf(ModulePropType.isRequired).isRequired,
+    onClickModule: PropTypes.func.isRequired, // func(moduleIdName) => undefined
+  }
+
+  state = {
+    activeModule: null // idName
+  }
+
+  get resultGroups () {
+    return this._buildResultGroups(this.allGroups, this.props.search)
+  }
+
+  get allGroups () {
+    return this._buildAllGroups(this.props.modules)
+  }
+
+  _buildAllGroups = memoize(groupModules)
+
+  _buildResultGroups = memoize((groups, search) => {
+    const escapedValue = escapeRegexCharacters(search.trim())
+    const regex = new RegExp(escapedValue, 'i')
+    const predicate = (module) => regex.test(module.name) || regex.test(module.description)
+    return groups
+      .map(({ name, modules }) => ({ name, modules: modules.filter(predicate) }))
+      .filter(({ modules }) => modules.length > 0)
+  })
+
+  onMouseEnterModule = (moduleIdName) => {
+    this.setState({ activeModule: moduleIdName })
+  }
+
+  render () {
+    const { modules, onClickModule } = this.props
+    const { activeModule } = this.state
+
+    return (
+      <ul className='module-search-result-groups'>
+        {this.resultGroups.map(rg => (
+          <SearchResultGroup
+            key={rg.name}
+            name={rg.name}
+            modules={rg.modules}
+            activeModule={activeModule}
+            onClickModule={onClickModule}
+            onMouseEnterModule={this.onMouseEnterModule}
+          />
+        ))}
+      </ul>
+    )
+  }
+}
--- assets/js/ModuleSearch/index.js
+++ assets/js/ModuleSearch/index.js
@@ -6,62 +6,11 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import memoize from 'memoize-one'
 import { ModulePropType } from './PropTypes'
 import Prompt from './Prompt'
-import SearchResultGroup from './SearchResultGroup'
+import SearchResults from './SearchResults'
 
 import lessonSelector from '../lessons/lessonSelector'
-
-const GroupOrder = {
-  // dont use 0 -- we use the "||" operator to detect misses
-  'Add data': 1,
-  'Scrape': 2,
-  'Clean': 3,
-  'Analyze': 4,
-  'Visualize': 5,
-  'Code': 6,
-}
-
-function compareGroups(a, b) {
-  const ai = GroupOrder[a.name] || 99;
-  const bi = GroupOrder[b.name] || 99;
-  return ai - bi;
-}
-
-// Function to sort modules in alphabetical order
-function compareModules(a, b) {
-  if (a.name.toLowerCase() < b.name.toLowerCase()) return -1
-  else if (a.name.toLowerCase() > b.name.toLowerCase()) return 1
-  else return 0
-}
-
-/**
- * Return [ { name: 'Clean', modules: [ ... ] }, ... ]
- */
-function groupModules(items) {
-  const ret = []
-  const temp = {}
-  items.sort(compareModules)
-
-  items.forEach(item => {
-    if (temp[item.category]) {
-      temp[item.category].push(item)
-    } else {
-      const obj = { name: item.category, modules: [ item ] }
-      temp[item.category] = obj.modules
-      ret.push(obj)
-    }
-  })
-
-  ret.sort(compareGroups)
-
-  return ret
-}
-
-const escapeRegexCharacters = (str) => {
-  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-}
 
 export class ModuleSearch extends React.Component {
   static propTypes = {
@@ -73,73 +22,34 @@ export class ModuleSearch extends React.Component {
   }
 
   state = {
-    input: '',
-    activeModule: null // idName
+    search: ''
   }
-
-  get resultGroups () {
-    return this._buildResultGroups(this.allGroups, this.state.input)
-  }
-
-  get allGroups () {
-    return this._buildAllGroups(this.props.modules)
-  }
-
-  _buildAllGroups = memoize(groupModules)
-
-  _buildResultGroups = memoize((groups, input) => {
-    const escapedValue = escapeRegexCharacters(input.trim())
-    const regex = new RegExp(escapedValue, 'i')
-    const predicate = (module) => regex.test(module.name) || regex.test(module.description)
-    return groups
-      .map(({ name, modules }) => ({ name, modules: modules.filter(predicate) }))
-      .filter(({ modules }) => modules.length > 0)
-  })
 
   onSearchInputChange = (value) => {
-    this.setState({ input: value })
+    this.setState({ search: value })
   }
 
   onClickModule = (moduleIdName) => {
-    this.setState({ input: '' })
+    this.setState({ search: '' })
     this.props.onClickModule(moduleIdName)
   }
 
-  onMouseEnterModule = (moduleIdName) => {
-    this.setState({ activeModule: moduleIdName })
-  }
-
   cancel = () => {
-    this.setState({ input: '' })
+    this.setState({ search: '' })
     this.props.onCancel()
   }
 
   render () {
-    const { input, activeModule } = this.state
-
-    const resultGroupComponents = this.resultGroups.map(rg => (
-      <SearchResultGroup
-        key={rg.name}
-        name={rg.name}
-        modules={rg.modules}
-        activeModule={activeModule}
-        onClickModule={this.onClickModule}
-        onMouseEnterModule={this.onMouseEnterModule}
-      />
-    ))
-    const resultGroupsComponent = (
-      <ul className="module-search-result-groups">
-        {resultGroupComponents}
-      </ul>
-    )
+    const { modules } = this.props
+    const { search } = this.state
 
     const className = [ 'module-search' ]
     if (this.props.isLessonHighlight) className.push('lesson-highlight')
 
     return (
       <div className={className.join(' ')}>
-        <Prompt value={input} cancel={this.cancel} onChange={this.onSearchInputChange} />
-        {resultGroupsComponent}
+        <Prompt value={search} cancel={this.cancel} onChange={this.onSearchInputChange} />
+        <SearchResults search={search} modules={modules} onClickModule={this.onClickModule} />
       </div>
     )
   }
