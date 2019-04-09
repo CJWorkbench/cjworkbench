278fb0472815897e5deba8b0621afe9d60231586
Adam Hooper <adam@adamhooper.com>
2019-04-09 17:12:40 -0400

    converttotext: internal now; respect type formats
    We don't have a solid way to pass code to modules yet. So we import
    NumberFormatter in converttotext, which means we need to be internal.
    (Normally, "things are under-designed" is a bad reason to internalize
    a module. But converttotext _screams_ to be internal -- it's a core
    feature -- so now is a great time to do it.)
    
    There's a migration to change the module id_name: the original id_name
    is incompatible with internal modules. Grr. So this deploy will be a
    bit scary.

---
+++ server/tests/modules/test_converttotext.py
@@ -0,0 +1,89 @@
+import unittest
+import numpy as np
+import pandas as pd
+from pandas.testing import assert_frame_equal
+from cjworkbench.types import RenderColumn
+from server.modules.converttotext import render
+
+
+class TestConvertText(unittest.TestCase):
+    def setUp(self):
+        self.table = pd.DataFrame([
+            [99,   100,   '2018', None],
+            [99,   100,   '2018', None]],
+            columns=['intcol', 'floatcol', 'datecol', 'nullcol'])
+
+        self.table['intcol'] = self.table['intcol'].astype(int)
+        self.table['floatcol'] = self.table['floatcol'].astype(float)
+        self.table['datecol'] = self.table['datecol'].astype(np.datetime64)
+        self.table['nullcol'] = self.table['nullcol'].astype(float)
+
+    def test_NOP(self):
+        # should NOP when first applied
+        result = render(
+            pd.DataFrame({'A': [0.006]}),
+            {'colnames': ''},
+            input_columns={
+                'A': RenderColumn('A', 'number', '{:.2f}'),
+            }
+        )
+        assert_frame_equal(result, pd.DataFrame({'A': [0.006]}))
+
+    def test_convert_str(self):
+        result = render(
+            pd.DataFrame({'A': ['a']}),
+            {'colnames': 'A'},
+            input_columns={
+                'A': RenderColumn('A', 'text', None),
+            }
+        )
+        assert_frame_equal(result, pd.DataFrame({'A': ['a']}))
+
+    def test_convert_int(self):
+        result = render(
+            pd.DataFrame({'A': [1], 'B': [2]}),
+            {'colnames': 'A,B'},
+            input_columns={
+                'A': RenderColumn('A', 'number', '{:.2f}'),
+                'B': RenderColumn('B', 'number', '{:d}'),
+            }
+        )
+        assert_frame_equal(result, pd.DataFrame({'A': ['1.00'], 'B': ['2']}))
+
+    def test_convert_float(self):
+        result = render(
+            pd.DataFrame({'A': [1.111], 'B': [2.6]}),
+            {'colnames': 'A,B'},
+            input_columns={
+                'A': RenderColumn('A', 'number', '{:.2f}'),
+                'B': RenderColumn('B', 'number', '{:d}'),
+            }
+        )
+        assert_frame_equal(result, pd.DataFrame({'A': ['1.11'], 'B': ['2']}))
+
+    def test_convert_datetime(self):
+        result = render(
+            pd.DataFrame({
+                'A': [np.datetime64('2018-01-01'), np.datetime64('2019-02-13')]
+            }),
+            {'colnames': 'A'},
+            input_columns={
+                'A': RenderColumn('A', 'datetime', None),
+            }
+        )
+        assert_frame_equal(result,
+                           pd.DataFrame({'A': ['2018-01-01', '2019-02-13']}))
+
+    def test_convert_null(self):
+        result = render(
+            pd.DataFrame({'A': [1, np.nan]}),
+            {'colnames': 'A'},
+            input_columns={
+                'A': RenderColumn('A', 'number', '{:,d}'),
+            }
+        )
+        assert_frame_equal(result, pd.DataFrame({'A': ['1', np.nan]}))
+
+
+if __name__ == '__main__':
+    unittest.main()
