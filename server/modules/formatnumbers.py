112f69493c4861b0845af6ce96819f855aa3ecc1
Adam Hooper <adam@adamhooper.com>
2019-04-15 16:03:06 -0400

    formatnumbers: require number; label field "Display As"

--- server/modules/formatnumbers.py
+++ server/modules/formatnumbers.py
@@ -1,16 +1,8 @@
-def render(table, params, *, input_columns):
+def render(table, params):
     colnames = [c for c in params['colnames'].split(',') if c]
 
     if not colnames:
         return table
-
-    for colname in colnames:
-        coltype = input_columns[colname].type
-        if coltype != 'number':
-            return (
-                'Cannot format column "%s" because it is of type "%s".'
-                % (colname, coltype)
-            )
 
     # Don't edit `table` at all. Just set new column_formats.
     return {
--- server/tests/modules/test_formatnumbers.py
+++ server/tests/modules/test_formatnumbers.py
@@ -9,33 +9,16 @@ class FormatnumbersTest(unittest.TestCase):
     def test_render_empty_is_no_op(self):
         result = render(
             pd.DataFrame({'A': [1]}),
-            {'colnames': '', 'format': 'X{:d}'},
-            input_columns={'A': RenderColumn('A', 'number', '{:d}')}
+            {'colnames': '', 'format': 'X{:d}'}
         )
         assert_frame_equal(result, pd.DataFrame({'A': [1]}))
 
     def test_render_multiple_columns(self):
         result = render(
             pd.DataFrame({'A': [1], 'B': [2], 'C': [3]}),
-            {'colnames': 'A,B', 'format': 'X{:d}'},
-            input_columns={
-                'A': RenderColumn('A', 'number', 'A{:d}'),
-                'B': RenderColumn('B', 'number', 'B{:d}'),
-                'C': RenderColumn('C', 'number', 'C{:d}'),
-            }
+            {'colnames': 'A,B', 'format': 'X{:d}'}
         )
         assert_frame_equal(result['dataframe'],
                            pd.DataFrame({'A': [1], 'B': [2], 'C': [3]}))
         self.assertEqual(result['column_formats'],
                          {'A': 'X{:d}', 'B': 'X{:d}'})
-
-    def test_render_non_number_is_error(self):
-        result = render(
-            pd.DataFrame({'A': ['x']}),
-            {'colnames': 'A', 'format': 'X{:d}'},
-            input_columns={'A': RenderColumn('A', 'text', None)}
-        )
-        self.assertEqual(
-            result,
-            'Cannot format column "A" because it is of type "text".'
-        )
