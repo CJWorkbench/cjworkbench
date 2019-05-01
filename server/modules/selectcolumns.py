83c60b293e33c5d7e34719006333d1c1cf605263
Adam Hooper <adam@adamhooper.com>
2019-05-01 16:51:40 -0400

    Migrate params.
    * Allow columns with commas [refs #160463316]
    * Convert old-style radio to new-style boolean radio
    * Clean up -- parse() is overkill now that input params are exactly what
      we want.

--- server/modules/selectcolumns.py
+++ server/modules/selectcolumns.py
@@ -1,6 +1,6 @@
 import re
 from typing import Tuple
-from pandas import IntervalIndex
+import pandas as pd
 from .utils import parse_multicolumn_param
 
 
@@ -8,50 +8,20 @@ commas = re.compile(r'\s*,\s*')
 numbers = re.compile(r'(?P<first>[1-9]\d*)(?:-(?P<last>[1-9]\d*))?')
 
 
-Drop = 0
-Keep = 1
-
-
-def select_columns_by_name(table, cols, drop_or_keep):
-    # if no column has been selected, keep the columns
-    if not cols:
-        return table
-
-    # ensure we do not change the order of the columns, even if they are
-    # listed in another order this also silently removes any nonexistent
-    # columns (can happen when re-ordering module, etc.)
-    newcols = table.columns.intersection(set(cols))
-
-    if drop_or_keep == Keep:
-        table2 = table[newcols]
-    else:
-        table2 = table.drop(newcols, axis=1)
-    return table2
-
-
-def select_columns_by_number(table, str_col_nums, drop_or_keep):
-    try:
-        form = Form.parse(str_col_nums)
-        if not form:
-            return table
-    except ValueError as err:
-        return str(err)
+def select_columns_by_number(table, str_col_nums):
+    """
+    Return a list of column names, or raise ValueError.
+    """
+    index = parse_interval_index(str_col_nums)  # raises ValueError
 
     table_col_nums = list(range(0, len(table.columns)))
 
     try:
-        mask = form.index.get_indexer(table_col_nums) != -1
+        mask = index.get_indexer(table_col_nums) != -1
     except KeyError:
-        return 'There are overlapping numbers in input range'
-    except Exception as err:  # TODO huh? What exceptions?
-        return str(err.args[0])
+        raise ValueError('There are overlapping numbers in input range')
 
-    if drop_or_keep == Drop:
-        new_col_nums = [x[0] for x in enumerate(mask) if not x[1]]
-    else:
-        new_col_nums = [x[0] for x in enumerate(mask) if x[1]]
-    ret = table.iloc[:, new_col_nums]
-    return ret
+    return list(table.columns[mask])
 
 
 def parse_interval(s: str) -> Tuple[int, int]:
@@ -66,12 +36,12 @@ def parse_interval(s: str) -> Tuple[int, int]:
     >>> parse_interval('hi')
     Traceback (most recent call last):
         ...
-    ValueError: Rows must look like "1-2", "5" or "1-2, 5"; got "hi"
+    ValueError: Column numbers must look like "1-2", "5" or "1-2, 5"; got "hi"
     """
     match = numbers.fullmatch(s)
     if not match:
         raise ValueError(
-            f'Rows must look like "1-2", "5" or "1-2, 5"; got "{s}"'
+            f'Column numbers must look like "1-2", "5" or "1-2, 5"; got "{s}"'
         )
 
     first = int(match.group('first'))
@@ -79,36 +49,56 @@ def parse_interval(s: str) -> Tuple[int, int]:
     return (first - 1, last - 1)
 
 
-# Copied from droprowsbyposition.py and modified for columns
-class Form:
-    def __init__(self, index: IntervalIndex):
-        self.index = index
+# See also droprowsbyposition, which inspired this logic.
+def parse_interval_index(column_numbers: str) -> pd.IntervalIndex:
+    """
+    Turn a string like '2,3-5' to a pd.IntervalIndex.
 
-    @staticmethod
-    def parse_v1(column_numbers: str) -> 'Form':
-        """Parse 'column_numbers', or raise ValueError"""
-        tuples = [parse_interval(s)
-                  for s in commas.split(column_numbers.strip())]
-        return Form(IntervalIndex.from_tuples(tuples, closed='both'))
+    Return an empty index on empty string.
 
-    @staticmethod
-    def parse(str_col_nums: str) -> 'Form':
-        try:
-            if not str_col_nums:
-                return None
-            return Form.parse_v1(str_col_nums)
-        except KeyError:
-            return None
+    Raise ValueError on invalid string.
+    """
+    tuples = [parse_interval(s)
+              for s in commas.split(column_numbers.strip())]
+    return pd.IntervalIndex.from_tuples(tuples, closed='both')
 
 
 def render(table, params, **kwargs):
-    drop_or_keep: int = params['drop_or_keep']
-    select_range: bool = params['select_range']
-
-    if not select_range:
-        cols, _ = parse_multicolumn_param(params['colnames'], table)
-        return select_columns_by_name(table, cols, drop_or_keep)
-
+    if params['select_range']:
+        try:
+            columns = select_columns_by_number(table, params['column_numbers'])
+        except ValueError as err:
+            return str(err)
     else:
-        str_col_nums: str = params['column_numbers']
-        return select_columns_by_number(table, str_col_nums, drop_or_keep)
+        columns = params['colnames']
+
+    # if no column has been selected, keep the columns
+    if not columns:
+        return table
+
+    if not params['keep']:
+        # Invert "columns", maintaining the order from the input table.
+        drop_columns = set(columns)
+        columns = [c for c in table.columns if c not in drop_columns]
+
+    return table[columns]
+
+
+def _migrate_params_v0_to_v1(params):
+    """
+    v0: colnames are comma-separated str; drop_or_keep is 0|1 drop|keep.
+
+    v1: colnames are List[str]; keep is bool.
+    """
+    return {
+        'colnames': [c for c in params['colnames'].split(',') if c],
+        'select_range': params['select_range'],
+        'column_numbers': params['column_numbers'],
+        'keep': params['drop_or_keep'] != 0,
+    }
+
+
+def migrate_params(params):
+    if 'drop_or_keep' in params:
+        params = _migrate_params_v0_to_v1(params)
+    return params
--- server/tests/modules/test_selectcolumns.py
+++ server/tests/modules/test_selectcolumns.py
@@ -1,60 +1,103 @@
+from typing import Any, Dict, List
 import unittest
 import pandas as pd
 from pandas.testing import assert_frame_equal
-from server.modules import selectcolumns
+from server.modules.selectcolumns import migrate_params, render
 
 
-def P(colnames, drop_or_keep, select_range, column_numbers):
+class MigrateParamsTest(unittest.TestCase):
+    def test_v0_no_colnames(self):
+        self.assertEqual(migrate_params({
+            'colnames': '',
+            'select_range': False,
+            'column_numbers': '',
+            'drop_or_keep': 0,
+        }), {
+            'colnames': [],
+            'select_range': False,
+            'column_numbers': '',
+            'keep': False,
+        })
+
+    def test_v0_colnames(self):
+        self.assertEqual(migrate_params({
+            'colnames': 'A,B',
+            'select_range': False,
+            'column_numbers': '',
+            'drop_or_keep': 1,
+        }), {
+            'colnames': ['A', 'B'],
+            'select_range': False,
+            'column_numbers': '',
+            'keep': True,
+        })
+
+
+def P(colnames: List[str] = [], select_range: bool = False,
+      column_numbers: str = '', keep: bool = True) -> Dict[str, Any]:
+    """Build params dict."""
     return {
         'colnames': colnames,
-        'drop_or_keep': drop_or_keep,
         'select_range': select_range,
         'column_numbers': column_numbers,
+        'keep': keep,
     }
 
 
-def render(table, colnames, drop_or_keep, select_range, column_numbers):
-    params = P(colnames, drop_or_keep, select_range, column_numbers)
-    return selectcolumns.render(table, params)
-
-
-class SelectColumnsTests(unittest.TestCase):
-    def setUp(self):
-        super().setUp()
-        self.table = pd.DataFrame({'A': [1, 2], 'B': [2, 3], 'C': [3, 4]})
-
+class RenderTest(unittest.TestCase):
     def test_render_single_column(self):
-        result = render(self.table, 'A', 1, False, '')
+        table = pd.DataFrame({'A': [1, 2], 'B': [2, 3], 'C': [3, 4]})
+        result = render(table, P(['A']))
         assert_frame_equal(result, pd.DataFrame({'A': [1, 2]}))
 
     def test_render_maintain_input_column_order(self):
-        result = render(self.table, 'B,A', 1, False, '')
-        assert_frame_equal(result, pd.DataFrame({'A': [1, 2], 'B': [2, 3]}))
-
-    def test_render_ignore_invalid_column_name(self):
-        result = render(self.table, 'A,X', 1, False, '')
-        assert_frame_equal(result, pd.DataFrame({'A': [1, 2]}))
+        table = pd.DataFrame({'A': [1, 2], 'B': [2, 3], 'C': [3, 4]})
+        result = render(table, P(['B'], keep=False))
+        assert_frame_equal(result, pd.DataFrame({'A': [1, 2], 'C': [3, 4]}))
 
     def test_render_drop_columns(self):
-        result = render(self.table, 'B,C', 0, False, '')
+        table = pd.DataFrame({'A': [1, 2], 'B': [2, 3], 'C': [3, 4]})
+        result = render(table, P(['B', 'C'], keep=False))
         assert_frame_equal(result, pd.DataFrame({'A': [1, 2]}))
 
+    def test_render_range_ignore_empty_range(self):
+        table = pd.DataFrame({'A': [1, 2], 'B': [2, 3], 'C': [3, 4]})
+        result = render(table.copy(), P(select_range=True, column_numbers=''))
+        self.assertEqual(
+            result,
+            'Column numbers must look like "1-2", "5" or "1-2, 5"; got ""'
+        )
+
     def test_render_range_comma_separated(self):
-        result = render(self.table.copy(), '', 1, True, '1,3')
-        assert_frame_equal(result, self.table[['A', 'C']])
+        table = pd.DataFrame({'A': [1, 2], 'B': [2, 3], 'C': [3, 4]})
+        result = render(table, P(select_range=True, column_numbers='1,3'))
+        assert_frame_equal(result, pd.DataFrame({'A': [1, 2], 'C': [3, 4]}))
 
     def test_render_range_hyphen_separated(self):
-        result = render(self.table.copy(), '', 0, True, '2-3')
-        assert_frame_equal(result, pd.DataFrame(self.table[['A']]))
+        table = pd.DataFrame({'A': [1, 2], 'B': [2, 3], 'C': [3, 4]})
+        result = render(table,
+                        P(select_range=True, column_numbers='2-3', keep=False))
+        assert_frame_equal(result, pd.DataFrame({'A': [1, 2]}))
 
     def test_render_range_overlapping_ranges(self):
-        result = render(self.table.copy(), '', 0, True, '2-3,2')
+        table = pd.DataFrame({'A': [1, 2], 'B': [2, 3], 'C': [3, 4]})
+        result = render(table, P(select_range=True, column_numbers='2-3,2'))
         self.assertEqual(result,
                          'There are overlapping numbers in input range')
 
-    def test_render_range_non_numeric_ranges(self):
-        result = render(self.table.copy(), '', 0, True, '2-3,giraffe')
+    def test_render_range_clamp_range(self):
+        table = pd.DataFrame({'A': [1, 2], 'B': [2, 3], 'C': [3, 4]})
+        result = render(table, P(select_range=True, column_numbers='-1,2,6'))
         self.assertEqual(
             result,
-            'Rows must look like "1-2", "5" or "1-2, 5"; got "giraffe"'
+            'Column numbers must look like "1-2", "5" or "1-2, 5"; got "-1"'
         )
+
+    def test_render_range_non_numeric_ranges(self):
+        table = pd.DataFrame({'A': [1, 2], 'B': [2, 3], 'C': [3, 4]})
+        result = render(table,
+                        P(select_range=True, column_numbers='2-3,giraffe'))
+        self.assertEqual(result, (
+            'Column numbers must look like "1-2", "5" or "1-2, 5"; '
+            'got "giraffe"'
+        ))
