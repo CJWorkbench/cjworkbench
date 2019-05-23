c854e2162958490f2fbe69635b6afea02e9d6845
Adam Hooper <adam@adamhooper.com>
2019-05-23 14:45:26 -0400

    startfromtab: output column formats
    [finishes #166211749]

--- server/modules/startfromtab.py
+++ server/modules/startfromtab.py
@@ -4,4 +4,9 @@ def render(table, params):
     if tab is None:
         return None
 
-    return tab.dataframe
+    return {
+        'dataframe': tab.dataframe,
+        'column_formats': {c.name: c.format
+                           for c in tab.columns.values()
+                           if c.format is not None},
+    }
