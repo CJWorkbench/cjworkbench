24e77458434779b8f070a52e3a4624c72c647be1
Adam Hooper <adam@adamhooper.com>
2019-06-03 12:34:54 -0400

    Integration-test multipart upload
    (now that multipart upload actually works on Firefox!)

--- integrationtests/test_upload.py
+++ integrationtests/test_upload.py
@@ -22,22 +22,21 @@ class TestUpload(LoggedInIntegrationTest):
             text='1'
         )
 
-    # BROKEN: https://github.com/minio/minio/issues/7492
-    # def test_upload_multipart_csv(self):
-    #     b = self.browser
-    #     b.click_button('Create Workflow')
-    #
-    #     # Empty module stack
-    #     b.wait_for_element('.module-stack')
-    #
-    #     self.add_data_step('Upload')
-    #     b.wait_for_element('label', text='Browse')
-    #     b.attach_file('file',
-    #                   Path(__file__).parent / 'files' / 'multipart.csv',
-    #                   wait=True) # Wait for file input to exist
-    #     # submit happens automatically
-    #
-    #     self.browser.wait_for_element(
-    #         '.react-grid-Row:nth-child(1) .react-grid-Cell:nth-child(1)',
-    #         text='This file is >5MB'
-    #     )
+    def test_upload_multipart_csv(self):
+        b = self.browser
+        b.click_button('Create Workflow')
+
+        # Empty module stack
+        b.wait_for_element('.module-stack')
+
+        self.add_data_step('Upload')
+        b.wait_for_element('label', text='Browse')
+        b.attach_file('file',
+                      Path(__file__).parent / 'files' / 'multipart.csv',
+                      wait=True) # Wait for file input to exist
+        # submit happens automatically
+
+        self.browser.wait_for_element(
+            '.react-grid-Row:nth-child(1) .react-grid-Cell:nth-child(1)',
+            text='This file is >5MB'
+        )
