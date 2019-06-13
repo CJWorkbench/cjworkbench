81ff6cb1e2413155b474f2a422bf8305500c9893
Adam Hooper <adam@adamhooper.com>
2019-06-13 15:19:49 -0400

    Fix unit-test typo

--- server/tests/views/test_acl.py
+++ server/tests/views/test_acl.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.utils import timezone
-from server.models Workflow
+from server.models import Workflow
 from server.tests.utils import DbTestCase
 
 
