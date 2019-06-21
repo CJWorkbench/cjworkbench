2d25f17216024827aa2d130199a8c3e0cf6d8b31
Pierre Conti <pierre@tablesdata.com>
2019-06-20 17:56:42 -0700

    no click through the add data modal header

--- assets/js/WorkflowEditor/AddData/Modal.js
+++ assets/js/WorkflowEditor/AddData/Modal.js
@@ -16,11 +16,11 @@ export const Modal = React.memo(function Modal ({ modules, tabSlug, close, addMo
   })
 
   return (
-    <section className='add-data-modal' onClick={closeIfClickIsOnBackdrop}>
+    <section className='add-data-modal'>
       <header>
         <div className="title">
           <h5>CHOOSE A DATA SOURCE</h5>
-          <button type='button' className='close' aria-label='Close' title='Close' onClick={close}>×</button>  
+          <button type='button' className='close' aria-label='Close' title='Close' onClick={close}>×</button>
         </div>
         <Search value={search} onChange={setSearch} />
       </header>
