230c5a9f59efc4ce85ced3985ae48ea7418112c2
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2019-08-09 11:51:47 +0300

    Reformat code

--- assets/js/i18n/catalogs.test.js
+++ assets/js/i18n/catalogs.test.js
@@ -1,9 +1,10 @@
+/* global describe, it, expect */
 import { supportedLocales } from './locales'
 import fetchCatalog from './catalogs'
 
 describe('Translation catalogs', () => {
   it.each(Object.keys(supportedLocales))('fetches a valid catalog for all supported locales', (locale) => {
-    const catalog = fetchCatalog(locale);
+    const catalog = fetchCatalog(locale)
     expect(catalog).toBeDefined()
     expect(catalog.messages).toBeDefined()
     expect(Object.keys(catalog.messages)).not.toHaveLength(0)
