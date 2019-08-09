1ee2321a85e35f7be84f87b10430ffae9ef3b353
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2019-08-09 10:59:47 +0300

    Added a test for loading catalogs in js

---
+++ assets/js/i18n/catalogs.test.js
@@ -0,0 +1,12 @@
+import { supportedLocales } from './locales'
+import fetchCatalog from './catalogs'
+
+describe('Translation catalogs', () => {
+  it.each(Object.keys(supportedLocales))('fetches a valid catalog for all supported locales', (locale) => {
+    const catalog = fetchCatalog(locale);
+    expect(catalog).toBeDefined()
+    expect(catalog.messages).toBeDefined()
+    expect(Object.keys(catalog.messages)).not.toHaveLength(0)
+    expect(catalog.languageData).toBeDefined()
+  })
+})
