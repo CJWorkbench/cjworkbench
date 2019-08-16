118de17e7d33f7cb2f31fb45b4cfbb819cdf9501
Adam Hooper <adam@adamhooper.com>
2019-08-16 12:37:50 -0400

    'cell_is_empty_str_or_null' needs no 'value'
    [finishes #167467347]

--- assets/js/params/Custom/Filters/Subfilter.js
+++ assets/js/params/Custom/Filters/Subfilter.js
@@ -15,6 +15,13 @@ const CaseSensitiveOperations = [
   'text_is_exactly_regex'
 ]
 
+const ConditionsNeedingNoValue = [
+  'cell_is_empty',
+  'cell_is_not_empty',
+  'cell_is_empty_str_or_null',
+  'cell_is_not_empty_str_or_null'
+]
+
 export default class Subfilter extends React.PureComponent {
   static propTypes = {
     isReadOnly: PropTypes.bool.isRequired,
@@ -59,9 +66,7 @@ export default class Subfilter extends React.PureComponent {
   render () {
     const { isReadOnly, inputColumns, name, fieldId, value, onDelete, onSubmit } = this.props
     const column = (inputColumns || []).find(c => c.name === value.colname) || null
-    const needValue = column !== null && (
-      value.condition !== 'cell_is_empty' && value.condition !== 'cell_is_not_empty'
-    )
+    const needValue = column !== null && !ConditionsNeedingNoValue.includes(value.condition)
 
     return (
       <div className='subfilter'>
