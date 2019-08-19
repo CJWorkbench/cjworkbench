6ae18e79f336743f9609c3eadf9464bf95e9c3e6
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2019-08-19 10:31:41 +0300

    Remove unused code and clean up exports in locales.js

--- assets/js/i18n/locales.js
+++ assets/js/i18n/locales.js
@@ -1,45 +1,23 @@
 import { t } from '@lingui/macro'
-// import { csrfToken } from '../utils'
 
-const supportedLocales = {
+export const supportedLocales = {
   en: t('locales.en')`English`,
   el: t('locales.el')`Greek`
 }
 
-const defaultLocale = 'en'
+export const defaultLocale = 'en'
 
-const currentLocale = window.i18n && window.i18n.locale && isSupported(window.i18n.locale) ? window.i18n.locale : defaultLocale
+export const currentLocale = window.i18n && window.i18n.locale && isSupported(window.i18n.locale) ? window.i18n.locale : defaultLocale
 
 function isSupported (locale) {
   return Object.prototype.hasOwnProperty.call(supportedLocales, locale)
 }
 
 /**
- * This function tries to change locale by reloading the page and adding a locale query parameter
+ * Change locale by reloading the page and adding a locale query parameter
  */
-function setLocaleWithQueryParameter (locale) {
+export function setLocale (locale) {
   if (isSupported(locale)) {
     window.location += (window.location.search ? '&' : '?') + `locale=${locale}` // very quick and dirty solution for reloading with added parameter
   }
 }
-
-/**
- * This function tries to change locale by sending a POST request to the server, in Django fashion
- */
-// function setLocaleWithPostRequest (locale) {
-// if (isSupported(locale)) {
-// fetch('/i18n/setlang/', {//We will need a polyfill for fetch, if we decide to use it
-// method: 'POST',
-// body: JSON.stringify({
-// language: locale
-// }),
-// redirect: 'follow',
-// headers: {
-// 'X-CSRFToken': csrfToken,
-// 'Content-Type': 'application/json'
-// }
-// })
-// }
-// }
-
-export { supportedLocales, defaultLocale, currentLocale, setLocaleWithQueryParameter as setLocale, isSupported as isSupportedLocale }
