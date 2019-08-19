17acafa35a8d09edcab365e3823e8cf65213f965
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2019-08-19 10:42:03 +0300

    Remove commented-out code from catalogs.js

--- assets/js/i18n/catalogs.js
+++ assets/js/i18n/catalogs.js
@@ -9,27 +9,3 @@ const catalogs = {
 export default function fetchCatalog (language) {
   return catalogs[language]
 }
-
-/*
- * This function loads the requested catalog dynamically.
- * At this time, it is not used, since we use a static approach.
- *
- */
-// async function fetchCatalog(language){
-// let catalog;
-// if (process.env.NODE_ENV !== 'production') {
-/// / prettier-ignore
-// catalog = await import(
-/// * webpackMode: "lazy", webpackChunkName: "i18n-[index]" */
-// `@lingui/loader!../../locales/${language}/messages.po`
-// )
-// } else {
-/// / prettier-ignore
-// catalog = await import(
-/// * webpackMode: "lazy", webpackChunkName: "i18n-[index]" */
-// `../../locales/${language}/messages.js`
-// )
-// }
-
-// return catalog;
-// }
