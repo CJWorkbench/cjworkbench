07b4987c9f15115cc19029dd19519791df02241c
Adam Hooper <adam@adamhooper.com>
2019-08-20 11:48:39 -0400

    LocaleSwitcher: Select.change, not Option.click
    (Chrome doesn't fire Option.onClick. Also, keyboard events don't invoke
    onClick, right?)
    
    While I'm there:
    
    * Make it a functional component
    * Memoize it
    * Cache its callback function

--- assets/js/i18n/LocaleSwitcher.js
+++ assets/js/i18n/LocaleSwitcher.js
@@ -3,31 +3,36 @@ import PropTypes from 'prop-types'
 import { withI18n } from '@lingui/react'
 import { supportedLocales, supportedLocaleIds, setLocale } from './locales'
 
-// LocaleSwitcher uses window.i18nConfig, which is injected by django in the base template
-class LocaleSwitcher extends React.Component {
-  static propTypes = {
-    // i18n is an object injected by withI18n of lingui
-    i18n: PropTypes.shape({
-      language: PropTypes.oneOf(supportedLocaleIds),
-      _: PropTypes.func.isRequired
-    })
-  }
+/**
+ * A menu for the user to select a locale.
+ *
+ * Uses window.i18nConfig, which is injected by Django in the base template
+ */
+const LocaleSwitcher = React.memo(function LocaleSwitcher ({ i18n }) {
+  const onChangeLocale = React.useCallback(ev => {
+    setLocale(ev.target.value)
+  })
 
-  render () {
-    if (window.i18nConfig && window.i18nConfig.showSwitcher) {
-      return (
-        <select defaultValue={this.props.i18n.language}>
-          {supportedLocaleIds.map((locale) => (
-            <option key={locale} onClick={() => setLocale(locale)} value={locale}>
-              {this.props.i18n._(supportedLocales[locale])}
-            </option>
-          ))}
-        </select>
-      )
-    } else {
-      return null
-    }
+  if (window.i18nConfig && window.i18nConfig.showSwitcher) {
+    return (
+      <select value={i18n.language} onChange={onChangeLocale}>
+        {supportedLocaleIds.map((locale) => (
+          <option key={locale} value={locale}>
+            {i18n._(supportedLocales[locale])}
+          </option>
+        ))}
+      </select>
+    )
+  } else {
+    return null
   }
+})
+LocaleSwitcher.propTypes = {
+  i18n: PropTypes.shape({
+    // i18n object injected by LinguiJS withI18n()
+    language: PropTypes.oneOf(supportedLocaleIds),
+    _: PropTypes.func.isRequired
+  })
 }
 
 export default withI18n()(LocaleSwitcher)
