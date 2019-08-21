15d73a1daebd305c03c91f61d85c923c1ffd6523
Adam Hooper <adam@adamhooper.com>
2019-08-21 16:24:52 -0400

    sanitizedataframe: move to modules.formula
    sanitize_series() was used in exactly one place, for exactly one
    purpose. The code belongs in the place where the purpose is clear.

--- server/modules/formula.py
+++ server/modules/formula.py
@@ -1,14 +1,58 @@
 import itertools
 from formulas import Parser
-from schedula import DispatcherError
-import pandas as pd
 import numpy as np
+import pandas as pd
+from pandas.api.types import is_numeric_dtype, is_datetime64_dtype
+from schedula import DispatcherError
 from .utils import build_globals_for_eval
-from server.sanitizedataframe import sanitize_series
-from django.utils.translation import gettext as _
 from .utils import autocast_series_dtype
 
-# ---- Formula ----
+
+def sanitize_series(series: pd.Series) -> pd.Series:
+    """
+    Enforce type rules on input pandas `Series.values`.
+
+    The return value is anything that can be passed to the `pandas.Series()`
+    constructor.
+
+    Specific fixes:
+
+    * Make sure categories have no excess values.
+    * Convert numeric categories to 
+    * Convert unsupported dtypes to string.
+    * Reindex so row numbers are contiguous.
+    """
+    series.reset_index(drop=True, inplace=True)
+    if hasattr(series, "cat"):
+        series.cat.remove_unused_categories(inplace=True)
+
+        categories = series.cat.categories
+        if pd.api.types.is_numeric_dtype(categories.values):
+            # Un-categorize: make array of int/float
+            return pd.to_numeric(series)
+        elif (
+            categories.dtype != object
+            or pd.api.types.infer_dtype(categories.values, skipna=True) != "string"
+        ):
+            # Map from non-Strings to Strings
+            #
+            # 1. map the _codes_ to unique _codes_
+            mapping = pd.Categorical(categories.astype(str))
+            values = pd.Categorical(series.cat.codes[mapping.codes])
+            # 2. give them names
+            values.rename_categories(mapping.categories, inplace=True)
+            series = pd.Series(values)
+
+        return series
+    elif is_numeric_dtype(series.dtype):
+        return series
+    elif is_datetime64_dtype(series.dtype):
+        return series
+    else:
+        # Force it to be a str column: every object is either str or np.nan
+        ret = series.astype(str)
+        ret[pd.isna(series)] = np.nan
+        return ret
 
 
 def python_formula(table, formula):
@@ -63,11 +107,11 @@ def eval_excel_one_row(code, table):
     formula_args = []
     for token, obj in code.inputs.items():
         if obj is None:
-            raise ValueError(_("Invalid cell range: %s") % token)
+            raise ValueError("Invalid cell range: %s" % token)
         ranges = obj.ranges
         if len(ranges) != 1:
             # ...not sure what input would get us here
-            raise ValueError(_("Excel range must be a rectangular block of values"))
+            raise ValueError("Excel range must be a rectangular block of values")
         range = ranges[0]
 
         # Unpack start/end row/col
--- server/tests/modules/test_formula.py
+++ server/tests/modules/test_formula.py
@@ -2,9 +2,10 @@ import unittest
 from typing import Any, Dict
 import numpy as np
 import pandas as pd
-from pandas.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal, assert_series_equal
 from cjworkbench.types import ProcessResult
 from server.modules import formula
+from server.modules.formula import sanitize_series
 from .util import MockParams
 
 
@@ -76,6 +77,97 @@ class MigrateParamsTests(unittest.TestCase):
                 "all_rows": True,
             },
         )
+
+
+class SanitizeSeriesFrameTest(unittest.TestCase):
+    def test_mixed_to_string_keeps_nan(self):
+        # check that sanitizing a non-string column with missing data produces
+        # empty cells, not 'nan' strings
+        # https://www.pivotaltracker.com/story/show/154619564
+        series = pd.Series([1.0, "str", np.nan, ""])  # mixed
+        result = sanitize_series(series)
+        assert_series_equal(result, pd.Series(["1.0", "str", np.nan, ""]))
+
+    def test_mixed_to_string_allows_custom_types(self):
+        class Obj:
+            def __str__(self):
+                return "x"
+
+        series = pd.Series([Obj(), Obj()])
+        result = sanitize_series(series)
+        expected = pd.Series(["x", "x"])
+        assert_series_equal(result, expected)
+
+    def test_categories_to_string_allows_custom_category_types(self):
+        class Obj:
+            def __init__(self, value):
+                self.value = value
+
+            def __str__(self):
+                return self.value
+
+        series = pd.Series([Obj("a"), Obj("b"), Obj("a"), "a", "y"], dtype="category")
+        result = sanitize_series(series)
+        expected = pd.Series(["a", "b", "a", "a", "y"], dtype="category")
+        assert_series_equal(result, expected)
+
+    def test_categories_to_string_allows_abnormal_index(self):
+        class Obj:
+            def __init__(self, value):
+                self.value = value
+
+            def __str__(self):
+                return self.value
+
+        # Slicing a Series means the category list remains complete, even
+        # though some categories aren't used. In this example, `series` has an
+        # Obj('a') category, even though the value doesn't appear anywhere in
+        # the dataframe. (This is because slicing creates a numpy "view", not a
+        # copy of the original array of codes.)
+        #
+        # Sanitize's output shouldn't include any categories that aren't
+        # visible. (The data in memory should not be a "view".)
+        #
+        # Also, sanitize_series() should reset the index.
+        series = pd.Series([Obj("a"), Obj("b"), "c", "b"], dtype="category")[1:]
+        result = sanitize_series(series)
+        expected = pd.Series(["b", "c", "b"], dtype="category")
+        assert_series_equal(result, expected)
+        # to reiterate: 'result' has no category that looks like 'a'.
+        self.assertEqual(sorted(result.cat.categories.tolist()), ["b", "c"])
+
+    def test_lists_and_dicts(self):
+        series = pd.Series([[5, 6, 7], {"a": "b"}])
+        result = sanitize_series(series)
+        expected = pd.Series(["[5, 6, 7]", "{'a': 'b'}"])
+        assert_series_equal(result, expected)
+
+    def test_reset_index(self):
+        # should always come out with row numbers contiguous from zero
+        series = pd.Series([1, 2, 3])[1:]
+        result = sanitize_series(series)
+        assert_series_equal(result, pd.Series([2, 3]))  # index is [0,1]
+
+    def test_cast_int_category_to_int(self):
+        series = pd.Series([1, 2], dtype="category")
+        result = sanitize_series(series)
+        assert_series_equal(result, pd.Series([1, 2]))
+
+    def test_cast_mixed_category_to_str(self):
+        series = pd.Series([1, "2"], dtype="category")
+        result = sanitize_series(series)
+        expected = pd.Series(["1", "2"], dtype="category")
+        assert_series_equal(result, expected)
+
+    def test_remove_unused_categories(self):
+        series = pd.Series(
+            ["a", "b"],
+            # extraneous value
+            dtype=pd.api.types.CategoricalDtype(["a", "b", "c"]),
+        )
+        result = sanitize_series(series)
+        expected = pd.Series(["a", "b"], dtype="category")
+        assert_series_equal(result, expected)
 
 
 class FormulaTests(unittest.TestCase):
