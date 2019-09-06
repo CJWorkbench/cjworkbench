a5f3d0a20740a93f3599f6a4a9d905c6d8a3b526
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2019-09-06 12:38:52 +0300

    Change the interface of custom trans tag

--- server/templatetags/i18n_icu.py
+++ server/templatetags/i18n_icu.py
@@ -4,9 +4,10 @@ from i18n.trans import get_translations
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
-def trans(context, message_id, default=None, parameters={}, locale=None):
+def trans(context, message_id, default=None, locale=None, **kwargs):
     """Translate a message, supporting ICU syntax.
 
-    The locale will be taken from request if not provided
+    The locale will be taken from request if not provided.
+    The named arguments, apart from default and locale, will be passed to the ICU message.
     """
-    return get_translations(locale or context['request'].locale_id)._(message_id, default=default, parameters=parameters)
+    return get_translations(locale or context['request'].locale_id)._(message_id, default=default, parameters=kwargs)
