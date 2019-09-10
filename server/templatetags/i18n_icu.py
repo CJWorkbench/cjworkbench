b3d196155ec4df908a8046b7c7704f21b09035a9
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2019-09-10 15:46:45 +0300

    Fix naming in template tags

--- server/templatetags/i18n_icu.py
+++ server/templatetags/i18n_icu.py
@@ -1,8 +1,7 @@
 from django import template
-from i18n.trans import trans
+from i18n.trans import trans as trans_icu
 
 register = template.Library()
-
 
 @register.simple_tag(takes_context=True)
 def trans(context, message_id, default=None, locale=None, **kwargs):
@@ -11,7 +10,7 @@ def trans(context, message_id, default=None, locale=None, **kwargs):
     The locale will be taken from request if not provided.
     Named arguments, apart from default and locale, will be passed to the ICU message.
     """
-    return trans(
+    return trans_icu(
         locale or context["request"].locale_id,
         message_id,
         default=default,
