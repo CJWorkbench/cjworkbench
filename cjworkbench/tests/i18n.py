3a7594c068b1e55617d7af7d68c51a1922355de7
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2019-09-25 12:36:06 +0300

    Add some tests regarding i18n

---
+++ cjworkbench/tests/i18n.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase
+from cjworkbench.i18n.trans import trans, MessageTranslator, InvalidICUParameters
+from cjworkbench.i18n import default_locale
+
+
+mock_message_id = (
+    "some+crazy+id+that+will+never+be+actually+used+in+real+translation+files"
+)
+
+class TransTest(SimpleTestCase):
+    def test_format_invalid_default(self):
+        '''Tests that a programmer will get an exception when including a numeric variable in the message
+        '''
+        with self.assertRaises(InvalidICUParameters):
+            trans(default_locale, mock_message_id, default="Hello {a} {0} {b}", parameters={'a': 'you', '0': '!', 'b': '2'}),
+        
+    def test_format_invalid_message(self):
+        '''Tests that a translator can't break our system by including a numeric variable in the message
+        '''
+        self.assertEqual(MessageTranslator(default_locale).process_message('Hello {a} {0} {b}', 'Hello {a} {b}', parameters={'a': 'you', 'b': '!'}), 'Hello you !')
--- server/tests/templatetags/test_trans.py
+++ server/tests/templatetags/test_trans.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from server.templatetags.i18n_icu import trans
 from cjworkbench.i18n import default_locale
-
+from cjworkbench.tests.i18n import mock_message_id
 
 class MockRequest(object):
     def __init__(self, **kwargs):
@@ -12,44 +12,81 @@ def mock_context(**kwargs):
     return {"request": MockRequest(**kwargs)}
 
 
-mock_message_id = (
-    "some+crazy+id+that+will+never+be+actually+used+in+real+translation+files"
-)
-
 
 class TransTemplateTagTests(SimpleTestCase):
-    def test_trans_default(self):
-        self.assertEqual(
-            trans(mock_context(), mock_message_id, default="The default"), "The default"
-        )
+    def test_trans_no_default(self):
+        '''Tests that when no default is given, the message id is returned
+        '''
+        self.assertEqual(trans(mock_context(), mock_message_id), mock_message_id)
 
     def test_trans_noop(self):
+        '''Tests that `noop=True` returns `None`
+        '''
         self.assertIsNone(
             trans(mock_context(), mock_message_id, noop=True, default="Hello {a} {b}!")
         )
 
     def test_trans_params(self):
+        '''Tests that `arg_XX` arguments replace variables in the message.
+        
+        1) Missing variables are ignored.
+        2) The order of `arg` arguments is not important.
+        3) When the programmer tries to use numeric arguments, an exception is raised
+           (behaviour for when the translator tries to use numeric arguments is tested elsewhere)
+        '''
         self.assertEqual(
             trans(
                 mock_context(),
                 mock_message_id,
-                default="Hello {a} {b}!",
+                default="Hello {a} {param_b} {c}!",
+                arg_param_b="there",
                 arg_a="you",
-                arg_b="there",
             ),
-            "Hello you there!",
+            "Hello you there {c}!",
         )
+        
+        with self.assertRaises(Exception):
+            trans(mock_context(), mock_message_id, default="Hello {a} {0} {b}", arg_a='you', arg_0='!', arg_b='2'),
+        
 
     def test_trans_tag_placeholders(self):
+        ''' Tests that placeholder tags work as intended.
+        
+        1) `tag_XX_YY` arguments are used to replace placeholders
+        2) Tags or placeholders that have no counterpart in the arguments are removed
+        3) The order of `tag` arguments is not important
+        4) Special characters, except for the ones of valid tags, are escaped
+        
+        Does not test what happens to nested tags, since we have no hard policy
+        '''
         self.assertEqual(
             trans(
                 mock_context(),
                 mock_message_id,
-                default='<span0>Hello</span0> <a0>{a}<b></b></a0> < <a1>there<</a1>!<br /><script type="text/javascript" src="mybadscript.js"></script>',
+                default='<span0>Hello</span0><span1></span1> <a0>{a}<b></b></a0> < <a1>there<</a1>!<br /><script type="text/javascript" src="mybadscript.js"></script>',
                 arg_a="you",
                 tag_a0_href="/you",
                 tag_a1_href="/there",
                 tag_a1_class="red big",
+                tag_span0_id="hi"
             ),
-            'Hello <a href="/you">you</a> &lt; <a class="red big" href="/there">there&lt;</a>!',
+            '<span id="hi">Hello</span> <a href="/you">you</a> &lt; <a class="red big" href="/there">there&lt;</a>!',
+        )
+
+    def test_trans_nested_tags(self):
+        ''' Tests that nested tags in messages are not tolerated.
+        
+        At this point, nested tags are ignored, but their contents are kept.
+        This may change in the future.
+        '''
+        self.assertEqual(
+            trans(
+                mock_context(),
+                mock_message_id,
+                default='<a0>Hello<b0>you</b0><div>there</div></a0>',
+                arg_a="you",
+                tag_a0_href="/you",
+                tag_b0_id="hi"
+            ),
+            '<a href="/you">Helloyouthere</a>',
         )
