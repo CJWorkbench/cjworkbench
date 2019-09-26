04178c8bd49c96eafb3b53f431ad2cfabb7cd6f0
Adam Hooper <adam@adamhooper.com>
2019-09-26 19:26:20 -0400

    Upgrade minio
    Fixes security vulnerabilities and lets us deploy more than one server.
    
    This is already deployed on staging and production; this commit is for
    devs.
    
    [finishes #168789997]

---
+++ deploy/kubernetes/minio/recreate-minio-user.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -ex
+
+DIR="$(dirname "$0")"
+ENV=${1:?"Usage: $0 ENVIRONMENT"}
+
+# Now use the root user to generate the non-root user.
+kubectl -n "$ENV" run minio-adduser \
+  -i --rm=true \
+  --restart=Never \
+  --image=minio/mc:RELEASE.2019-09-24T01-36-20Z \
+  --overrides='
+    {
+      "spec": {
+        "containers": [{
+          "name": "minio-adduser",
+          "image": "minio/mc:RELEASE.2019-09-24T01-36-20Z",
+          "command": [
+            "sh", "-c",
+            "while ! nc -z minio-service 80; do sleep 0.1; done; mc config host add workbench http://minio-service \"$ROOT_ACCESS_KEY\" \"$ROOT_SECRET_KEY\" && mc admin user add workbench \"$ACCESS_KEY\" \"$SECRET_KEY\" && mc admin policy set workbench readwrite user=\"$ACCESS_KEY\""
+          ],
+          "env": [
+            {"name": "ROOT_ACCESS_KEY", "valueFrom": {"secretKeyRef": {"name": "minio-root-access-key", "key": "access_key"}}},
+            {"name": "ROOT_SECRET_KEY", "valueFrom": {"secretKeyRef": {"name": "minio-root-access-key", "key": "secret_key"}}},
+            {"name": "ACCESS_KEY", "valueFrom": {"secretKeyRef": {"name": "minio-access-key", "key": "access_key"}}},
+            {"name": "SECRET_KEY", "valueFrom": {"secretKeyRef": {"name": "minio-access-key", "key": "secret_key"}}}
+          ]
+        }]
+      }
+    }'
