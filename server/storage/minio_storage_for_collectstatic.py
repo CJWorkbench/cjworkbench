e334fe12098e3bfd57de482616ec8fe5f51c4a69
Adam Hooper <adam@adamhooper.com>
2019-09-27 19:10:05 -0400

    collectstatic: upload to minio in parallel
    We upload in 10 threads now. So I expect this stage of integration
    testing and deployment to take 10x less time.
    
    Should save 3min during build+auto-deploy to staging, and another 3min
    on deploy. Next time we have a critical fix, we can deploy it 6min
    sooner.

--- server/storage/minio_storage_for_collectstatic.py
+++ server/storage/minio_storage_for_collectstatic.py
@@ -1,24 +1,29 @@
+import concurrent.futures
 import gzip
+import logging
 import mimetypes
+from queue import Queue
 from django.conf import settings
+from django.core.files import File
 from django.core.files.storage import Storage
 from cjwstate.minio import ensure_bucket_exists, client, StaticFilesBucket
+
+
+logger = logging.getLogger(__name__)
 
 
 class MinioStorage(Storage):
     def __init__(self, *args, **kwargs):
         Storage.__init__(self, *args, **kwargs)
-        ensure_bucket_exists(StaticFilesBucket)
 
     """Dumb class for uploading straight to S3."""
 
-    def _save(self, name, file) -> None:
-        # TODO django 2.0: with file.open('wb') as f:
-        file.open("rb")
-        try:
-            data = file.read()
-        finally:
-            file.close()
+    def _upload_in_thread(self, name: str, data: bytes) -> None:
+        """
+        Perform the actual upload.
+
+        Raise an exception if the file is not certainly uploaded.
+        """
 
         content_type, _ = mimetypes.guess_type(name, strict=False)
         content_type = content_type or "application/octet-stream"
@@ -45,6 +50,48 @@ class MinioStorage(Storage):
             CacheControl="public, max-age=86400",
             **kwargs,
         )
+        logger.info("Finished uploading %s (%d bytes)" % (name, len(data)))
+
+    def save(self, name: str, file: File, max_length=None) -> None:
+        """
+        *Start* uploading `file` to S3.
+
+        Return before finish.
+        """
+        if not hasattr(self, "_executor"):
+            self._executor = concurrent.futures.ThreadPoolExecutor(
+                max_workers=10, thread_name_prefix="collectstatic"
+            )
+            self._upload_futures = []
+
+        # The caller is about to _close_ `file`. So let's read all the data
+        # into RAM while we still can. Good thing these files are small, right?
+        data = file.read()
+        self._upload_futures.append(
+            self._executor.submit(self._upload_in_thread, name, data)
+        )
+
+    def post_process(self, *args, **kwargs) -> None:
+        """
+        Wait for all started uploads to finish, then clean up.
+
+        Log and raise an exception if any of the uploads fail.
+        """
+        error = False
+        for future in concurrent.futures.as_completed(self._upload_futures):
+            try:
+                future.result()
+            except Exception:
+                logger.exception("Exception during upload of a file")
+                error = True
+        self._executor.shutdown(True)
+        del self._executor
+        del self._upload_futures
+
+        if error:
+            raise OSError("Failed to upload all the files")
+
+        return []  # because Django expects a list of postprocessed files
 
     def delete(self, name):
         # We never want to delete
