358f98f9285365926e6fbee997987168e57c8671
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2019-10-03 08:42:12 +0300

    Restructure tests for message translation and add some more

--- cjworkbench/tests/i18n.py
+++ cjworkbench/tests/i18n.py
@@ -9,6 +9,27 @@ mock_message_id = (
 
 
 class TransTest(SimpleTestCase):
+    def test_trans_no_default(self):
+        """Tests that when no default is given, the message id is returned
+        """
+        self.assertEqual(trans(default_locale, mock_message_id), mock_message_id)
+
+    def test_trans_params(self):
+        """Tests that `parameters` argument replaces variables in the message.
+        
+        1) Parameters that do not exist in the message are ignored.
+        2) Variables in the message for which no parameter has been given are ignored.
+        """
+        self.assertEqual(
+            trans(
+                default_locale,
+                mock_message_id,
+                default="Hello {a} {param_b} {c}!",
+                parameters={"param_b": "there", "a": "you", "d": "tester"},
+            ),
+            "Hello you there {c}!",
+        )
+
     def test_format_invalid_default(self):
         """Tests that a programmer will get an exception when including a numeric variable in the message
         """
@@ -28,4 +49,135 @@ class TransTest(SimpleTestCase):
                 "Hello {a} {0} {b}", "Hello {a} {b}", parameters={"a": "you", "b": "!"}
             ),
             "Hello you !",
+        )
+
+    def test_trans_tags(self):
+        """ Tests that tags in messages are replaced correctly.
+        
+        1) Tags in `tags` that are not in the message are ignored.
+        2) Tags in the message but not in `tags` are ignored. At this point, their contents are kept, but this may change in the future.
+        3) All attributes given for a tag in `tags` are used.
+        4) Tag attributes existing in the message are ignored. 
+        """
+        self.assertEqual(
+            trans(
+                default_locale,
+                mock_message_id,
+                default='<a0 id="nope">Hello</a0><b0>you</b0><div>there</div>',
+                parameters={"a": "you"},
+                tags={
+                    "a0": {
+                        "tag": "a",
+                        "attrs": {
+                            "href": "/you",
+                            "class": "the test",
+                            "data-target": "someid",
+                        },
+                    },
+                    "span0": {"tag": "span", "attrs": {"id": "ignore"}},
+                },
+            ),
+            '<a class="the test" data-target="someid" href="/you">Hello</a>youthere',
+        )
+
+    def test_trans_nested_tags(self):
+        """ Tests that nested tags in messages are not tolerated.
+        
+        At this point, nested tags are ignored, but their contents are kept.
+        This may change in the future.
+        """
+        self.assertEqual(
+            trans(
+                default_locale,
+                mock_message_id,
+                default="<a0>Hello<b0>you</b0><div>there</div></a0>",
+                parameters={"a": "you"},
+                tags={
+                    "a0": {"tag": "a", "attrs": {"href": "/you"}},
+                    "b0": {"tag": "b", "attrs": {"id": "hi"}},
+                },
+            ),
+            '<a href="/you">Helloyouthere</a>',
+        )
+
+    def test_trans_params_in_tags(self):
+        """ Tests that parameters are substituted within tags
+        """
+        self.assertEqual(
+            trans(
+                default_locale,
+                mock_message_id,
+                default="<a0>Hello {name}</a0>{test}",
+                parameters={"name": "you", "test": "!"},
+                tags={"a0": {"tag": "a", "attrs": {"href": "/you"}}},
+            ),
+            '<a href="/you">Hello you</a>!',
+        )
+
+    def test_trans_escapes_text(self):
+        """ Tests that special characters in the text are escaped, in any depth
+        """
+        self.assertEqual(
+            trans(
+                default_locale,
+                mock_message_id,
+                default="<a0>Hello &<b>&</b></a0>>",
+                tags={"a0": {"tag": "a", "attrs": {"href": "/you"}}},
+            ),
+            '<a href="/you">Hello &amp;&amp;</a>&gt;',
+        )
+
+    def test_trans_escapes_params(self):
+        """ Tests that message parameters are escaped
+        """
+        self.assertEqual(
+            trans(
+                default_locale,
+                mock_message_id,
+                default="<a0>Hello {name}</a0>{test}",
+                parameters={"name": "<b>you</b>", "test": "<b>there</b>"},
+                tags={"a0": {"tag": "a", "attrs": {"href": "/you"}}},
+            ),
+            '<a href="/you">Hello &lt;b&gt;you&lt;/b&gt;</a>&lt;b&gt;there&lt;/b&gt;',
+        )
+
+    def test_trans_escapes_tag_attrs(self):
+        """ Tests that tag attributes in messages are escaped
+        """
+        self.assertEqual(
+            trans(
+                default_locale,
+                mock_message_id,
+                default="<a0>Hello</a0>",
+                tags={"a0": {"tag": "a", "attrs": {"href": "/you?a=b&c=d"}}},
+            ),
+            '<a href="/you?a=b&amp;c=d">Hello</a>',
+        )
+
+    def test_trans_tag_placeholders(self):
+        """ Tests the combination of properties of placeholder tags and of message parameters.
+        
+        0) In settings where there are multiple tags, some of which have to be deleted, all of them are processed
+        1) the `tags` argument is used to replace placeholders and they are escaped correctly
+        2) Tags or tag placeholders that have no counterpart in the arguments are removed
+        3) Special characters, except for the ones of valid tags, are escaped
+        4) Nested tags are not tolerated
+        5) `arg_XX` arguments are replaced and escaped correctly
+        """
+        self.assertEqual(
+            trans(
+                default_locale,
+                mock_message_id,
+                default='<span0 class="nope">Hello {first}</span0><span1></span1> {second} <a0>{a}<b></b></a0> < <a1>there<</a1>!<br /><script type="text/javascript" src="mybadscript.js"></script>',
+                parameters={"a": "you", "first": "hello", "second": "&"},
+                tags={
+                    "a0": {"tag": "a", "attrs": {"href": "/you"}},
+                    "a1": {
+                        "tag": "a",
+                        "attrs": {"href": "/there?a=b&c=d", "class": "red big"},
+                    },
+                    "span0": {"tag": "span", "attrs": {"id": "hi"}},
+                },
+            ),
+            '<span id="hi">Hello hello</span> &amp; <a href="/you">you</a> &lt; <a class="red big" href="/there?a=b&amp;c=d">there&lt;</a>!',
         )
--- server/tests/templatetags/test_trans.py
+++ server/tests/templatetags/test_trans.py
@@ -31,9 +31,10 @@ class TransTemplateTagTests(SimpleTestCase):
     def test_trans_params(self):
         """Tests that `arg_XX` arguments replace variables in the message.
         
-        1) Missing variables are ignored.
-        2) The order of `arg` arguments is not important.
-        3) When the programmer tries to use numeric arguments, an exception is raised
+        1) Parameters that do not exist in the message are ignored.
+        2) Variables in the message for which no parameter has been given are ignored.
+        3) The order of `arg` arguments is not important.
+        4) When the programmer tries to use numeric arguments, an exception is raised
            (behaviour for when the translator tries to use numeric arguments is tested elsewhere)
         """
         self.assertEqual(
@@ -43,6 +44,7 @@ class TransTemplateTagTests(SimpleTestCase):
                 default="Hello {a} {param_b} {c}!",
                 arg_param_b="there",
                 arg_a="you",
+                arg_d="tester",
             ),
             "Hello you there {c}!",
         )
@@ -58,87 +60,29 @@ class TransTemplateTagTests(SimpleTestCase):
             ),
 
     def test_trans_tag_placeholders(self):
-        """ Tests that placeholder tags work as intended.
+        """ Tests the combination of properties of placeholder tags and of message parameters.
         
-        1) `tag_XX_YY` arguments are used to replace placeholders
+        0) In settings where there are multiple tags, some of which have to be deleted, all of them are processed
+        1) `tag_XX_YY` arguments are used to replace placeholders; existing attributes are removed
         2) Tags or placeholders that have no counterpart in the arguments are removed
         3) The order of `tag` arguments is not important
-        4) Special characters, except for the ones of valid tags, are escaped
-        5) In settings where there are multiple tags, some of which have to be deleted, all of them are processed
-        
-        Does not test what happens to nested tags, since we have no hard policy
+        4) Special characters, except for the ones of valid tags, are escaped, even in tag attributes and in args
+        5) Nested tags are not tolerated
+        6) `arg_XX` arguments are replaced correctly
         """
         self.assertEqual(
             trans_html(
                 mock_context(),
                 mock_message_id,
-                default='<span0>Hello</span0><span1></span1> <a0>{a}<b></b></a0> < <a1>there<</a1>!<br /><script type="text/javascript" src="mybadscript.js"></script>',
+                default='<span0 class="nope">Hello {first}</span0><span1></span1> {second} <a0>{a}<b></b></a0> < <a1>there<</a1>!<br /><script type="text/javascript" src="mybadscript.js"></script>',
                 arg_a="you",
                 tag_a0_href="/you",
-                tag_a1_href="/there",
+                tag_a1_href="/there?a=b&c=d",
                 tag_a1_class="red big",
                 tag_span0_id="hi",
+                tag_div0_class="red big",
+                arg_first="hello",
+                arg_second="&",
             ),
-            '<span id="hi">Hello</span> <a href="/you">you</a> &lt; <a class="red big" href="/there">there&lt;</a>!',
-        )
-
-    def test_trans_nested_tags(self):
-        """ Tests that nested tags in messages are not tolerated.
-        
-        At this point, nested tags are ignored, but their contents are kept.
-        This may change in the future.
-        """
-        self.assertEqual(
-            trans_html(
-                mock_context(),
-                mock_message_id,
-                default="<a0>Hello<b0>you</b0><div>there</div></a0>",
-                arg_a="you",
-                tag_a0_href="/you",
-                tag_b0_id="hi",
-            ),
-            '<a href="/you">Helloyouthere</a>',
-        )
-
-    def test_trans_params_in_tags(self):
-        """ Tests that parameters are substituted within tags
-        """
-        self.assertEqual(
-            trans_html(
-                mock_context(),
-                mock_message_id,
-                default="<a0>Hello {name}</a0>{test}",
-                arg_name="you",
-                arg_test="!",
-                tag_a0_href="/you",
-            ),
-            '<a href="/you">Hello you</a>!',
-        )
-
-    def test_trans_escapes_params(self):
-        """ Tests that message parameters are escaped
-        """
-        self.assertEqual(
-            trans_html(
-                mock_context(),
-                mock_message_id,
-                default="<a0>Hello {name}</a0>{test}",
-                arg_name="<b>you</b>",
-                arg_test="<b>there</b>",
-                tag_a0_href="/you",
-            ),
-            '<a href="/you">Hello &lt;b&gt;you&lt;/b&gt;</a>&lt;b&gt;there&lt;/b&gt;',
-        )
-
-    def test_trans_escapes_tag_attrs(self):
-        """ Tests that tag attributes in messages are escaped
-        """
-        self.assertEqual(
-            trans_html(
-                mock_context(),
-                mock_message_id,
-                default="<a0>Hello</a0>",
-                tag_a0_href="/you?a=b&c=d",
-            ),
-            '<a href="/you?a=b&amp;c=d">Hello</a>',
+            '<span id="hi">Hello hello</span> &amp; <a href="/you">you</a> &lt; <a class="red big" href="/there?a=b&amp;c=d">there&lt;</a>!',
         )
