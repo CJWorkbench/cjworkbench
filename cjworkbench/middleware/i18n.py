50723e6cf3ee027e4e80f5748168721ae6110f06
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2019-10-09 10:40:38 +0300

    Make i18n middleware smarter
    Look for the locale to set in more places (session, user settings, HTTP header).
    Save the locale to session.

--- cjworkbench/middleware/i18n.py
+++ cjworkbench/middleware/i18n.py
@@ -1,5 +1,10 @@
 from cjworkbench.i18n import default_locale, supported_locales
 from django.utils.translation import activate
+from django.utils.translation.trans_real import (
+    language_code_re,
+    get_supported_language_variant,
+    parse_accept_lang_header,
+)
 
 
 class SetCurrentLocaleMiddleware:
@@ -10,11 +15,61 @@ class SetCurrentLocaleMiddleware:
     def __call__(self, request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
-        locale = request.GET.get("locale")
+        self._set_locale(request, self._decide_locale(request))
 
-        if locale not in supported_locales:
-            locale = default_locale
+        response = self.get_response(request)
 
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
+
+    def _decide_locale(self, request):
+        """ Search for the locale to use.
+        
+        We search in the following places, in order
+         1. In the current request attributes, so that the user can change it any time
+         2. In the current session, in case it's there from some previous request
+         3. In the current user's settings
+         4. In the Accept-Language header sent by the browser
+         
+         If the locale found is not supported, we fallback to the default
+        """
+        locale = (
+            self._get_locale_from_request_attributes(request)
+            or self._get_locale_from_session(request)
+            or self._get_locale_from_current_user(request)
+            or self._get_locale_from_language_header(request)
+        )
+
+        return locale if locale in supported_locales else default_locale
+
+    def _get_locale_from_request_attributes(self, request):
+        return request.GET.get("locale")
+
+    def _get_locale_from_session(self, request):
+        return request.session.get("locale_id")
+
+    def _get_locale_from_current_user(self, request):
+        return getattr(request.user, "locale_id", None)
+
+    def _get_locale_from_language_header(self, request):
+        # Copied from django.utils.translation.real_trans.get_language_from_request
+        accept = request.META.get("HTTP_ACCEPT_LANGUAGE", "")
+        for accept_lang, unused in parse_accept_lang_header(accept):
+            if accept_lang == "*":
+                break
+
+            if not language_code_re.search(accept_lang):
+                continue
+
+            try:
+                return get_supported_language_variant(accept_lang)
+            except LookupError:
+                continue
+        return None
+
+    def _set_locale(self, request, locale):
         request.locale_id = locale
         # We set the locale of django, in order to
         # a) activate the automatic translation of its translatable elements
@@ -23,9 +78,6 @@ class SetCurrentLocaleMiddleware:
         #    (e.g. for use in lazy translations)
         activate(locale)
 
-        response = self.get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
-
-        return response
+        # We set the locale in session, so that we will remember it in future requests
+        if request.session.get("locale_id") != locale:
+            request.session["locale_id"] = locale
