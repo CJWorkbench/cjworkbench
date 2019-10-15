6ac50d20bed15555dcb2d86fc99853b607b8c6af
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2019-10-15 19:44:18 +0300

    Add a comment to custom allauth account adapter

--- cjworkbench/allauth_account_adapter.py
+++ cjworkbench/allauth_account_adapter.py
@@ -3,6 +3,10 @@ from cjworkbench.i18n.templates import context_processor
 
 
 class AccountAdapter(DefaultAccountAdapter):
+    # allauth builds its own context, which does not include
+    # the context injected by the context processors in settings.
+    # Since we always need our i18n context for translation tags in templates,
+    # we have to inject it here.
     def send_mail(self, template_prefix, email, context):
         return super().send_mail(
             template_prefix, email, {**context, **context_processor(context["request"])}
