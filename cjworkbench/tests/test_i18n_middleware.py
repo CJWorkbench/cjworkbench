cad7367c98997665f6d58aafb398e3d9032a7e58
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2019-10-16 06:00:11 +0300

    Refactor i18n middleware tests
    Each case of request is now a different test function.
    We also test that locale is also set correctly in django.

--- cjworkbench/tests/test_i18n_middleware.py
+++ cjworkbench/tests/test_i18n_middleware.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from cjworkbench.middleware.i18n import SetCurrentLocaleMiddleware
 from django.contrib.auth.models import AnonymousUser, User
 from cjworkbench.i18n import default_locale
+from django.utils.translation import get_language
 
 non_default_locale = "el"
 unsupported_locale = "jp"
@@ -24,6 +25,20 @@ class MockUser:
 class SetCurrentLocaleMiddlewareTest(SimpleTestCase):
     """
     Tests the SetCurrentLocaleMiddleware.
+            
+    A registered user with a locale preference will be served a locale with the following order of preference:
+        1. Current GET request parameter, if the locale is supported
+        2. User preference, if the locale is supported
+        3. Accept-Language HTTP header, if the header is valid and contains a supported locale
+        4. Default locale
+    The selected locale will not modify the user's preferences
+    In case any locale is already stored in session, it will be ignored
+    
+    A non-registered user will be served the locale with the following order of preference:
+        1. Current GET request parameter, if the locale is supported
+        2. Session, if the locale is supported
+        3. Accept-Language HTTP header, if the header is valid and contains a supported locale
+        4. Default locale
     """
 
     def setUp(self):
@@ -50,138 +65,135 @@ class SetCurrentLocaleMiddlewareTest(SimpleTestCase):
             self._mock_request(**kwargs)
         ).request
 
-    def test_anonymous_user(self):
-        """ A non-registered user will be served the locale with the following order of preference:
-            1. Current GET request parameter, if the locale is supported
-            2. Session, if the locale is supported
-            3. Accept-Language HTTP header, if the header is valid and contains a supported locale
-            4. Default locale
-        
-        Registered users that have never set a locale preference will be treated as non-registered.
-        """
-        for user in [None, MockUser()]:
-            # 4
-            request = self._process_request(user=user)
-            self.assertEqual(request.locale_id, default_locale)
+    def test_anonymous_user_default(self):
+        # anonymous #4
+        request = self._process_request()
+        self.assertEqual(request.locale_id, default_locale)
 
-            # 3, a simple case: only a supported locale is requested
-            request = self._process_request(
-                user=user, accept_language_header=non_default_locale
-            )
-            self.assertEqual(request.locale_id, non_default_locale)
+    def test_anonymous_user_header_only_supported(self):
+        # anonymous #3, a simple case: only a supported locale is requested
+        request = self._process_request(accept_language_header=non_default_locale)
+        self.assertEqual(request.locale_id, non_default_locale)
+        self.assertEqual(get_language(), non_default_locale)
 
-            # 3, invalid header
-            request = self._process_request(
-                user=user, accept_language_header="invalid header content"
-            )
-            self.assertEqual(request.locale_id, default_locale)
+    def test_anonymous_user_header_invalid(self):
+        # anonymous #3, invalid header
+        request = self._process_request(accept_language_header="invalid header content")
+        self.assertEqual(request.locale_id, default_locale)
+        self.assertEqual(get_language(), default_locale)
 
-            # 3, only a non-supported locale is requested
-            request = self._process_request(
-                user=user, accept_language_header=unsupported_locale
-            )
-            self.assertEqual(request.locale_id, default_locale)
+    def test_anonymous_user_header_nonsupported(self):
+        # anonymous #3, only a non-supported locale is requested
+        request = self._process_request(accept_language_header=unsupported_locale)
+        self.assertEqual(request.locale_id, default_locale)
+        self.assertEqual(get_language(), default_locale)
 
-            # 3, multiple locales requested, one of them is supported
-            request = self._process_request(
-                user=user,
-                accept_language_header="%s,%s;q=0.5"
-                % (unsupported_locale, non_default_locale),
-            )
-            self.assertEqual(request.locale_id, non_default_locale)
+    def test_anonymous_user_header_multiple(self):
+        # anonymous #3, multiple locales requested, one of them is supported
+        request = self._process_request(
+            accept_language_header="%s,%s;q=0.5"
+            % (unsupported_locale, non_default_locale)
+        )
+        self.assertEqual(request.locale_id, non_default_locale)
+        self.assertEqual(get_language(), non_default_locale)
 
-            # 2, valid locale
-            request = self._process_request(
-                user=user,
-                accept_language_header=default_locale,
-                session_locale=non_default_locale,
-            )
-            self.assertEqual(request.locale_id, non_default_locale)
+    def test_anonymous_user_session_supported(self):
+        # anonymous #2, supported locale
+        request = self._process_request(
+            accept_language_header=default_locale, session_locale=non_default_locale
+        )
+        self.assertEqual(request.locale_id, non_default_locale)
+        self.assertEqual(get_language(), non_default_locale)
 
-            # 2, invalid locale
-            request = self._process_request(
-                user=user,
-                accept_language_header=non_default_locale,
-                session_locale=unsupported_locale,
-            )
-            self.assertEqual(request.locale_id, non_default_locale)
+    def test_anonymous_user_session_unsupported(self):
+        # anonymous #2, non-supported locale
+        request = self._process_request(
+            accept_language_header=non_default_locale, session_locale=unsupported_locale
+        )
+        self.assertEqual(request.locale_id, non_default_locale)
+        self.assertEqual(get_language(), non_default_locale)
 
-            # 1, valid locale
-            request = self._process_request(
-                user=user,
-                accept_language_header=default_locale,
-                session_locale=default_locale,
-                request_locale=non_default_locale,
-            )
-            self.assertEqual(request.locale_id, non_default_locale)
+    def test_anonymous_user_request_supported(self):
+        # anonymous #1, valid locale
+        request = self._process_request(
+            accept_language_header=default_locale,
+            session_locale=default_locale,
+            request_locale=non_default_locale,
+        )
+        self.assertEqual(request.locale_id, non_default_locale)
+        self.assertEqual(get_language(), non_default_locale)
 
-            # 1, invalid locale
-            request = self._process_request(
-                user=user, request_locale=unsupported_locale
-            )
-            self.assertEqual(request.locale_id, default_locale)
+    def test_anonymous_user_request_unsupported(self):
+        # anonymous #1, invalid locale
+        request = self._process_request(request_locale=unsupported_locale)
+        self.assertEqual(request.locale_id, default_locale)
+        self.assertEqual(get_language(), default_locale)
 
-    def test_registered_user(self):
-        """ A registered user with a locale preference will be served a locale with the following order of preference:
-            1. Current GET request parameter, if the locale is supported
-            2. User preference, if the locale is supported
-            3. Accept-Language HTTP header, if the header is valid and contains a supported locale
-            4. Default locale
-        
-        The selected locale will not modify the user's preferences
-        In case any locale is already stored in session, it will be ignored
-        """
-        # 4
+    def test_registered_user_default(self):
+        # registered #4
         request = self._process_request(
             user=MockUser(locale_preference=unsupported_locale)
         )
         self.assertEqual(request.locale_id, default_locale)
+        self.assertEqual(get_language(), default_locale)
         self.assertEqual(request.user.locale_id, unsupported_locale)
 
-        # session is ignored
+    def test_registered_user_no_session(self):
+        # registered, session is ignored
         request = self._process_request(
             user=MockUser(locale_preference=unsupported_locale),
             session_locale=non_default_locale,
         )
         self.assertEqual(request.locale_id, default_locale)
+        self.assertEqual(get_language(), default_locale)
         self.assertEqual(request.user.locale_id, unsupported_locale)
 
-        # 3, supported locale
+    def test_registered_user_header_supported(self):
+        # registered #3, supported locale
         request = self._process_request(
             user=MockUser(locale_preference=unsupported_locale),
             accept_language_header=non_default_locale,
         )
         self.assertEqual(request.locale_id, non_default_locale)
+        self.assertEqual(get_language(), non_default_locale)
         self.assertEqual(request.user.locale_id, unsupported_locale)
 
-        # 3, non-supported locale
+    def test_registered_user_header_nonsupported(self):
+        # registered #3, non-supported locale
         request = self._process_request(
             user=MockUser(locale_preference=unsupported_locale),
             accept_language_header=unsupported_locale,
         )
         self.assertEqual(request.locale_id, default_locale)
+        self.assertEqual(get_language(), default_locale)
         self.assertEqual(request.user.locale_id, unsupported_locale)
 
-        # 2
+    def test_registered_user_preferences(self):
+        # registered #2
         request = self._process_request(
             user=MockUser(locale_preference=non_default_locale),
             accept_language_header=default_locale,
         )
         self.assertEqual(request.locale_id, non_default_locale)
+        self.assertEqual(get_language(), non_default_locale)
         self.assertEqual(request.user.locale_id, non_default_locale)
 
-        # 1, supported locale
+    def test_registered_user_request_supported(self):
+        # registered #1, supported locale
         request = self._process_request(
             user=MockUser(locale_preference=non_default_locale),
             request_locale=default_locale,
         )
         self.assertEqual(request.locale_id, default_locale)
+        self.assertEqual(get_language(), default_locale)
         self.assertEqual(request.user.locale_id, non_default_locale)
 
-        # 1, non-supported locale
+    def test_registered_user_request_unsupported(self):
+        # registered #1, non-supported locale
         request = self._process_request(
             user=MockUser(locale_preference=non_default_locale),
             request_locale=unsupported_locale,
         )
         self.assertEqual(request.locale_id, non_default_locale)
+        self.assertEqual(get_language(), non_default_locale)
         self.assertEqual(request.user.locale_id, non_default_locale)
