920a0a30198691a85ee9c3e0462297f8c5df58d9
Adam Hooper <adam@adamhooper.com>
2019-10-31 11:49:00 -0400

    fetcher: fetch just once at a time
    This simplifies scaling, and it lets us use singleton locks in
    cjwkernel.kernel. That'll let us reuse sandboxes.

--- renderer/main.py
+++ renderer/main.py
@@ -1,15 +1,6 @@
-import os
 from cjworkbench import rabbitmq
 from cjworkbench.pg_render_locker import PgRenderLocker
 from .render import handle_render
-
-
-# NRenderers: number of renders to perform simultaneously. This should be 1 per
-# CPU, because rendering is CPU-bound. (It uses a fair amount of RAM, too.)
-#
-# Default is 1: we expect to run on a 2-CPU machine, so 1 CPU for render and 1
-# for cron-render.
-NRenderers = int(os.getenv("CJW_N_RENDERERS", 1))
 
 
 async def main_loop():
@@ -23,6 +14,6 @@ async def main_loop():
             return await handle_render(message, ack, pg_render_locker)
 
         connection = rabbitmq.get_connection()
-        connection.declare_queue_consume(rabbitmq.Render, NRenderers, render_callback)
+        connection.declare_queue_consume(rabbitmq.Render, render_callback)
         # Run forever
         await connection._closed_event.wait()
