7ec7b0ebc3db00aad7b119a6a7f1bee7752b2597
Adam Hooper <adam@adamhooper.com>
2019-11-07 12:10:50 -0500

    Wow -- I failed at writing a one-liner "echo" bash script

--- cjwkernel/setup-chroots.sh
+++ cjwkernel/setup-chroots.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo 'setup-chroots.sh (no-op here; overwrite from overlayfs-chroot branch when we're ready to deploy the _real_ script'
+echo "setup-chroots.sh (no-op here; overwrite from overlayfs-chroot branch when we're ready to deploy the _real_ script"
