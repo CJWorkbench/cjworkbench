891cd862077a54b6d940c44311966fa36574c9c7
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2019-12-07 13:03:30 +0200

    Change layout of language switcher in react pages

--- assets/js/WfHamburgerMenu.js
+++ assets/js/WfHamburgerMenu.js
@@ -4,7 +4,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from './components/Dropdown'
 import ImportModuleFromGitHub from './ImportModuleFromGitHub'
-import LocaleSwitcher from './i18n/LocaleSwitcher'
+import LocaleSwitcher, { enableLocaleSwitcher } from './i18n/LocaleSwitcher'
 import { Trans, t } from '@lingui/macro'
 import { withI18n } from '@lingui/react'
 
@@ -22,7 +22,8 @@ export class WfHamburgerMenu extends React.Component {
   }
 
   state = {
-    importModalOpen: false
+    importModalOpen: false,
+    localeSwitcherOpen: false
   }
 
   handleClickOpenImportModal = () => {
@@ -33,13 +34,20 @@ export class WfHamburgerMenu extends React.Component {
     this.setState({ importModalOpen: false })
   }
 
+  handleClickOpenLocaleSwitcher = () => {
+    this.setState({ localeSwitcherOpen: true })
+  }
+
+  closeLocaleSwitcher = () => {
+    this.setState({ localeSwitcherOpen: false })
+  }
+
   render () {
     const { api, workflowId, user, i18n } = this.props
     const loggedIn = !!user
 
     return (
       <>
-        <LocaleSwitcher />
         <UncontrolledDropdown>
 
           <DropdownToggle title={i18n._(t('js.WfHamburgerMenu.toggle.hoverText')`menu`)} className='context-button'>
@@ -55,6 +63,9 @@ export class WfHamburgerMenu extends React.Component {
             ) : (
               <DropdownItem href='//workbenchdata.com'><Trans id='js.WfHamburgerMenu.menu.home'>Home</Trans></DropdownItem>
             )}
+            {enableLocaleSwitcher ? (
+              <DropdownItem onClick={this.handleClickOpenLocaleSwitcher}><Trans id='js.WfHamburgerMenu.menu.languages'>Languages</Trans></DropdownItem>
+            ) : null}
             {loggedIn ? (
               <DropdownItem href='/account/logout'><Trans id='js.WfHamburgerMenu.menu.logout'>Log Out</Trans></DropdownItem>
             ) : null}
@@ -64,6 +75,11 @@ export class WfHamburgerMenu extends React.Component {
           <ImportModuleFromGitHub
             closeModal={this.closeImportModal}
             api={api}
+          />
+        ) : null}
+        {enableLocaleSwitcher && this.state.localeSwitcherOpen ? (
+          <LocaleSwitcher
+            closeModal={this.closeLocaleSwitcher}
           />
         ) : null}
       </>
--- assets/js/i18n/LocaleSwitcher.js
+++ assets/js/i18n/LocaleSwitcher.js
@@ -1,38 +1,64 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
+import { Trans } from '@lingui/macro'
 import { withI18n } from '@lingui/react'
-import { supportedLocales, supportedLocaleIds } from './locales'
+import { supportedLocales, supportedLocaleIds, currentLocale } from './locales'
 import { csrfToken } from '../utils'
+import { Modal, ModalHeader, ModalBody } from '../components/Modal'
 
 /**
- * A menu for the user to select a locale.
+ * Decides whether to enable locale switching.
  *
  * Uses window.i18nConfig, which is injected by Django in the base template
  */
-const LocaleSwitcher = React.memo(function LocaleSwitcher ({ i18n }) {
-  const onChangeLocale = React.useCallback(ev => {
-    ev.target.form.submit()
-  })
+export const enableLocaleSwitcher = window.i18nConfig && window.i18nConfig.showSwitcher
 
-  if (window.i18nConfig && window.i18nConfig.showSwitcher) {
-    return (
-      <form method='POST' action='/locale'>
-        <input type='hidden' name='next' value={window.location.href} />
-        <input type='hidden' name='csrfmiddlewaretoken' value={csrfToken} />
-        <select value={i18n.language} name='new_locale' onChange={onChangeLocale}>
-          {supportedLocaleIds.map((locale) => (
-            <option key={locale} value={locale}>
-              {i18n._(supportedLocales[locale])}
-            </option>
-          ))}
-        </select>
-      </form>
-    )
-  } else {
-    return null
+/**
+ * A menu for the user to select a locale.
+ */
+class LocaleSwitcher extends React.PureComponent {
+  handleLocaleChange (ev) {
+    ev.target.form.submit()
   }
-})
+
+  render () {
+    return (
+      <Modal isOpen toggle={this.props.closeModal}>
+        <ModalHeader toggle={this.props.closeModal}>
+          <Trans id='js.i18n.LocaleSwitcher.header.title' description='This should be all-caps for styling reasons'>
+            LANGUAGES
+          </Trans>
+        </ModalHeader>
+        <ModalBody>
+          <p>
+            <Trans id='js.i18n.LocaleSwitcher.body.description'>
+              This option will change the language of Workbench's interface. To submit translations for your language, visit <a href='#'>this</a> page.
+            </Trans>
+          </p>
+          <form method='POST' action='/locale'>
+            <input type='hidden' name='next' value={window.location.href} />
+            <input type='hidden' name='csrfmiddlewaretoken' value={csrfToken} />
+            {supportedLocaleIds.map((locale) => (
+              <label key={locale}>
+                <input
+                  type='radio'
+                  name='new_locale'
+                  value={locale}
+                  checked={locale === currentLocale}
+                  onChange={this.handleLocaleChange}
+                />
+                {this.props.i18n._(supportedLocales[locale])}
+              </label>
+            ))}
+          </form>
+        </ModalBody>
+      </Modal>
+    )
+  }
+}
+
 LocaleSwitcher.propTypes = {
+  closeModal: PropTypes.func.isRequired,
   i18n: PropTypes.shape({
     // i18n object injected by LinguiJS withI18n()
     language: PropTypes.oneOf(supportedLocaleIds),
