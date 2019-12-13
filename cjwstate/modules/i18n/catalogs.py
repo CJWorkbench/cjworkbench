5f8eb4d5ac5743b753ee8768d68adaad015ed602
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2019-12-13 17:43:29 +0200

    Only write module message catalogs if they have changed

--- cjwstate/modules/i18n/catalogs.py
+++ cjwstate/modules/i18n/catalogs.py
@@ -7,6 +7,7 @@ from cjworkbench.i18n.catalogs.util import (
     fill_catalog,
     mark_fuzzy,
     MessageUID,
+    catalogs_are_same,
 )
 from babel.messages.catalog import Catalog
 from cjwstate.modules.module_loader import ModuleFiles, ModuleSpec
@@ -20,25 +21,32 @@ def update_module_catalogs(directory: pathlib.Path):
     source_catalog = _build_source_catalog(ModuleSpec.load_from_path(module_files.spec))
 
     po_path = _po_path(directory, default_locale)
+
     try:
         old_source_catalog = read_po_catalog(po_path)
     except FileNotFoundError:
         old_source_catalog = Catalog(default_locale)
-    fuzzy = find_fuzzy_messages(
-        old_catalog=old_source_catalog, new_catalog=source_catalog
-    )
-    write_po_catalog(po_path, source_catalog)
 
-    for locale_id in supported_locales:
-        po_path = _po_path(directory, locale_id)
-        try:
-            old_catalog = read_po_catalog(po_path)
-        except FileNotFoundError:
-            old_catalog = Catalog(locale_id)
-        write_po_catalog(
-            po_path,
-            _merge_nonsource_catalog(locale_id, old_catalog, source_catalog, fuzzy),
+    if not catalogs_are_same(source_catalog, old_source_catalog):
+        write_po_catalog(po_path, source_catalog)
+
+        fuzzy = find_fuzzy_messages(
+            old_catalog=old_source_catalog, new_catalog=source_catalog
         )
+
+        for locale_id in supported_locales:
+            if locale_id != default_locale:
+                po_path = _po_path(directory, locale_id)
+                try:
+                    old_catalog = read_po_catalog(po_path)
+                except FileNotFoundError:
+                    old_catalog = Catalog(locale_id)
+                catalog = _merge_nonsource_catalog(
+                    locale_id, old_catalog, source_catalog, fuzzy
+                )
+
+                if not catalogs_are_same(catalog, old_catalog):
+                    write_po_catalog(po_path, catalog)
 
 
 def _po_path(basepath: pathlib.Path, locale_id: str) -> pathlib.Path:
@@ -50,8 +58,7 @@ def _build_source_catalog(spec: ModuleSpec) -> Catalog:
     # TODO: also find messages in module code
     source_catalog = Catalog(default_locale)
     for message_id, source_string in messages.items():
-        if source_string is not None:
-            source_catalog.add(message_id, string=source_string)
+        source_catalog.add(message_id, string=source_string)
 
     return source_catalog
 
