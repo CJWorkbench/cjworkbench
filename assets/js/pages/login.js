ce69952a49645a1c9e0e03e88cdc5b983c8b4d8d
Adam Hooper <adam@adamhooper.com>
2019-12-17 12:54:50 -0500

    login.js: use popper with locale-switcher menu
    Now the menu will pop _up_ if we run out of vertical space.

--- assets/js/pages/login.js
+++ assets/js/pages/login.js
@@ -1,5 +1,19 @@
 import bootstrap from 'bootstrap.native'
+import Popper from 'popper.js'
 
 Array.prototype.forEach.call(document.querySelectorAll('div.dropdown'), (el) => {
-  /* new */ bootstrap.Dropdown(el, {})
+  /* new */ bootstrap.Dropdown(el)
+  let popper = null
+
+  el.parentNode.addEventListener('shown.bs.dropdown', (ev) => {
+    const toggleEl = el.querySelector('.dropdown-toggle')
+    const menuEl = el.querySelector('.dropdown-menu')
+
+    popper = new Popper(toggleEl, menuEl)
+  })
+
+  el.parentNode.addEventListener('hidden.bs.dropdown', () => {
+    popper.destroy()
+    popper = null
+  })
 })
