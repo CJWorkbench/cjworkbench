072fd0ca72b3fc5113e568644bb17723565bdd6c
Adam Hooper <adam@adamhooper.com>
2019-12-17 11:47:45 -0500

    unittest-entrypoint.sh: setup minio when called with -k

--- bin/unittest-entrypoint.sh
+++ bin/unittest-entrypoint.sh
@@ -13,27 +13,38 @@ setup_env() {
 }
 
 # Skip setup_env if we are only testing within `cjwkernel/`.
-if (( $# == 1)); then
+#
+# need_setup states:
+# 0: "yes ... unless later, we only specify cjwkernel.*"
+# 1: "no ... unless later, we add *not* cjwkernel.*"
+# 2: "yes"
+need_setup=0
+for arg in "$@"; do
+  case $arg in
+    cjwkernel)
+      # tests that don't require state
+      if test $need_setup -eq 0; then
+        need_setup=1
+      fi
+      ;;
+    cjwkernel.*)
+      # tests that don't require state
+      if test $need_setup -eq 0; then
+        need_setup=1
+      fi
+      ;;
+    -*)
+      # command-line flag passed to manage.py unittest
+      ;;
+    *)
+      # tests that may require state
+      need_setup=2
+      break
+      ;;
+  esac
+done
+if test $need_setup -ne 1; then
   setup_env
-else
-  for arg in "$@"; do
-    case $arg in
-      cjwkernel)
-        # tests that don't require state
-        ;;
-      cjwkernel.*)
-        # tests that don't require state
-        ;;
-      -*)
-        # command-line flag passed to manage.py unittest
-        ;;
-      *)
-        # tests that may require state
-        setup_env
-        break
-        ;;
-    esac
-  done
 fi
 
 # Ignore "WARNING: failed to mount loopback filesystem" by redirecting
