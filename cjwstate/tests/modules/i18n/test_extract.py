8d46377bdf414f71400392d3d39de370ab80ec4d
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2019-12-17 23:45:29 +0200

    Add some more unit tests to message extraction from module code

--- cjwstate/tests/modules/i18n/test_extract.py
+++ cjwstate/tests/modules/i18n/test_extract.py
@@ -102,6 +102,33 @@ def render(table):
         }
         self.assertEqual(result, expected)
 
+    def test_translation_multiple_appearances(self):
+        code = BytesIO(
+            b"""
+from cjwmodule import i18n
+from mygreatmodule import this
+
+def render(table):
+    if this():
+        return (table, 
+            i18n.trans('message.id', 'Default message with {parameter}', {'parameter': this()})
+        )
+    else:
+        return (table, 
+            i18n.trans('message.id', 'Default message with {parameter}', {'parameter': 'other'})
+        )
+        """
+        )
+        result = _find_messages_in_module_code(code, "module.py")
+        expected = {
+            "message.id": {
+                "string": "Default message with {parameter}",
+                "locations": [("module.py", 8), ("module.py", 12)],
+                "comments": [],
+            }
+        }
+        self.assertEqual(result, expected)
+
     def test_multiple_translations(self):
         code = BytesIO(
             b"""
@@ -137,6 +164,54 @@ def render(table):
                 "comments": [],
             },
         }
+        self.assertEqual(result, expected)
+
+    def test_translation_with_nonstring_id(self):
+        code = BytesIO(
+            b"""
+from cjwmodule.i18n import trans
+
+def render(table):
+    id = 'message.id'
+    return (table, 
+        trans(id, 'Default message')
+    )
+        """
+        )
+        result = _find_messages_in_module_code(code, "module.py")
+        expected = {}
+        self.assertEqual(result, expected)
+
+    def test_translation_with_nonstring_default(self):
+        code = BytesIO(
+            b"""
+from cjwmodule.i18n import trans
+
+def render(table):
+    default_message = 'Default message'
+    return (table, 
+        trans('message.id', default_message)
+    )
+        """
+        )
+        with self.assertRaises(SyntaxError):
+            result = _find_messages_in_module_code(code, "module.py")
+
+    def test_translation_with_nonstring_id_and_default(self):
+        code = BytesIO(
+            b"""
+from cjwmodule.i18n import trans
+
+def render(table):
+    id = 'message.id'
+    default_message = 'Default message'
+    return (table, 
+        trans(id, default_message)
+    )
+        """
+        )
+        result = _find_messages_in_module_code(code, "module.py")
+        expected = {}
         self.assertEqual(result, expected)
 
     def test_simple_translation_without_i18n_literal(self):
