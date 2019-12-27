2dbc69fd3e684ea8a15f00b975af77fcdd6dc65c
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2019-12-27 13:59:33 +0200

    Add i18n integration test for remembering user locale

--- integrationtests/test_i18n.py
+++ integrationtests/test_i18n.py
@@ -163,6 +163,57 @@ class TestI18n(WorkbenchBase):
         b.click_button("Σύνδεση")
         b.wait_for_element("a", text="ΟΙ ΡΟΈΣ ΕΡΓΑΣΙΏΝ ΜΟΥ", wait=True)
 
+    def test_remember_signup_locale(self):
+        b = self.browser
+        b.visit(self.live_server_url + "/account/signup/")
+        self.switch_locale_django("el")
+
+        # self.assertTrue(b.is_element_present_by_text('Use Facebook account'))
+        # self.assertTrue(b.is_element_present_by_text('Use Google account'))
+
+        b.fill_in("email", "user-el@example.org")
+        b.fill_in("first_name", "Jane")
+        b.fill_in("last_name", "Doe")
+        b.fill_in("password1", "?P455W0rd!")
+        b.fill_in("password2", "?P455W0rd!")
+        b.click_button("Εγγραφή")
+
+        b.assert_element(
+            "h1", text="Επιβεβαιώστε τη διεύθυνση του ηλ. ταχυδρομείου σας", wait=True
+        )
+
+        # Test the email is right
+        email = self.account_admin.latest_sent_email
+        self.assertIsNotNone(email)
+        self.assertEqual("user-el@example.org", email["To"])
+        url = find_url_in_email(email)
+        self.assertIsNotNone(url)
+
+        # Follow the link
+        b.visit(url)
+        b.click_button("Επιβεβαίωση", wait=True)
+
+        # Change locale to English and browse
+        self.switch_locale_django("en")
+        b.click_link("Sign up", wait=True)
+        b.assert_element(
+            'input[name="first_name"][placeholder="First name"]', wait=True
+        )
+        b.assert_element('button[type="submit"]', text="Register")
+        b.click_link("Sign in")
+
+        # Now log in with our new account
+        # TODO _why_? The user already logged in
+        b.fill_in("login", "user-el@example.org", wait=True)
+        b.fill_in("password", "?P455W0rd!")
+        b.click_button("Sign In")
+        b.wait_for_element("a", text="ΟΙ ΡΟΈΣ ΕΡΓΑΣΙΏΝ ΜΟΥ", wait=True)
+
+        # After logout, page must still be in Greek.
+        logout(b, "el")
+        b.assert_element('input[name="password"][placeholder="Συνθηματικό"]', wait=True)
+        b.assert_element('button[type="submit"]', text="Σύνδεση")
+
     def test_remember_locale_on_login(self):
         # When a user logs out, their locale continues to be used.
         # When a user logs in, the locale they used the last time they were logged in is used.
