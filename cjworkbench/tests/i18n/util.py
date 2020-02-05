ea4f19d50d327d8f65d9b37aad2b03511864d04e
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2020-02-05 22:32:43 +0200

    Fix utils for i18n test

--- cjworkbench/tests/i18n/util.py
+++ cjworkbench/tests/i18n/util.py
@@ -6,12 +6,11 @@ from unittest.mock import patch
 
 def mock_app_catalogs(catalogs: Dict[str, Catalog]):
     return patch.object(
-        MODULE_LOCALIZER_REGISTRY, "application_localizer", MessageLocalizer(catalogs)
+        MESSAGE_LOCALIZER_REGISTRY, "application_localizer", MessageLocalizer(catalogs)
     )
 
 
-@contextmanager
 def mock_cjwmodule_catalogs(catalogs: Dict[str, Catalog]):
     return patch.object(
-        MODULE_LOCALIZER_REGISTRY, "cjwmodule_localizer", MessageLocalizer(catalogs)
+        MESSAGE_LOCALIZER_REGISTRY, "cjwmodule_localizer", MessageLocalizer(catalogs)
     )
