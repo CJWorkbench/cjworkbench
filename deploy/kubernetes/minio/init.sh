234647f37c04a9ffbebacb4015d22d92d468e10a
Adam Hooper <adam@adamhooper.com>
2020-10-15 10:30:42 -0400

    Upgrade minio [finishes #175286609]
    Also, update k8s deployment to run with --json for nice logs.
    
    To deploy: we'll kubectl apply -f minio-deployment.yaml. First, let's
    let integration tests pass.

--- deploy/kubernetes/minio/init.sh
+++ deploy/kubernetes/minio/init.sh
@@ -123,13 +123,13 @@ kubectl apply -f "$DIR"/minio-service.yaml
 kubectl run minio-adduser \
   -i --rm=true \
   --restart=Never \
-  --image=minio/mc:RELEASE.2020-02-25T18-10-03Z \
+  --image=minio/mc:RELEASE.2020-10-03T02-54-56Z \
   --overrides='
     {
       "spec": {
         "containers": [{
           "name": "minio-adduser",
-          "image": "minio/mc:RELEASE.2020-02-25T18-10-03Z",
+          "image": "minio/mc:RELEASE.2020-10-03T02-54-56Z",
           "command": [
             "sh", "-c",
             "while ! nc -z minio-service 80; do sleep 0.1; done; mc config host add workbench http://minio-service \"$ROOT_ACCESS_KEY\" \"$ROOT_SECRET_KEY\" && mc admin user add workbench \"$ACCESS_KEY\" \"$SECRET_KEY\" && mc admin policy set workbench readwrite user=\"$ACCESS_KEY\""
