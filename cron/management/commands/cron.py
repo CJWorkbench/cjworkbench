9372549a339f9c12ca0c923112043e4cea9adbd6
Adam Hooper <adam@adamhooper.com>
2021-01-19 09:03:35 -0500

    cron: exit on any exception
    Previously, an rmq CancelledError wouldn't lead to restart.

--- cron/management/commands/cron.py
+++ cron/management/commands/cron.py
@@ -1,6 +1,24 @@
 import asyncio
+
 from django.core.management.base import BaseCommand
-from cron.main import main
+
+from cron.main import main as main_loop
+
+
+def exit_on_exception(loop, context):
+    logger.error(
+        "Exiting because of unhandled error: %s\nContext: %r",
+        context["message"],
+        context,
+        exc_info=context.get("exception"),
+    )
+    os._exit(1)
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+    loop.set_exception_handler(exit_on_exception)
+    await main_loop()
 
 
 class Command(BaseCommand):
