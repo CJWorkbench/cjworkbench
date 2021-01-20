f2d2ef19be10e36c14ba4b9ca08c70b8e523af73
Adam Hooper <adam@adamhooper.com>
2021-01-20 12:46:16 -0500

    OutputPane, Embed: add 16px margin around charts
    This is a _rewrite_ of Embed.js/embed.scss. No more Bootstrap.
    
    [finishes #176324687]

--- assets/js/Embed.js
+++ assets/js/Embed.js
@@ -1,104 +1,120 @@
 import React from 'react'
-import { escapeHtml, timeDifference } from './utils'
+import PropTypes from 'prop-types'
+import { timeDifference } from './utils'
 import { i18n } from '@lingui/core'
-import { t, Trans } from '@lingui/macro'
+import { Trans } from '@lingui/macro'
+import EmbedIcon from '../icons/embed.svg'
 
-export default class Embed extends React.Component {
-  state = {
-    overlayOpen: false
-  }
+function IframeContainer (props) {
+  const { step } = props
+  return (
+    <div className='iframe-container'>
+      <iframe src={'/api/wfmodules/' + step.id + '/output'} />
+    </div>
+  )
+}
 
-  handleToggleOverlay = (e) => {
-    this.setState({
-      overlayOpen: !this.state.overlayOpen
-    })
-  }
+function Logo (props) {
+  return (
+    <a className='logo' href='https://workbenchdata.com' target='_blank' rel='noopener noreferrer'>
+      <img src={`${window.STATIC_URL}images/logo.png`} width='35' alt='Workbench' />
+    </a>
+  )
+}
 
-  renderNotAvailable () {
-    return (
-      <div className='embed-wrapper'>
-        <div className='embed-not-available'>
-          <h1><Trans id='js.Embed.workflowNotAvailable.title'>This workflow is not available</Trans></h1>
-        </div>
-        <div className='embed-footer'>
-          <div className='embed-footer-logo'>
-            <img src={`${window.STATIC_URL}images/logo.png`} width='21' />
-          </div>
-          <div className='embed-footer-meta'>
-            <h1>
-              <Trans id='js.Embed.workflowNotAvailable.footer.logo' comment='This should be all-caps for styling reasons'>
-                    WORKBENCH
-              </Trans>
-            </h1>
-          </div>
-          <div className='embed-footer-button'>
-            <i className='icon icon-share' />
-          </div>
-        </div>
-      </div>
-    )
-  }
+function EmbedButton (props) {
+  const { step } = props
+  const [isOpen, setOpen] = React.useState(false)
 
-  render () {
-    if (!this.props.workflow && !this.props.step) {
-      return this.renderNotAvailable()
-    }
-    const iframeCode = escapeHtml('<iframe src="' + window.location.protocol + '//' + window.location.host + '/embed/' + this.props.step.id + '" width="560" height="315" frameborder="0"></iframe>')
-    const timeAgo = timeDifference(this.props.workflow.last_update, new Date(), i18n)
+  const iframeCode = `<iframe src="${window.location.origin}/embed/${step.id} width="560" height="315" frameborder="0"></iframe>`
+  const handleClickOpen = React.useCallback(() => setOpen(true), [setOpen])
+  const handleClickClose = React.useCallback(() => setOpen(false), [setOpen])
 
-    return (
-      <div className='embed-wrapper'>
-        <iframe src={'/api/wfmodules/' + this.props.step.id + '/output'} frameBorder={0} />
-        <div className='embed-footer'>
-          <div className='metadata-stack'>
-            <div className='embed-footer-logo'>
-              <a href='http://workbenchdata.com' target='_blank' rel='noopener noreferrer'>
-                <img src={`${window.STATIC_URL}images/logo.png`} width='35' />
-              </a>
-            </div>
-            <div className='embed-footer-meta'>
-              <div className='title'>
-                <a href={'/workflows/' + this.props.workflow.id} target='_blank' rel='noopener noreferrer'>
-                  {this.props.workflow.name}
-                </a>
-              </div>
-              <div className='metadata'>
-                <ul>
-                  <li>
-                    <a href={'/workflows/' + this.props.workflow.id} target='_blank' rel='noopener noreferrer'>
-                      <Trans id='js.Embed.metadata.author'>by {this.props.workflow.owner_name}</Trans>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={'/workflows/' + this.props.workflow.id} target='_blank' rel='noopener noreferrer'>
-                      {t({
-                        comment: "{timeAgo} will contain a time difference (i.e. something like '4h ago')",
-                        id: 'js.Embed.metadata.updated',
-                        message: 'Updated {timeAgo}',
-                        values: { timeAgo }
-                      })}
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <button type='button' onClick={this.handleToggleOverlay} className='embed-footer-button'>
-            <i className='icon icon-code' />
-          </button>
-        </div>
-        <div className={'embed-overlay' + (this.state.overlayOpen ? ' open' : '')} onClick={this.handleToggleOverlay}>
-          <div className='embed-share-links' onClick={(e) => { e.stopPropagation() }}>
-            <h1><Trans id='js.Embed.embedThisChart' comment='This should be all-caps for styling reasons'>EMBED THIS CHART</Trans></h1>
-            <h2><Trans id='js.Embed.embedCode'>Paste this code into any webpage HTML</Trans></h2>
-            <div className='code-snippet'>
-              <code className='embed--share-code'>
-                {iframeCode}
-              </code>
-            </div>
+  return (
+    <>
+      <button name='embed' type='button' onClick={handleClickOpen}>
+        <EmbedIcon />
+      </button>
+      <div className={'embed-overlay' + (isOpen ? ' open' : '')} onClick={handleClickClose}>
+        <div className='embed-share-links' onClick={(e) => { e.stopPropagation() }}>
+          <h1><Trans id='js.Embed.embedThisChart' comment='This should be all-caps for styling reasons'>EMBED THIS CHART</Trans></h1>
+          <h2><Trans id='js.Embed.embedCode'>Paste this code into any webpage HTML</Trans></h2>
+          <div className='code-snippet'>
+            <code className='embed--share-code'>
+              {iframeCode}
+            </code>
           </div>
         </div>
       </div>
-    )
+    </>
+  )
+}
+
+function Footer (props) {
+  const { workflow, step } = props
+  const timeAgo = timeDifference(workflow.last_update, new Date(), i18n)
+
+  return (
+    <footer>
+      <Logo />
+      <div className='metadata'>
+        <h1>
+          <a href={`/workflows/${workflow.id}/`} target='_blank' rel='noopener noreferrer'>
+            {workflow.name}
+          </a>
+        </h1>
+        <ul>
+          <li><Trans id='js.Embed.metadata.author'>by {workflow.owner_name}</Trans></li>
+          <li>
+            <Trans
+              id='js.Embed.metadata.updated'
+              comment="`<0>{timeAgo}</0>` will show a time difference (i.e. something like '4h ago')"
+            >
+              Updated <time time={workflow.last_update}>{timeAgo}</time>
+            </Trans>
+          </li>
+        </ul>
+      </div>
+      <EmbedButton step={step} />
+    </footer>
+  )
+}
+
+function NotAvailable () {
+  return (
+    <>
+      <p className='not-available'><Trans id='js.Embed.workflowNotAvailable.title'>This workflow is not available</Trans></p>
+      <footer>
+        <Logo />
+        <div className='embed-footer-meta'>
+          <h1><Trans id='js.Embed.workflowNotAvailable.footer.logo'>Workbench</Trans></h1>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default function Embed (props) {
+  const { workflow, step } = props
+
+  if (!workflow && !step) {
+    return <NotAvailable />
   }
+
+  return (
+    <>
+      <IframeContainer step={step} />
+      <Footer workflow={workflow} step={step} />
+    </>
+  )
+}
+Embed.propTypes = {
+  workflow: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    last_update: PropTypes.string.isRequired, // ISO8601 String
+    owner_name: PropTypes.string.isRequired
+  }), // or null, on error
+  step: PropTypes.shape({
+    id: PropTypes.number.isRequired // TODO migrate to workflowId+stepSlug URLs
+  }) // or null, on error
 }
