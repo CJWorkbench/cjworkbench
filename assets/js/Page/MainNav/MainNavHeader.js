52b4295cebd5ff21cae280dcb93e92a2c01cad0e
Adam Hooper <adam@adamhooper.com>
2021-02-05 14:46:40 -0500

    nav.main-nav: look good on mobile

--- assets/js/Page/MainNav/MainNavHeader.js
+++ assets/js/Page/MainNav/MainNavHeader.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { t } from '@lingui/macro'
+import MenuIcon from '../../../icons/menu.svg'
 
 export default function MainNavHeader (props) {
-  const { href } = props
+  const { href, onToggleOpen } = props
+
   return (
     <header>
       <a href={href}>
@@ -12,9 +14,17 @@ export default function MainNavHeader (props) {
           alt={t({ id: 'js.Page.MainNav.Header.brandName', message: 'Workbench' })}
         />
       </a>
+      <button
+        className='toggle-open'
+        onClick={onToggleOpen}
+        title={t({ id: 'js.WfHamburgerMenu.toggle.hoverText', message: 'menu' })}
+      >
+        <MenuIcon />
+      </button>
     </header>
   )
 }
 MainNavHeader.propTypes = {
-  href: PropTypes.string // or null
+  href: PropTypes.string, // or null
+  onToggleOpen: PropTypes.func.isRequired
 }
--- assets/js/Page/MainNav/index.js
+++ assets/js/Page/MainNav/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import MainNavFragment from './MainNavFragment'
+import MainNavHeader from './MainNavHeader'
+import TopPaths from './TopPaths'
+import BottomPaths from './BottomPaths'
 
 export default function MainNav (props) {
+  const [open, toggleOpen] = React.useReducer(open => !open, false)
   const { currentPath, user } = props
 
   return (
-    <nav className='main-nav'>
-      <MainNavFragment user={user} currentPath={currentPath} />
+    <nav className={`main-nav${open ? ' open' : ''}`}>
+      <MainNavHeader
+        href={currentPath === '/workflows' ? null : '/workflows'}
+        onToggleOpen={toggleOpen}
+      />
+      <TopPaths currentPath={currentPath} />
+      <BottomPaths user={user} currentPath={currentPath} />
     </nav>
   )
 }
