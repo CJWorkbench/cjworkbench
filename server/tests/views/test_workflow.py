ec5f43f6051274896393cf02f39a8e0d4eb3a89d
Adam Hooper <adam@adamhooper.com>
2021-02-08 11:04:08 -0500

    views.test_workflow: simplify and fix

--- server/tests/views/test_workflow.py
+++ server/tests/views/test_workflow.py
@@ -9,10 +9,7 @@ from cjwkernel.types import RenderResult
 from cjwstate import rabbitmq
 from cjwstate.rendercache import cache_render_result
 from cjwstate.models import Workflow
-from cjwstate.tests.utils import (
-    DbTestCaseWithModuleRegistryAndMockKernel,
-    create_module_zipfile,
-)
+from cjwstate.tests.utils import DbTestCase
 from server.views.workflows import Index, render_workflow
 
 
@@ -23,8 +20,109 @@ async def async_noop(*args, **kwargs):
     pass
 
 
+class WorkflowListTest(DbTestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.user = User.objects.create(
+            username="user", email="user@example.com", password="password"
+        )
+        self.other_user = User.objects.create(
+            username="other_user", email="other_user@example.com", password="password"
+        )
+
+    def test_index_get(self):
+        # set dates to test reverse chron ordering
+        workflow1 = Workflow.create_and_init(
+            name="Workflow 1", owner=self.user, updated_at="2010-10-20T1:23"
+        )
+        workflow2 = Workflow.create_and_init(
+            name="Workflow 2", owner=self.user, updated_at="2015-09-18T2:34"
+        )
+
+        self.client.force_login(self.user)
+        response = self.client.get("/workflows")
+        self.assertEqual(response.status_code, status.OK)
+
+        workflows = response.context_data["initState"]["workflows"]
+        # should not pick up other user's workflows, even public ones
+        self.assertEqual(len(workflows), 2)
+
+        self.assertEqual(workflows[0]["name"], "Workflow 2")
+        self.assertEqual(workflows[0]["id"], workflow2.id)
+        self.assertEqual(workflows[0]["public"], workflow1.public)
+        self.assertEqual(workflows[0]["read_only"], False)  # user is owner
+        self.assertEqual(workflows[0]["is_owner"], True)  # user is owner
+        self.assertIsNotNone(workflows[0]["last_update"])
+        self.assertEqual(workflows[0]["owner_name"], "user@example.com")
+
+        self.assertEqual(workflows[1]["name"], "Workflow 1")
+        self.assertEqual(workflows[1]["id"], workflow1.id)
+
+    def test_index_ignore_other_user_workflows(self):
+        Workflow.create_and_init(name="Hers", owner=self.other_user)
+        self.client.force_login(self.user)
+        response = self.client.get("/workflows")
+        self.assertEqual(response.context_data["initState"]["workflows"], [])
+
+    def test_index_ignore_example(self):
+        Workflow.create_and_init(
+            name="Example",
+            owner=self.other_user,
+            public=True,
+            example=True,
+            in_all_users_workflow_lists=True,
+        )
+        self.client.force_login(self.user)
+        response = self.client.get("/workflows")
+        self.assertEqual(response.context_data["initState"]["workflows"], [])
+
+    def test_index_ignore_lesson(self):
+        Workflow.create_and_init(name="Hers", owner=self.user, lesson_slug="a-lesson")
+        self.client.force_login(self.user)
+        response = self.client.get("/workflows")
+        self.assertEqual(response.context_data["initState"]["workflows"], [])
+
+    def test_shared(self):
+        workflow = Workflow.create_and_init(name="Hers", owner=self.other_user)
+        workflow.acl.create(email=self.user.email, can_edit=False)
+        self.client.force_login(self.user)
+        response = self.client.get("/workflows/shared-with-me")
+        self.assertEqual(
+            [w["name"] for w in response.context_data["initState"]["workflows"]],
+            ["Hers"],
+        )
+
+    def test_examples(self):
+        Workflow.create_and_init(
+            name="Example",
+            owner=self.other_user,
+            public=True,
+            example=True,
+            in_all_users_workflow_lists=True,
+        )
+        self.client.force_login(self.user)
+        response = self.client.get("/workflows/examples")
+        self.assertEqual(
+            [w["name"] for w in response.context_data["initState"]["workflows"]],
+            ["Example"],
+        )
+
+    def test_examples_ignore_when_not_in_all_users_workflow_lists(self):
+        Workflow.create_and_init(
+            name="Example",
+            owner=self.other_user,
+            public=True,
+            example=True,
+            in_all_users_workflow_lists=False,
+        )
+        self.client.force_login(self.user)
+        response = self.client.get("/workflows/examples")
+        self.assertEqual(response.context_data["initState"]["workflows"], [])
+
+
 @patch("cjwstate.commands.websockets_notify", async_noop)
-class WorkflowViewTests(DbTestCaseWithModuleRegistryAndMockKernel):
+class WorkflowViewTests(DbTestCase):
     def setUp(self):
         super().setUp()
 
@@ -41,7 +139,6 @@ class WorkflowViewTests(DbTestCaseWithModuleRegistryAndMockKernel):
 
         self.workflow1 = Workflow.create_and_init(name="Workflow 1", owner=self.user)
         self.tab1 = self.workflow1.tabs.first()
-        self.module_zipfile1 = create_module_zipfile("module1")
 
         # Add another user, with one public and one private workflow
         self.otheruser = User.objects.create(
@@ -54,79 +151,6 @@ class WorkflowViewTests(DbTestCaseWithModuleRegistryAndMockKernel):
         super().tearDown()
 
     # --- Workflow list ---
-    def test_index_get(self):
-        # set dates to test reverse chron ordering
-        self.workflow1.updated_at = "2010-10-20 1:23Z"
-        self.workflow1.save()
-        self.workflow2 = Workflow.create_and_init(
-            name="Workflow 2", owner=self.user, updated_at="2015-09-18 2:34Z"
-        )
-
-        self.client.force_login(self.user)
-        response = self.client.get("/workflows")
-        self.assertEqual(response.status_code, status.OK)
-
-        workflows = response.context_data["initState"]["workflows"]
-        # should not pick up other user's workflows, even public ones
-        self.assertEqual(len(workflows), 2)
-
-        self.assertEqual(workflows[0]["name"], "Workflow 2")
-        self.assertEqual(workflows[0]["id"], self.workflow2.id)
-        self.assertEqual(workflows[0]["public"], self.workflow1.public)
-        self.assertEqual(workflows[0]["read_only"], False)  # user is owner
-        self.assertEqual(workflows[0]["is_owner"], True)  # user is owner
-        self.assertIsNotNone(workflows[0]["last_update"])
-        self.assertEqual(workflows[0]["owner_name"], "user@example.com")
-
-        self.assertEqual(workflows[1]["name"], "Workflow 1")
-        self.assertEqual(workflows[1]["id"], self.workflow1.id)
-
-    def test_index_include_example_in_all_users_workflow_lists(self):
-        self.other_workflow_public = Workflow.create_and_init(
-            name="Other workflow public",
-            owner=self.otheruser,
-            public=True,
-            example=True,
-            in_all_users_workflow_lists=True,
-        )
-        self.workflow2 = Workflow.create_and_init(owner=self.user)
-
-        self.client.force_login(self.user)
-        response = self.client.get("/workflows")
-        self.assertEqual(response.status_code, status.OK)
-
-        workflows = response.context_data["initState"]["workflows"]
-        self.assertEqual(len(workflows["owned"]), 2)
-        self.assertEqual(len(workflows["templates"]), 1)
-
-    def test_index_exclude_example_not_in_all_users_lists(self):
-        self.other_workflow_public = Workflow.create_and_init(
-            name="Other workflow public",
-            owner=self.otheruser,
-            public=True,
-            example=True,
-            in_all_users_workflow_lists=False,
-        )
-        self.workflow2 = Workflow.create_and_init(owner=self.user)
-
-        self.client.force_login(self.user)
-        response = self.client.get("/workflows")
-        self.assertEqual(response.status_code, status.OK)
-
-        workflows = response.context_data["initState"]["workflows"]
-        self.assertEqual(len(workflows["owned"]), 2)
-        self.assertEqual(len(workflows["templates"]), 0)
-
-    def test_index_exclude_lesson(self):
-        self.workflow1.lesson_slug = "some-lesson"
-        self.workflow1.save()
-        self.workflow2 = Workflow.create_and_init(owner=self.user)
-
-        self.client.force_login(self.user)
-        response = self.client.get("/workflows")
-        workflows = response.context_data["initState"]["workflows"]
-        self.assertEqual(response.status_code, status.OK)
-        self.assertEqual(len(workflows["owned"]), 1)
 
     def test_index_post(self):
         self.client.force_login(self.user)
