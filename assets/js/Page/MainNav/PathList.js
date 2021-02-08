5ea6ce46aa487da99b9af940354dd2edf4dd580f
Adam Hooper <adam@adamhooper.com>
2021-02-08 14:10:47 -0500

    MainNav/: nix cruft

--- assets/js/Page/MainNav/PathList.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-export default function PathList (props) {
-}
-PathList.propTypes = {
-  path: PropTypes.string // or null, if subpaths is set
-}
