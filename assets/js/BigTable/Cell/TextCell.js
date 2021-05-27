3597424cd1575ac67683f83f1dc2a9c51fed31ba
Adam Hooper <adam@adamhooper.com>
2021-05-27 11:30:12 -0400

    Make text cells link [refs #177180100]
    Ran into a bit of a design problem: clicking should open edit mode!
    Committing this half-finished idea while we discuss next steps.

--- assets/js/BigTable/Cell/TextCell.js
+++ assets/js/BigTable/Cell/TextCell.js
@@ -14,10 +14,63 @@ const UnicodeWhitespaceReplacements = {
   '\v': '⭿'
 }
 
+const ValidSchemes = /^https?:\/\/[^/]/
+const UrlCodePointOrPercentEscape = "(?:[-!$&'()*+,./:;=?@_~0-9a-zA-Z\u{00a0}-\u{10fffd}]|%[0-9a-fA-F]{2})"
+const AllUrlCodePointsOrPercentEscapes = new RegExp(`^${UrlCodePointOrPercentEscape}*(?:#${UrlCodePointOrPercentEscape}*)?$`, 'u')
+
+function parseValidUrl (s) {
+  // https://url.spec.whatwg.org/#concept-basic-url-parser
+  // The spec talks of "validation error", but `URL()` does not expose
+  // validation errors anywhere! (It recovers from them). So our algorithm
+  // uses URL() for the "hard" parts ... and it returns false if it sees
+  // some validation errors.
+
+  if (!ValidSchemes.test(s)) {
+    // avoid "url is special" everywhere
+    // avoid "file" logic everywhere
+    //
+    // The regex also bans "https:///", because
+    // https://url.spec.whatwg.org/#example-url-parsing says it's invalid,
+    // though [adamhooper, 2021-05-27] I can't find the portion of the spec
+    // that explains why it's invalid.
+    return null
+  }
+
+  if (!AllUrlCodePointsOrPercentEscapes.test(s)) {
+    // 1.2. If input contains any leading or trailing C0 control or space, validation error.
+    // 1. If input contains any ASCII tab or newline, validation error.
+    // path state: URL code point or invalid % => validation error
+    // query state: URL code point or invalid % => validation error
+    // fragment state: URL code point or invalid % => validation error
+    return null
+  }
+
+  try {
+    const url = new URL(s)
+    return url.href
+  } catch (e) {
+    return null
+  }
+}
+
 export default function TextCell (props) {
   const { value } = props
   if (value === null) {
     return <NullCell type='text' />
+  }
+
+  const href = parseValidUrl(value)
+  if (href !== null) {
+    return (
+      <a
+        className='cell-text'
+        target='_blank'
+        rel='noopener noreferrer'
+        href={href}
+      >
+        {value}
+      </a>
+    )
   }
 
   // Make a one-line value: we'll style it with white-space: pre so the user
--- assets/js/BigTable/Cell/TextCell.test.js
+++ assets/js/BigTable/Cell/TextCell.test.js
@@ -23,6 +23,64 @@ test('className=cell-text', () => {
   expect(container.firstChild.className).toEqual('cell-text')
 })
 
+test('absolute link => <a>', () => {
+  const url = 'http://example.org/page?foo=bar&bar=baz#a23'
+  const { container } = renderWithI18n(<TextCell value={url} />)
+  expect(container.firstChild.tagName).toEqual('A')
+  expect(container.firstChild.className).toEqual('cell-text')
+  expect(container.firstChild.href).toEqual(url)
+  expect(container.firstChild.textContent).toEqual(url)
+})
+
+test('imperfect link => not <a>', () => {
+  const url = 'http://example.org/page?foo=bar&bar=baz#a23 '
+  const { container } = renderWithI18n(<TextCell value={url} />)
+  expect(container.firstChild.tagName).toEqual('DIV')
+})
+
+test('percent-encode link paths', () => {
+  // https://url.spec.whatwg.org/#example-5434421b
+  const { container } = renderWithI18n(<TextCell value='https://example.org/💩' />)
+  expect(container.firstChild.href).toEqual('https://example.org/%F0%9F%92%A9')
+  expect(container.firstChild.textContent).toEqual('https://example.org/💩')
+})
+
+test('punycode link hosts', () => {
+  // https://url.spec.whatwg.org/#example-host-psl
+  const { container } = renderWithI18n(<TextCell value='http://إختبار' />)
+  expect(container.firstChild.href).toEqual('http://xn--kgbechtv/')
+  expect(container.firstChild.textContent).toEqual('http://إختبار')
+})
+
+test('link with fragment', () => {
+  const { container } = renderWithI18n(<TextCell value='https://example.org/page#💩' />)
+  expect(container.firstChild.href).toEqual('https://example.org/page#%F0%9F%92%A9')
+  expect(container.firstChild.textContent).toEqual('https://example.org/page#💩')
+})
+
+test('not link whitespace', () => {
+  const { container } = renderWithI18n(<TextCell value='https://example.com ' />)
+  expect(container.firstChild.tagName).toEqual('DIV')
+})
+
+;
+[
+  'https:example.org',
+  'https://////example.com///',
+  'https://user:password@example.org/\t',
+  'https://example.org/foo bar',
+  'https://ex ample.org/',
+  'example',
+  'https://example.com:demo',
+  'http://[www.example.com]/'
+].forEach(url => {
+  test(`not link invalid URL ${url}`, () => {
+    // as per https://url.spec.whatwg.org/#example-url-parsing
+    const { container } = renderWithI18n(<TextCell value={url} />)
+    expect(container.firstChild.tagName).toEqual('DIV')
+  })
+})
+
 test('show whitespace as special characters', () => {
   // relates to https://www.pivotaltracker.com/story/show/159269958
   // We use CSS to preserve whitespace, and we use 'pre' because we
