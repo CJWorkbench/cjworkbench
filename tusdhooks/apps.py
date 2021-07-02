239c06b49dd91766e490cf0ea40b009a9b35a997
Adam Hooper <adam@adamhooper.com>
2021-07-02 17:03:28 -0400

    tusdhooks: fix integration tests
    ? ... it's Friday, so I'll let this run over the weekend and
    investigate Monday.

--- tusdhooks/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TusdHooksConfig(AppConfig):
-    name = "tusdhooks"
-    path = "/app/tusdhooks"
--- tusdhooks/settings.py
+++ tusdhooks/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sites",
     "cjworkbench",  # UserProfile model, etc.
-    "tusdhooks.apps.TusdHooksConfig",
+    "tusdhooks",
 ]
 
 MIDDLEWARE = []
