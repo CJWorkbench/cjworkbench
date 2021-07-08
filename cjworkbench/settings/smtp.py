3f1d17b4b811c07c89ba29e4b53f35a8af868d8e
Adam Hooper <adam@adamhooper.com>
2021-07-08 13:58:09 -0400

    CJW_MOCK_EMAIL: interpret missing as false

--- cjworkbench/settings/smtp.py
+++ cjworkbench/settings/smtp.py
@@ -6,7 +6,7 @@ from .util import DJANGO_ROOT as _django_root, FalsyStrings as _falsy_strings
 DEFAULT_FROM_EMAIL = "Workbench <hello@workbenchdata.com>"
 
 # EMAIL_BACKEND (copied in server/settings.py)
-if _debug or _os.environ.get("CJW_MOCK_EMAIL") not in _falsy_strings:
+if _debug or _os.environ.get("CJW_MOCK_EMAIL", "False") not in _falsy_strings:
     EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
     EMAIL_FILE_PATH = _os.path.join(_django_root, "local_mail")
 else:
