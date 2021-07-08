5fd04569c5d3e1c31da0dcf67649eb680ea99f32
Adam Hooper <adam@adamhooper.com>
2021-07-08 14:21:33 -0400

    Whoop, flip the meaning of False

--- cjworkbench/settings/debug.py
+++ cjworkbench/settings/debug.py
@@ -5,10 +5,6 @@ from .util import FalsyStrings
 
 __all__ = ("DEBUG", "I_AM_TESTING", "TEST_RUNNER")
 
-if "CJW_PRODUCTION" in os.environ:
-    DEBUG = os.environ["CJW_PRODUCTION"] not in FalsyStrings
-else:
-    DEBUG = True
-
+DEBUG = os.environ.get("CJW_PRODUCTION", "False") not in FalsyStrings
 I_AM_TESTING = "test" in sys.argv
 TEST_RUNNER = "server.tests.runner.TimeLoggingDiscoverRunner"  # only for unittests
