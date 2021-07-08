9a1b479e70c94a477b6acc72de03159a6c80caf3
Adam Hooper <adam@adamhooper.com>
2021-07-08 15:17:19 -0400

    Un-invert CJW_PRODUCTION

--- cjworkbench/settings/debug.py
+++ cjworkbench/settings/debug.py
@@ -5,6 +5,6 @@ from .util import FalsyStrings
 
 __all__ = ("DEBUG", "I_AM_TESTING", "TEST_RUNNER")
 
-DEBUG = os.environ.get("CJW_PRODUCTION", "False") not in FalsyStrings
+DEBUG = os.environ.get("CJW_PRODUCTION", "False") in FalsyStrings
 I_AM_TESTING = "test" in sys.argv
 TEST_RUNNER = "server.tests.runner.TimeLoggingDiscoverRunner"  # only for unittests
